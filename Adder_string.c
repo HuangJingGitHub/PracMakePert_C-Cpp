eb9f96152eb957f44cbf02a7e570f278d2f13722
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2018-06-18 17:56:08 +0800

    Add files via upload

---
+++ Adder_string.c
@@ -0,0 +1,31 @@
+/*Function: 将字符串中数字前加上$*/ 
+#include <stdio.h>
+
+int Adder(char *s)
+{
+   int i, j;      // 字符串的复制比较思路 
+   char t[100];
+   for(i=0; s[i]!='\0'; i++)
+     t[i]=s[i];
+	 t[i]='\0'; 
+   for(i=0, j=0; t[i]!='\0'; i++,j++)
+   {  if(t[i]>='0' && t[i]<='9')
+      {  s[j]='$';
+         j++;
+         s[j]=t[i];
+	   }
+	  else
+	    s[j]=t[i];
+   }
+   return 0;
+}
+
+int main()
+{  
+   char ori[100];
+   printf("Please input the string:\n");
+   scanf("%s",ori);
+   Adder(ori);
+   printf("The new string is: %s\n",ori);
+   
+}
