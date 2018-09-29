9cb2c4c26466e9251dcf0c7c0dc0ade417e65217
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2018-09-29 22:38:51 +0800

    Add files via upload

--- circle.cpp
+++ circle.cpp
@@ -2,6 +2,60 @@
 
 const double pi = 3.141592653589793;
 
+bool operator ==(const Circle &c1, const Circle &c2)
+{
+	return (c1.r == c2.r)&&(c1.x == c2.x)&&(c1.y == c2.y);
+}
+
+bool operator !=(const Circle &c1, const Circle &c2)
+{
+	return (c1.r != c2.r)||(c1.x != c2.x )||(c1.y != c2.y);
+}
+
+Circle & Circle::operator =(const Circle &c)
+{
+	r = c.r;
+	x = c.x;
+	y = c.y;
+	return *this;
+}
+
+Circle & Circle::operator +=(const Circle & c)
+{
+	r += c.r;
+	x += c.x;
+	y += c.y;
+	return *this;
+}
+
+Circle & Circle::operator *=(const Circle & c)
+{
+	r *= c.r;
+	x *= c.x;
+	y *= c.y;
+	return *this;
+ } 
+ 
+ Circle & Circle::operator *=(const int & k)
+ {
+ 	r *= k;
+ 	x *= k;
+ 	y *= k;
+ 	return *this;
+ }
+ 
+ Circle & Circle::operator ++()
+ {
+ 	++ r;
+ 	return *this;
+  } 
+  
+Circle Circle::operator ++(int)
+{
+	Circle _t(*this);
+	++ r;
+	return _t;
+ } 
 void Circle::GetOrigin(double *x, double *y)
 {
 	*x = this->x;
--- circle.h
+++ circle.h
@@ -1,6 +1,19 @@
 class Circle
 {
 	public:
+		Circle(double ri, double xi, double yi):
+		        x (xi) , y (yi)
+				{ r = ri > 0 ? ri : 0;
+				 } 
+		friend bool operator ==(const Circle &c1, const Circle &c2);
+		friend bool operator !=(const Circle &c1, const Circle &c2);
+		Circle & operator =(const Circle & c);
+		Circle & operator +=(const Circle & c);
+		Circle & operator *=(const Circle & c);
+		Circle & operator *=(const int & k);
+		Circle & operator ++();
+		Circle operator ++(int);
+		
 		void GetOrigin(double *x, double *y);
 		void SetOrigin(double x, double y);
 		double GetRadius();
