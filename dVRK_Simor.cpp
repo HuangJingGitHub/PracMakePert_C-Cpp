63e57c5cbd9c1e9549abf8cb9d412a15a521b522
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2018-12-11 09:30:48 +0800

    Add files via upload

--- dVRK_Simor.cpp
+++ dVRK_Simor.cpp
@@ -1,4 +1,11 @@
-/*#include <iostream>
+#include <fstream>
 #include "Robotics_config.h"
+#include "dVRK_config.h"
 
-MatrixXf MTM_DH;*/
+int main()
+{
+	MatrixXf mtm_q_test = MatrixXf::Zero(MTM_frame_num, 1);
+	cout << mtm_q_test << endl;
+	DH MTM_test(7);
+	DH MTM_test1 = MTM_Model(mtm_q_test);
+}
