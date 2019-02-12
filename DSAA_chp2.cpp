853d6e30f7386212ecdb6b3022b914fe5aa7c157
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-02-12 22:40:34 +0800

    Add files via upload

--- DSAA_chp2.cpp
+++ DSAA_chp2.cpp
@@ -1,21 +1,57 @@
-#inlude <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
-int maxSubSum2( const vector<int> &a )
+int max3(int, int, int);
+int maxSumRec(const vector<int> &a, int left, int right)
 {
-	int maxSum = 0;
-	
-	for (int i = 0; i < a.size(); ++i)
-	{
-		int thisSum = 0;
-		for (int j = i; j < a.size(); ++j)
-		{
-			thisSum += a[j];
-			if (thisSum > maxSum)
-				maxSum = thisSum;
-		}
-	}
-	return maxSum;
+    if (left == right)
+        if (a[left] > 0)
+            return a[left];
+    else
+            return 0;
+
+    int center = (left + right) / 2;
+    int maxLeftSum = maxSumRec(a, left, center);
+    int maxRightSum = maxSumRec(a, center+1, right);
+
+    int maxLeftBorderSum = 0, leftBorderSum = 0;
+    for (int i = center; i >= left; --i)
+    {
+        leftBorderSum += a[i];
+        if(leftBorderSum > maxLeftBorderSum)
+            maxLeftBorderSum = leftBorderSum;
+    }
+
+    int maxRightBorderSum = 0, rightBorderSum = 0;
+    for (int j = center + 1; j <= right; ++j)
+    {
+        rightBorderSum += a[j];
+        if( rightBorderSum > maxRightBorderSum)
+            maxRightBorderSum = rightBorderSum;
+    }
+
+    return max3(maxLeftSum, maxRightSum, maxLeftBorderSum + maxRightBorderSum);
 }
 
+int maxSunSum3( const vector<int> &a)
+{
+    return maxSumRec(a, 0, a.size()-1);
+}
+
+int max3(int x, int y, int z)
+{
+    int max = x;
+    if (max > y)
+        return y;
+    else if (max > z)
+        return z;
+    else
+        return max;
+}
+
+int main()
+{
+    cout << "HAHA" << endl;
+}
+//int maxSumRec()
