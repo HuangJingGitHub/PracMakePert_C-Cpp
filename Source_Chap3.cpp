836ee2796500710620e8b9cf45d235d853db59df
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-02-27 22:57:20 +0800

    Add files via upload

---
+++ Source_Chap3.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+#include <vector>
+#include "Source_Chap3.h"
+//using namespace std;
+
+int main()
+{
+	vector<int> a(10, 100);
+	print(a);
+	class Vector<int> V1(10);
+	print(V1);
+}
