12baa89de18298866e70600b5eee4c5b19f54d56
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-08-07 22:56:39 +0800

    Update MedianOfTwoSortedArrays.cpp

--- leetcode/two_sum/MedianOfTwoSortedArrays.cpp
+++ leetcode/two_sum/MedianOfTwoSortedArrays.cpp
@@ -8,8 +8,6 @@ public:
 	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 		nums1.insert(nums1.end(), nums2.begin(), nums2.end());
 		sort(nums1.begin(), nums1.end());
-		//nums1.erase(unique(nums1.begin(), nums1.end()), nums1.end());
-		//for (int i = 0; i < nums1.size(); i++) cout << nums1[i] << " ";
 		if (nums1.size() % 2 == 0)
 			return (nums1[nums1.size() / 2] + nums1[nums1.size() / 2 - 1]) / 2.0;
 		else
