c24ed7629d788f26c8461bd993bfd5bbd8f68d4f
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-12-19 22:07:44 +0800

    Create leetcode_19.cpp

---
+++ leetcode/two_sum/leetcode_19.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy = ListNode(0);
+        dummy.next = head;
+        int length = 0;
+        ListNode* first = head;
+        while (first != NULL)
+        {
+            length++;
+            first = (*first).next;
+        }
+        length -= n;
+        first = &dummy;
+        while (length > 0)
+        {
+            length--;
+            first = (*first).next;
+        }
+        (*first).next = (*(*first).next).next;
+        return dummy.next;
+    }
+};
