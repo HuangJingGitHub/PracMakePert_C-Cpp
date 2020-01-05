29288d3784eecc7ce90eb4bf3a7749c32a3588c8
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-01-05 14:33:38 +0800

    Update leetcode_24.cpp

--- leetcode/two_sum/leetcode_24.cpp
+++ leetcode/two_sum/leetcode_24.cpp
@@ -25,3 +25,35 @@ public:
         return originalHead;      
     }
 };
+// (1) The solution above is problematic. The reason for the problem is the pointer manipulation. Once a pointer points to some address, it 
+// will always points to this physical address no matter what following operations are performed via another pointer pointing to the   
+// same physical address. Pay attention to this. (2) The other thing is the whole solution is not good. Simpler solution is feasible. No
+// need to deal with pointer. Just swap the valule of the node is OK.
+
+
+// Follwing solution is good and simple.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode *originalHead = head;
+        for (int swapVal; head != NULL && head->next != NULL;)
+        {
+            swapVal = head->val;
+            head->val = head->next->val;
+            head->next->val = swapVal;
+            head = head->next->next;
+        }
+        return originalHead;
+    }
+};
