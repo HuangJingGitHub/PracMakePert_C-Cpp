95faadd929f7e6064a991d3343f40f5f8611b510
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-01-05 22:44:48 +0800

    Update leetcode_24.cpp

--- leetcode/two_sum/leetcode_24.cpp
+++ leetcode/two_sum/leetcode_24.cpp
@@ -89,3 +89,32 @@ public:
         return dummy->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *point = dummy;
+        while (point->next != NULL && point->next->next != NULL){
+            ListNode swap1 = *(point->next), swap2 = *(point->next->next);
+            point->next = &swap2; 
+            //cout << "test " << dummy->next->val << " " << point.next->val << endl;
+            swap1.next = swap2.next; 
+            swap2.next = &swap1; 
+            point = &swap1;
+        }
+        return dummy->next;
+    }
+};
