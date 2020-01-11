8df68f32c05cce1f68493676607df8f350059678
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-01-11 13:36:39 +0800

    Update leetcode_30.cpp

--- leetcode/two_sum/leetcode_30.cpp
+++ leetcode/two_sum/leetcode_30.cpp
@@ -1,3 +1,5 @@
+// This solution is feasible. But the speed is slow for the manipulation of the vector like copy and erase. To deal with this hashmap
+// is better.
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
@@ -12,26 +14,19 @@ public:
             return ret;
 
         for (int i = 0, j = 0; i + charNum <= s.size(); i++){
-            string firstStr = s.substr(i, wordLength), lastStr = s.substr(i+charNum-wordLength, wordLength);
+            string firstStr = s.substr(i, wordLength);
             vector<string> currentWords = words;
-            if (words.size() == 1){
-                if (!isInWords(firstStr, currentWords))
-                    continue;
-            }
-            else if (!isInWords(firstStr, currentWords) || !isInWords(lastStr, currentWords))
-                continue;
-            else{
-                for (j = i + wordLength; j < i + charNum - wordLength; j += wordLength){
+                for (j = i; j <= i + charNum - wordLength; j += wordLength){
                     string currentStr = s.substr(j, wordLength);
                     if (!isInWords(currentStr, currentWords))
                         break;
                 }
-            }
             if ( currentWords.size() == 0)
                 ret.push_back(i);
         }
-        return ret;
-    }
+        return ret;        
+        }
+
         bool isInWords(string s, vector<string>& words)
         {
             for (int i = 0; i < words.size(); i++)
@@ -41,5 +36,40 @@ public:
                 }
             return false;
         }     
+};
 
+// This solution can pass the test with the usage of hashmap to deal with the words check. But the speed is not optimized to the best.
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> ret;
+
+        if (words.size() == 0)
+            return ret;
+
+        int wordNum = words.size(), wordLength = words[0].size(), charNum = words.size() * words[0].size();
+
+        if (s.size() < charNum)
+            return ret;
+        unordered_map<string, int> hmap1, hmap2;
+        for (int i = 0; i < wordNum; i++)
+            hmap1[words[i]]++;
+
+        for (int i = 0, j = 0; i + charNum <= s.size(); i++){
+            for(j = i; j <= i + charNum - wordLength; j += wordLength){
+                    string currentStr = s.substr(j, wordLength);
+                    if (hmap1[currentStr] == 0)
+                        break;
+                    else{
+                        hmap2[currentStr]++;
+                        if (hmap1[currentStr] < hmap2[currentStr])
+                            break;
+                    }
+                }
+            if (j == i + charNum)
+                ret.push_back(i);
+            hmap2.clear();
+        }
+        return ret;
+        } 
 };
