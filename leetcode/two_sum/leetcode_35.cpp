c89633b901806f8a6e60631a47d506da84ab5f20
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-01-19 23:15:05 +0800

    Update leetcode_35.cpp

--- leetcode/two_sum/leetcode_35.cpp
+++ leetcode/two_sum/leetcode_35.cpp
@@ -3,7 +3,7 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         if (nums.size() == 0 || nums[0] >= target)
             return 0;
-        if (nums[nums.size()-1] == target)      // Just pay attention to the details: == and < different indices should be given.
+        if (nums[nums.size()-1] == target)      // Just pay attention to the details: == and <, different indices should be given.
             return nums.size() - 1;            
         if (nums[nums.size()-1] < target)
             return nums.size();
