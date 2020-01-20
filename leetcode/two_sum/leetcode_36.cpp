f867d5f2bf21b77381ee3b4e4c1cbe56b7dd3f21
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-01-20 14:38:34 +0800

    Create leetcode_36.cpp

---
+++ leetcode/two_sum/leetcode_36.cpp
@@ -0,0 +1,28 @@
+// The solution use the idea of the offical solution, which is good. And some details in the C++ implementation is noteworthy.  
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<std::map<int, int>> rowLog(9), columnLog(9), subboxLog(9);   // The vector size must be declared first befor following indexing.
+
+        int digit, boxIndex;
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++)
+            {
+                if (board[i][j] == '.')
+                    continue;
+                else 
+                {
+                    digit =  board[i][j] - '0';     // Pay attention how to convert char number to int. Not use std::stoi() here as the
+                    boxIndex = i / 3 + 3 * (j / 3); // char here is in a vector. std::stoi() need passing of address.
+                    rowLog[i][digit]++;
+                    columnLog[j][digit]++;
+                    subboxLog[boxIndex][digit]++;
+                }
+
+                if (rowLog[i][digit] > 1 || columnLog[j][digit] > 1 || 
+                    subboxLog[boxIndex][digit] > 1)
+                    return false;
+            }
+        return true;  
+    }
+};
