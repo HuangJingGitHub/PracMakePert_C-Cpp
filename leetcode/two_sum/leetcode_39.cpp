4ada0f0db781450c70bc9c3ee71f937f15fb3a40
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-01-26 21:02:12 +0800

    Create leetcode_39.cpp

---
+++ leetcode/two_sum/leetcode_39.cpp
@@ -0,0 +1,28 @@
+// See the reference solution for backtrack method.
+class Solution {
+private:
+    vector<int> candidates;
+    vector<vector<int>> res;
+    vector<int> path;
+
+public:
+    void backtrack(int start, int target){
+        if (target == 0){
+            res.push_back(path);
+            return;
+        }
+        for (int i = start; i < candidates.size() && target - candidates[i] >= 0; i++){
+            path.push_back(candidates[i]);
+            backtrack(i, target - candidates[i]);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        this->candidates = candidates;
+        backtrack(0, target);
+
+        return res;
+    }
+};
