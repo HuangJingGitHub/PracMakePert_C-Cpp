c40da47569dc8ed9b0f8ea9a7c1e96f51a78ecfd
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-01-29 21:41:19 +0800

    Update leetcode_41.cpp

--- leetcode/two_sum/leetcode_41.cpp
+++ leetcode/two_sum/leetcode_41.cpp
@@ -1,13 +1,22 @@
+// Interesting problem, refer to the solutions and discussions on leetcode.
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int smallestPositiveInt = pow(2, 32);
         for (int i = 0; i < nums.size(); i++){
-            if (nums[i] <= 0)
-                continue;
-            if (nums[i] == smallestPositiveInt + 1)  
-
+            while (nums[i] != i+1){
+                if (nums[i] <= 0 || nums[i] > nums.size() || nums[i] == nums[nums[i] - 1])
+                    break;
+                int idx = nums[i] - 1;
+                nums[i] = nums[idx];
+                nums[idx] = idx + 1;
+            }
         }
         
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] != i+1)
+                return i+1;
+        }
+
+        return nums.size() + 1;
     }
 };
