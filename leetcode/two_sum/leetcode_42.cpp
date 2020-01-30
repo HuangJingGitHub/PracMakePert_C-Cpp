9576c6db68b4d5f7cc1f23b3725d67cc5d3380c6
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-01-30 23:36:20 +0800

    Update leetcode_42.cpp

--- leetcode/two_sum/leetcode_42.cpp
+++ leetcode/two_sum/leetcode_42.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left, right, vol = 0, volSum = 0;
-        if (height.size() <= 2)
-            return 0;
-        
-        for(left = 0; left < height.size() && height[left] == 0; left++);
-        if (left == height.size() || left == height.size() - 1)
+        if ( height.size() < 3)
             return 0;
 
-        for (; left < height.size() - 2; ){
-            if (height[left+1] >= height[left]){
-                left++;
-                continue;
-            }
-            else{
-                right = left + 2;
-                for (vol = height[left] - height[left-1]; right < height.size() &&
-                 height[right] < height[left]; right++){
-                     vol += height[left] - height[right];
-                 }
-                 if (right == height.size())
-                    break;
-                volSum += vol;
-                left = right;
-            }
+        int volSum = 0;
+        vector<int> max_left(height.size()), max_right(height.size());
+
+        for (int i = 1; i < height.size(); i++){
+            max_left[i] = max(height[i-1], max_left[i-1]);
+        }
+        for(int i = height.size() - 2; i >= 0; i--){
+            max_right[i] = max(height[i+1], max_right[i+1]);
+        }
+
+        for (int i = 1; i < height.size() - 1; i++){
+            int minHeight = min(max_left[i], max_right[i]);
+            if (minHeight > height[i])
+                volSum += minHeight - height[i];
         }
         return volSum;
     }  
