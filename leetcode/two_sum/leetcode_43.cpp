11b77a95b608d8a2da20c2c3b18b419157035c70
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-02-01 22:44:22 +0800

    Update leetcode_43.cpp

--- leetcode/two_sum/leetcode_43.cpp
+++ leetcode/two_sum/leetcode_43.cpp
@@ -1,3 +1,6 @@
+// This is the personal solution which misunderstands the problem as the problem wants to solve the very important problem
+// of multiplicaiton of very large numbers that cannot be performed by int muplicaiton in computer. That is to implement the
+// vertical multiplication which can computate multiplication of really large numbers, like huands, thousands of digits like human do.
 class Solution {
 public:
     string multiply(string num1, string num2) {
@@ -28,5 +31,28 @@ public:
             digits--;
         }    
        return ret; 
+    }
+};
+
+// Very elegant solution. Refer to the offical discussion page.
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int n1 = num1.size(), n2 = num2.size();
+        string  res(n1+n2, '0');
+
+        for (int i = n2 - 1; i >= 0; i--){
+            for (int j = n1 - 1; j >= 0; j--){
+                int temp = (res[i+j+1] - '0') + (num1[j] - '0') * (num2[i] - '0');
+                res[i+j+1] = temp % 10 + '0';
+                res[i+j] += temp / 10;
+            }
+        }
+        
+        for (int i = 0; i < res.size(); i++){
+            if (res[i] != '0')
+                return res.substr(i);
+        }
+        return "0";
     }
 };
