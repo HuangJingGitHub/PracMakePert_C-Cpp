d93ca5bc6f3135b099e633dbfa3b0b11cc81df61
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-02-02 23:41:52 +0800

    Delete leetcode_27.cpp

--- leetcode_27.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        int newLength = 0;
-        for (int i = 0; i < nums.size(); i++)
-         if (nums[i] != val) nums[newLength++] = nums[i];  
-        return newLength;
-    }
-};
