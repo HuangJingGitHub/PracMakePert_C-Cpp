3934f3030940f1dc2e453f277afaf28a81fbd24e
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-02-03 23:56:34 +0800

    Create leetcode_44.cpp

---
+++ leetcode/two_sum/leetcode_44.cpp
@@ -0,0 +1,26 @@
+// See with Problem 10. Used solution from discussion page.
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int i = 0, j =  0, iStar = -1, jStar = -1, m = s.size(), n = p.size();
+        while (i < m){
+            if (j < n && (s[i] == p[j] || p[j] == '?')){
+                i++;
+                j++;
+            }
+            else if (j < n && p[j] == '*'){
+                iStar = i;
+                jStar = j++;
+            }
+            else if (iStar >= 0){
+                i = ++iStar;
+                j = jStar + 1;
+            }
+            else
+                return false;
+        }
+        while (j < n && p[j] == '*')
+            ++j;
+        return j == n;
+    }
+};
