8b4427c8fcab86b19f332c0bfa4f942656315ffb
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-02-04 22:19:16 +0800

    Update leetcode_45.cpp

--- leetcode/two_sum/leetcode_45.cpp
+++ leetcode/two_sum/leetcode_45.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int jumpSum = 0, currentPos, nextPos, maxJump;
-        for (currentPos = 0; currentPos < nums.size()-1; ){
+        for (currentPos = 0; currentPos + nums[currentPos] < nums.size(); ){
             maxJump = nums[currentPos];
             jumpSum++;
-            for (int i = 2, nextPos = currentPos + 1; i <= maxJump; i++){
+            for (int i = 2, nextPos = currentPos + 1; i <= maxJump && currentPos + i < nums.size(); i++){
                 nextPos = (i + nums[currentPos + i]) > (nextPos - currentPos + nums[nextPos]) ? (currentPos + i) : nextPos;
                 if ((nextPos + nums[nextPos]) >= nums.size())
                     return jumpSum;
             }
             currentPos = nextPos;
-            cout << currentPos << " ";
         }
         return jumpSum;
     }
