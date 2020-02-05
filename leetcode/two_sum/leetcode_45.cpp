b20498b1d4e8cda0b014bdf39fd21ac7c6aaa74f
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-02-05 23:50:28 +0800

    Update leetcode_45.cpp

--- leetcode/two_sum/leetcode_45.cpp
+++ leetcode/two_sum/leetcode_45.cpp
@@ -1,16 +1,20 @@
+// greedy action
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int jumpSum = 0, currentPos, nextPos, maxJump;
-        for (currentPos = 0; currentPos + nums[currentPos] < nums.size(); ){
+        if (nums.size() == 1)
+            return 0;
+
+        int jumpSum = 1, currentPos, nextPos, maxJump;
+        for (currentPos = 0; currentPos + nums[currentPos] < nums.size() - 1; currentPos = nextPos){
             maxJump = nums[currentPos];
-            jumpSum++;
-            for (int i = 2, nextPos = currentPos + 1; i <= maxJump && currentPos + i < nums.size(); i++){
+            nextPos = currentPos + 1;
+            for (int i = 2; i <= maxJump; i++){
                 nextPos = (i + nums[currentPos + i]) > (nextPos - currentPos + nums[nextPos]) ? (currentPos + i) : nextPos;
-                if ((nextPos + nums[nextPos]) >= nums.size())
-                    return jumpSum;
+                if ((nextPos + nums[nextPos]) >= nums.size() - 1)
+                    return ++jumpSum;
             }
-            currentPos = nextPos;
+            jumpSum++;
         }
         return jumpSum;
     }
