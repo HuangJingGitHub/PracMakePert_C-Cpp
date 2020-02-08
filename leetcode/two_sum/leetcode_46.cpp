8a6faa723b2e960e60bcb3ee29afc9677cf10aca
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-02-08 22:12:59 +0800

    Update leetcode_46.cpp

--- leetcode/two_sum/leetcode_46.cpp
+++ leetcode/two_sum/leetcode_46.cpp
@@ -28,3 +28,45 @@ public:
         return res;
     }
 };
+
+
+// This is the backtracking algorithm. See the discussion for details.
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int len = nums.size();
+        vector<vector<int>> res;
+
+        if (len == 0 || len == 1)
+        {
+            res.push_back(nums);
+            return res;
+        }
+
+        vector<bool> used(len, false);
+        vector<int> path;
+
+        dfs(nums, len, 0, path, used, res);
+        return res;
+    }
+
+private:
+    void dfs(vector<int> nums, int len, int depth, vector<int> path, vector<bool> used, 
+            vector<vector<int>>& res){
+            if (depth == len){
+                res.push_back(path);
+                return;
+            }
+
+            for (int i = 0; i < len; i++){
+                if (!used[i]){
+                    path.push_back(nums[i]);
+                    used[i] = true;
+
+                    dfs(nums, len, depth + 1, path, used, res);
+                    used[i] = false;
+                    path.pop_back();
+                }
+            }
+        }        
+};
