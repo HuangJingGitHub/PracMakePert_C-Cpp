d698065a26b5df09bde47dbb863aa904e6475013
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-02-25 21:16:21 +0800

    Update leetcode_54.cpp

--- leetcode/two_sum/leetcode_54.cpp
+++ leetcode/two_sum/leetcode_54.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> res;
-        if (matrix.size() == 0 || matrix[0].size() == 0)
-            return res;
-        int row = matrix.size(), col = matrix[0].size(), counter = 0, i = 0, j = 0;
-        while (counter < row * col){
-            res.push_back(matrix[i][j]);
-            
+        if (matrix.size() == 0 || matrix[0].size() == 0){
+            vector<int> res1;
+            return res1;
         }
-
+            
+        int row = matrix.size(), col = matrix[0].size(), counter = 0, startRow = 0, startCol = 0, i, j;
+        int length = row * col;
+        vector<int> res(length);
+        while (counter < length){
+            for(i = startRow, j = startCol; j < col - startCol; j++){
+                // res.push_back(matrix[i][j]);
+                // counter++;
+                res[counter++] = matrix[i][j];
+            }
+            if (counter == length)
+                break;
+            for(i = startRow + 1, j = col - startCol - 1; i < row - startRow; i++){
+                res[counter++] = matrix[i][j];
+            }
+            if (counter == length)
+                break;
+            for(i = row - startRow - 1, j = col - startCol - 2; j >= startCol; j--){
+                res[counter++] = matrix[i][j];
+            }  
+            if (counter == length)
+                break;            
+            for(i = row - startRow - 2, j = startCol; i > startCol; i--){
+                res[counter++] = matrix[i][j];
+            } 
+            if (counter == length)
+                break;            
+            startRow++;
+            startCol++;           
+        }
+        return res;
     }
 };
