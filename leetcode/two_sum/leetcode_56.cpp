7f23a2ff9496481fe23790b3d4e96094c04a31b8
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-03-05 23:43:07 +0800

    Create leetcode_56.cpp

---
+++ leetcode/two_sum/leetcode_56.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<vector<int>> res;
+        if (n == 0 || n == 1)
+            return intervals;
+        vector<int> curIntval = intervals[0];
+
+        for(int i = 0; i < n - 1; i++){
+            if (curIntval[1] < intervals[i+1][0] || curIntval[0] > intervals[i+1][1]){
+                res.push_back(curIntval);
+                curIntval = intervals[i+1];
+                continue;
+            }
+            else{ 
+                curIntval[0] = min(curIntval[0], intervals[i+1][0]);
+                curIntval[1] = max(curIntval[1], intervals[i+1][1]);
+            }
+        }
+        res.push_back(curIntval);
+        return res;
+    }
+};
