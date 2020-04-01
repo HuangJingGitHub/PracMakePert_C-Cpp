8c4e4b0819a11da065a1ec4171db261f78f35c88
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-04-01 23:21:03 +0800

    Update leetcode_66.cpp

--- leetcode/two_sum/leetcode_66.cpp
+++ leetcode/two_sum/leetcode_66.cpp
@@ -9,9 +9,8 @@ public:
             for (;i >= 0 && digits[i] == 9; i--){
                 digits[i] = 0;
             }
-            if (i>=0){
+            if (i >= 0)
                 digits[i] += 1;
-            }
             else 
                 digits.insert(digits.begin(), 1);
         }
