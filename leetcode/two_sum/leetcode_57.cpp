9535471a6a862d35d212f434e982c2c768987ad4
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-05-14 23:05:21 +0800

    Delete leetcode_57.cpp

--- leetcode/two_sum/leetcode_57.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n = intervals.size(), startPos = 0, endPos = 0;
-        for (int i = 0; i + 1 < n; i++){
-            
-        }
-    }
-};
