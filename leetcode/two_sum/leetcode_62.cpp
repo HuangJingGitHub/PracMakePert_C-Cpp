d3c45fe8d1b75798d0587d176ec79c2008dd53bf
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-05-22 22:34:38 +0800

    Create leetcode_62.cpp

---
+++ leetcode/two_sum/leetcode_62.cpp
@@ -0,0 +1,20 @@
+// Using permutation and combination the result is C_{m+n-2}^{m-1}
+class Solution {
+public:
+    // Recursion can be very slow
+    int uniquePathsSelf(int m, int n) {
+        if (m == 1 || n == 1)
+            return 1;
+        else
+            return uniquePathsSelf(m, n-1) + uniquePathsSelf(m-1, n);    
+    }
+    // Interesting DP
+    int uniquePaths(int m, int n) {
+    vector<int> dp(m,0);
+    dp[0] = 1;
+    for(int i  =0; i < n; i++)
+        for(int j = 1; j < m; j++)
+            dp[j] +=dp [j-1];
+    return dp[m-1];
+    }
+};
