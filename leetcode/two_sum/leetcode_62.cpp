309f294b01ec7b0228eaffccca1b8c1e4250b362
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-05-24 21:19:17 +0800

    Update leetcode_62.cpp

--- leetcode/two_sum/leetcode_62.cpp
+++ leetcode/two_sum/leetcode_62.cpp
@@ -12,9 +12,9 @@ public:
     int uniquePaths(int m, int n) {
     vector<int> dp(m,0);
     dp[0] = 1;
-    for(int i  =0; i < n; i++)
+    for(int i  = 0; i < n; i++)
         for(int j = 1; j < m; j++)
-            dp[j] +=dp [j-1];
+            dp[j] += dp[j-1];
     return dp[m-1];
     }
 };
