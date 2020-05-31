8e6e4705682c9589de1165ac5c431efcc138be2d
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-05-31 23:41:12 +0800

    Create leetcode_71.cpp

---
+++ leetcode/two_sum/leetcode_71.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<char> pathStack;
+
+        pathStack.push_back(path[0]);
+        for (int i = 1; i < path.size(); i++){
+            
+        }
+    }
+};
