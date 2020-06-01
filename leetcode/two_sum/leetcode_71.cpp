f6d474a1b35d28fae5c0c91c1bc3abb987174646
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-06-01 23:55:00 +0800

    Update leetcode_71.cpp

--- leetcode/two_sum/leetcode_71.cpp
+++ leetcode/two_sum/leetcode_71.cpp
@@ -5,6 +5,21 @@ public:
 
         pathStack.push_back(path[0]);
         for (int i = 1; i < path.size(); i++){
+            if (path[i] == '.'){
+                if (pathStack.back() == '.'){
+                    pathStack.push_back();
+                    pathStack.push_back();
+                    pathStack.push_back();
+                }
+                else
+                    continue;
+            }
+            if (path[i] == '/'){
+                if (pathStack.back() == '/')
+                    continue;
+                else
+                    pathStack.push_back('/');
+            }
             
         }
     }
