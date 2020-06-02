8cb4527e17704daf815192a80df2240a4042847b
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2020-06-02 10:50:06 +0800

    Update leetcode_71.cpp

--- leetcode/two_sum/leetcode_71.cpp
+++ leetcode/two_sum/leetcode_71.cpp
@@ -1,26 +1,24 @@
+// Use stringstream and getline() to split the path by "/".
 class Solution {
 public:
     string simplifyPath(string path) {
-        vector<char> pathStack;
+        stringstream ss(path);
+        vector<string> strs;
+        string res, tmp;
 
-        pathStack.push_back(path[0]);
-        for (int i = 1; i < path.size(); i++){
-            if (path[i] == '.'){
-                if (pathStack.back() == '.'){
-                    pathStack.push_back();
-                    pathStack.push_back();
-                    pathStack.push_back();
-                }
-                else
-                    continue;
-            }
-            if (path[i] == '/'){
-                if (pathStack.back() == '/')
-                    continue;
-                else
-                    pathStack.push_back('/');
-            }
-            
+        while(getline(ss, tmp, '/')){
+            if (tmp == "" || tmp == ".")
+                continue;
+            else if (tmp == ".." && !strs.empty())
+                strs.pop_back();
+            else if (tmp != "..")
+                strs.push_back(tmp);
         }
+
+        for (string str:strs)
+            res += "/" + str;
+        if (res.empty())
+            return "/";
+        return res;
     }
 };
