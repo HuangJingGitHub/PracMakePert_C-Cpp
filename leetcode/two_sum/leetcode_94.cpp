f933037d5e60e75ba058ce7ef770db683c7b6d05
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-07-09 22:49:25 +0800

    Create leetcode_94.cpp

---
+++ leetcode/two_sum/leetcode_94.cpp
@@ -0,0 +1,25 @@
+// Very classical algorithm for binary tree traversal using recursion.
+// inorder: left root right
+// preorder: root left right
+// postorder: left right root
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> res;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (root){
+            inorderTraversal(root->left);
+            res.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return res;
+    }
+};
