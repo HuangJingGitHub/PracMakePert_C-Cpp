fa512c5186b344f5eaf8b21f35754ce96243498a
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-07-14 23:51:36 +0800

    Create leetcode_98.cpp

---
+++ leetcode/two_sum/leetcode_98.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ // Inorder traversal
+class Solution {
+public:
+    long long top = LLONG_MIN;    // smaller than int minimum INT_MIN
+    bool isValid = true;
+    bool isValidBST(TreeNode* root) {
+        if (root){
+            isValidBST(root->left);
+            if (root->val <= top)
+                isValid = false;
+            else{
+                top = root->val;
+                isValidBST(root->right);
+            }
+        }
+        return isValid;
+    }
+};
