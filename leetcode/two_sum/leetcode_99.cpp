3c53efff87cca6002974b8aef44d257b432cb838
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-07-15 23:12:36 +0800

    Create leetcode_99.cpp

---
+++ leetcode/two_sum/leetcode_99.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* first = NULL;
+    TreeNode* second = NULL;
+    TreeNode* pre = NULL;
+    void recoverTree(TreeNode* root) {
+        inorderTraversal(root);
+        int tmp = first->val;
+        first->val = second->val;
+        second->val = tmp;
+    }
+
+    void inorderTraversal(TreeNode* root)
+    {
+        if (root == NULL)
+            return;
+
+        inorderTraversal(root->left);
+        if (pre != NULL && pre->val > root->val){
+            if (first == NULL){     // The swap can occur between two adjacent values or not adjacent values.
+                first = pre;
+                second = root;
+            }
+            else
+                second = root;
+        }
+        pre = root;
+        inorderTraversal(root->right);
+
+    }
+};
