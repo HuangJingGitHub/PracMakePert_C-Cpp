165b930393210880fb6f25947f35a2d53efcbf33
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-07-15 23:09:12 +0800

    Create leetcode_100.cpp

---
+++ leetcode/two_sum/leetcode_100.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool res = true;
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q != NULL)
+            return false;
+        else if (p != NULL && q == NULL)
+            return false;
+    
+        if (p && q){
+            isSameTree(p->left, q->left);
+            if (p->val != q->val 
+                || (p->left == NULL && q->left != NULL) || (p->left != NULL && q->left == NULL) 
+                || (p->right == NULL && q->right != NULL) || (p->right != NULL && q->right == NULL))
+                res = false;
+            else
+                isSameTree(p->right, q->right);
+        }
+        return res;
+    }
+};
