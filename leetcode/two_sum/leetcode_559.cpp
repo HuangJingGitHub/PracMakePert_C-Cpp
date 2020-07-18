56076af18aa8955f7ccfb104818b713d0d051e5c
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-07-18 22:46:22 +0800

    Create leetcode_559.cpp

---
+++ leetcode/two_sum/leetcode_559.cpp
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        int depth = 0;
+        if (!root)
+            return 0;
+        for (Node* it : root->children){
+            if (it)
+                depth = max(depth, maxDepth(it));
+        }
+        depth++;
+        return depth;
+    }
+};
