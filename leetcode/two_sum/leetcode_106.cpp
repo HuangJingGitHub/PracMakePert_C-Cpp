fd8ca9e7ab37a1f90b92bf9ba1576818230969f9
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-07-24 22:14:32 +0800

    Create leetcode_106.cpp

---
+++ leetcode/two_sum/leetcode_106.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        return buildTreeHelper(postorder, 0, postorder.size(), inorder, 0, inorder.size());
+    }
+
+    TreeNode* buildTreeHelper(vector<int>& postorder, int pStart, int pEnd, vector<int>& inorder, int iStart, int iEnd)
+    {
+        if (pStart == pEnd)
+            return NULL;
+        
+        TreeNode* root = new TreeNode(postorder[pEnd-1]);
+
+        int inorderRootIdx = 0;
+        for (int i = iStart; i < iEnd; i++){
+            if (inorder[i] == postorder[pEnd-1]){
+                inorderRootIdx = i;
+                break;
+            }
+        }
+
+        int leftChildNum = inorderRootIdx - iStart;
+        root->left = buildTreeHelper(postorder, pStart, pStart+leftChildNum+1, inorder, iStart, iStart+leftChildNum+1);
+        root->right = buildTreeHelper(postorder, pStart+leftChildNum+1, pEnd-1, inorder, inorderRootIdx+1, iEnd);
+        return root;
+    }
+};
