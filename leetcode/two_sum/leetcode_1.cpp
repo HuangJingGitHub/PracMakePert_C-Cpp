ddc59a74d512af64722c82b60227f63b165e7a24
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-07-26 11:22:08 +0800

    Update leetcode_1.cpp

--- leetcode/two_sum/leetcode_1.cpp
+++ leetcode/two_sum/leetcode_1.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res;
         unordered_map<int, int> hashMap;
+        
         for (int i = 0; i < nums.size(); i++){
             if (hashMap.find(nums[i]) != hashMap.end()){
                 res.push_back(hashMap.find(nums[i])->second);
