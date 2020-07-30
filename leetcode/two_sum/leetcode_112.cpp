f0b23aebc15295c1a0b7a97435a0eaac65de4a4f
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-07-30 22:08:09 +0800

    Create leetcode_112.cpp

---
+++ leetcode/two_sum/leetcode_112.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == NULL)
+            return false;
+        else if (!root->left && !root->right)
+            return sum == root->val;
+
+        // else if (root->left && !root->right)  // The commented part is redundant. Keeping the last case is sufficient and effective.
+        //    return hasPathSum(root->left, sum - root->val);
+        // else if (!root->left && root->right)
+        //    return hasPathSum(root->right, sum - root->val);
+        else
+            return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+    }
+};
