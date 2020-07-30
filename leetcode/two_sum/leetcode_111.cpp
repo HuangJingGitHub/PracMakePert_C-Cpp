2a0e460c28108d48f4c8ec0ddfea82dc9a4c371a
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-07-30 22:08:48 +0800

    Create leetcode_111.cpp

---
+++ leetcode/two_sum/leetcode_111.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        else if (root->left && !root->right)
+            return minDepth(root->left) + 1;
+        else if (!root->left && root->right)
+            return minDepth(root->right) + 1;
+        else
+            return min(minDepth(root->left), minDepth(root->right)) + 1;
+    }
+};
