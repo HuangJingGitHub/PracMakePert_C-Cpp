4542f438d2fd53537e34c33800405843ce4affb0
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-08-04 23:48:38 +0800

    Create leetcode_502.cpp

---
+++ leetcode/two_sum/leetcode_502.cpp
@@ -0,0 +1,27 @@
+// good practice on max_heap, min_heap
+class Solution {
+public:
+    int findMaximizedCapital(int k, int W, vector<int>& Profits, vector<int>& Capital) {
+        priority_queue<int> profitMaxHeap;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> capitalMinHeap;
+
+        int len = Capital.size();
+        for (int i = 0; i < len; i++){
+            capitalMinHeap.emplace(make_pair(Capital[i], Profits[i]));
+        }
+        // gready algorithm
+        for (int i = 0; i < k; i++){
+            while (!capitalMinHeap.empty() && capitalMinHeap.top().first <= W){
+                profitMaxHeap.emplace(capitalMinHeap.top().second);
+                capitalMinHeap.pop();
+            }
+
+            if (profitMaxHeap.empty())
+                break;
+            W += profitMaxHeap.top();
+            profitMaxHeap.pop();
+        }
+
+        return W;
+    }
+};
