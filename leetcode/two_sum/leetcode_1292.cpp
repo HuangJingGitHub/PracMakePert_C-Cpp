400d59ac33e615f320b4f0137b2a498e1090020e
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-08-08 23:55:48 +0800

    Update leetcode_1292.cpp

--- leetcode/two_sum/leetcode_1292.cpp
+++ leetcode/two_sum/leetcode_1292.cpp
@@ -4,22 +4,23 @@ public:
         if (mat.empty())
             return 0;
 
-        int integralSum = 0, res = 0, minDim = min(mat.size(), mat[0].size());
-        
-        for (int i = 0; i < minDim; i++){
-            updateSquareSum(mat, integralSum, i);
-            if (integralSum > threshold)
-                return i;
-        }
-        return minDim;
-    }
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> integralMat(m+1, vector<int>(n+1, 0));   // use m+1 x n+1 to avoid manual initialization of first row and first column.
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++){
+                integralMat[i][j] = mat[i-1][j-1] + integralMat[i-1][j] + integralMat[i][j-1] - integralMat[i-1][j-1];
+            }
 
-    void updateSquareSum(vector<vector<int>>& mat, int& integralSum, int idxLen)
-    {
-        for (int i = 0; i <= idxLen; i++){
-            integralSum += mat[idxLen][i];
-            integralSum += mat[i][idxLen];
-        }
-            integralSum -= mat[idxLen][idxLen];
+        int res = 0, maxLen = min(m, n);
+        for (int k = 1; k <= maxLen; k++)
+            for (int i = 1; i <= m; i++)
+                for (int j = 1; j <= n; j++){
+                    if (i - k < 0 || j - k < 0)
+                        continue;
+                    int temp = integralMat[i][j] - integralMat[i-k][j] - integralMat[i][j-k] + integralMat[i-k][j-k];
+                    if (temp <= threshold)
+                        res = max(res, k);
+                }
+        return res;
     }
 };
