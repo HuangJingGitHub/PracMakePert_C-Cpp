5dea0745c1baf9486a6c332daa8b6f1f0a6f4519
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-08-11 21:31:42 +0800

    Create leetcode_114.cpp

---
+++ leetcode/two_sum/leetcode_114.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* last = NULL;
+    void flatten(TreeNode* root) {
+        if (root == NULL)
+            return;
+        flatten(root->right);
+        flatten(root->left);
+        root->right = last;
+        root->left = NULL;
+        last = root;
+    }
+};
