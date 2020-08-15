c72025ad67cda079d1412888949f2b5f279b90b4
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-08-15 23:57:46 +0800

    Update leetcode_115.cpp

--- leetcode/two_sum/leetcode_115.cpp
+++ leetcode/two_sum/leetcode_115.cpp
@@ -11,3 +11,35 @@ public:
         return matched.back();
     }
 };
+
+
+// recursion with memeorization
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        map<string, int> interResMap;
+        return subDistinct(s, t, 0, 0, interResMap);
+    }
+
+    int subDistinct(string& s, string& t, int sStart, int tStart, map<string, int>& intermediateRes){
+        if (tStart == t.length())
+            return 1;
+        if (sStart == s.length())
+            return 0;
+
+        string key = to_string(sStart) + "_" + to_string(tStart);   // use to_string, a native way is to coast int to char, but that is NOT correct as sStart, tStart can easily exceed ASCII range!!!
+        map<string, int>::iterator it = intermediateRes.find(key);
+        if (it != intermediateRes.end()){
+            return it->second;
+        }
+
+        int res;
+        if (s[sStart] == t[tStart])
+            res = subDistinct(s, t, sStart+1, tStart+1, intermediateRes) + subDistinct(s, t, sStart+1, tStart, intermediateRes);
+        else
+            res = subDistinct(s, t, sStart+1, tStart, intermediateRes);
+
+        intermediateRes.emplace(key, res);
+        return res;
+    }
+};
