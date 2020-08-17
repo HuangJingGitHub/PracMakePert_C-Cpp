9aa6b866f97ac65cad67ac6d7fad0228fab2b0db
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-08-17 23:52:15 +0800

    Update leetcode_115.cpp

--- leetcode/two_sum/leetcode_115.cpp
+++ leetcode/two_sum/leetcode_115.cpp
@@ -1,18 +1,3 @@
-class Solution {
-public:
-    int numDistinct(string s, string t) {
-        vector<long long int> matched(t.size() + 1, 0);
-        matched[0] = 1;
-        for (int i = 0; i < s.size(); i++)
-            for (int j = t.size(); j > 0; j--){
-                if (s[i] == t[j-1])
-                    matched[j] += matched[j-1];
-            }
-        return matched.back();
-    }
-};
-
-
 // recursion with memeorization
 class Solution {
 public:
@@ -41,5 +26,42 @@ public:
 
         intermediateRes.emplace(key, res);
         return res;
+    }
+};
+
+
+// 2D dp solution, refer to the solution
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int sLen = s.size(), tLen = t.size();
+        vector<vector<long long int>> dp(sLen+1, vector<long long int>(tLen+1, 0));  // actually, int will result in overflow when ruing tests.
+
+        for (int i = 0; i < sLen+1; i++)
+            dp[i][tLen] = 1;
+        
+        for (int n = tLen-1; n >= 0; n--)
+            for (int m = sLen-1; m >= 0; m--){
+                if (s[m] == t[n])
+                    dp[m][n] = dp[m+1][n+1] + dp[m+1][n];
+                else
+                    dp[m][n] = dp[m+1][n];
+            }
+        return dp[0][0];
+    }
+};
+
+// 1D dp solution
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        vector<long long int> matched(t.size() + 1, 0);
+        matched[0] = 1;
+        for (int i = 0; i < s.size(); i++)
+            for (int j = t.size(); j > 0; j--){
+                if (s[i] == t[j-1])
+                    matched[j] += matched[j-1];
+            }
+        return matched.back();
     }
 };
