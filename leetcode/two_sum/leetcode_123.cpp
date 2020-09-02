2fac2805a0b39af7d4359c09a7ae15b7493baecf
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-09-02 22:29:46 +0800

    Update leetcode_123.cpp

--- leetcode/two_sum/leetcode_123.cpp
+++ leetcode/two_sum/leetcode_123.cpp
@@ -1,3 +1,4 @@
+// will exceed the time limit.
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -11,6 +12,7 @@ public:
                 continue;
             }
             res = max(res, maxProfitDay(prices, i));
+            currentLowest = prices[i];
         }
         return res;
     }
@@ -34,5 +36,23 @@ public:
             lowestProfit2 = min(lowestProfit2, prices[i]);
         }
         return res1 + res2;
+    }
+};
+
+// state machine method, very cool
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2)
+            return 0;
+        int buy1 = -prices[0], sell1 = 0, buy2 = -prices[0], sell2 = 0;  // represpent the moneny after ith purchase or sale
+
+        for (int i = 1; i < prices.size(); i++){
+            buy1 = max(buy1, -prices[i]);
+            sell1 = max(sell1, buy1 + prices[i]);
+            buy2 = max(buy2, sell1 - prices[i]);
+            sell2 = max(sell2, buy2 + prices[i]);
+        }
+        return max(0, sell2);
     }
 };
