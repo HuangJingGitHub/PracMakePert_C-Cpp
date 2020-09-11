c8767687b74a543105efe36974e7add038d0d66e
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-09-12 00:07:46 +0800

    Update leetcode_828.cpp

--- leetcode/two_sum/leetcode_828.cpp
+++ leetcode/two_sum/leetcode_828.cpp
@@ -1,10 +1,12 @@
+// understand the solution ---> what is unique in a substring, i.e., appear only once. Refer to the solution sector.
+// It is an O(n) solution
 class Solution {
 public:
     int uniqueLetterString(string s) {
         long long res = 0;
         int len = s.size();
         vector<int> leftIdx(len, -1), rightIdx(len, len);
-        map<char, int> idxDic;
+        map<char, int> idxDic;  // use hash to recond previous (next) idential letter index
         
         for (auto ch:s)
             idxDic[ch] = -1;
@@ -20,9 +22,8 @@ public:
             idxDic[s[i]] = i;
         }
 
-        for (int i = 0; i < len; i++){
-            res += (i - leftIdx[i])*(rightIdx[i] - i - 1);
-        }
+        for (int i = 0; i < len; i++)
+            res += (i - leftIdx[i])*(rightIdx[i] - i);
 
         return res % 1000000007;
 
