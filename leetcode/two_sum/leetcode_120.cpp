4128427e8cd21a5bc41218cdf1f813234a43c1e5
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-09-19 11:48:03 +0800

    Update leetcode_120.cpp

--- leetcode/two_sum/leetcode_120.cpp
+++ leetcode/two_sum/leetcode_120.cpp
@@ -1,18 +1,18 @@
-\\ classical dp
+// classical dp
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
+        int height = triangle.size();
 
-        for(int i = 1; i < m; i++)
+        for(int i = 1; i < height; i++)
             triangle[i][0] += triangle[i-1][0];
 
-        for (int i = 1; i < m; i++){
+        for (int i = 1; i < height; i++){
             for (int j = 1; j < triangle[i].size()-1; j++){
                 triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j]);
             }
             triangle[i].back() += triangle[i-1].back();
         }
-        return *min_element(triangle[m-1].begin(), triangle[m-1].end());
+        return *min_element(triangle[height-1].begin(), triangle[height-1].end());
     }
 };
