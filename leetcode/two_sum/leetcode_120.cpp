bd054eff3d19a9e73b49fb11d589e8d5cbbe25d8
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-09-19 12:00:35 +0800

    Update leetcode_120.cpp

--- leetcode/two_sum/leetcode_120.cpp
+++ leetcode/two_sum/leetcode_120.cpp
@@ -4,13 +4,10 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int height = triangle.size();
 
-        for(int i = 1; i < height; i++)
-            triangle[i][0] += triangle[i-1][0];
-
         for (int i = 1; i < height; i++){
-            for (int j = 1; j < triangle[i].size()-1; j++){
+            triangle[i][0] += triangle[i-1][0];
+            for (int j = 1; j < triangle[i].size()-1; j++)
                 triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j]);
-            }
             triangle[i].back() += triangle[i-1].back();
         }
         return *min_element(triangle[height-1].begin(), triangle[height-1].end());
