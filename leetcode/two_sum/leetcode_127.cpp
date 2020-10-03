c44f5bf294570418d53fc5376806922f44afe918
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-10-03 23:59:14 +0800

    Update leetcode_127.cpp

--- leetcode/two_sum/leetcode_127.cpp
+++ leetcode/two_sum/leetcode_127.cpp
@@ -44,3 +44,56 @@ public:
         return 0;
     }
 };
+
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if (std::find(wordList.begin(), wordList.end(), endWord) == wordList.end() )
+            return 0;
+
+        vector<int> visited(wordList.size(), 0);
+        queue<vector<string>> pathQueue;
+        vector<string> path{beginWord};
+        pathQueue.push(path);
+
+        while (!pathQueue.empty()){
+            int queueSize = pathQueue.size();
+            vector<int> subVisitedIdx;
+
+            for (int i = 0; i < queueSize; i++){
+                path = pathQueue.front();
+                pathQueue.pop();
+                string curEndWord = path.back();
+                
+                for (int i = 0; i < wordList.size(); i++){
+                    if (visited[i] == 1)
+                        continue;
+
+                    if (checkTrans(curEndWord, wordList, i)){
+                        if (wordList[i] == endWord)
+                            return path.size() + 1;
+
+                        path.push_back(wordList[i]);
+                        pathQueue.push(path);
+                        subVisitedIdx.push_back(i);
+                    }
+
+                }
+            }
+            for (int i : subVisitedIdx)
+                visited[i] = 1;
+        }
+        return 0;
+    }
+
+    bool checkTrans(string &curWord, vector<string> &wordList, int wordIdx){
+        int diffCount = 0;
+        for (int i = 0; i < curWord.size(); i++){
+            if (curWord[i] != wordList[wordIdx][i])
+                diffCount++;
+        }
+
+        return diffCount == 1;
+    }
+};
