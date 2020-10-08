ceac52420b86d39b17858d04efe7af9043fb35bc
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-10-08 11:08:23 +0800

    Update leetcode_295.cpp

--- leetcode/two_sum/leetcode_295.cpp
+++ leetcode/two_sum/leetcode_295.cpp
@@ -1,28 +1,40 @@
+/** Main idea is to maintain a max heap for small half elements and a min heap for larger half elements. Deal with their amount relationship 
+ * Look the favorited solution for reference. */
 class MedianFinder {
 private:
+    int dataCount;
     priority_queue<int> leftMaxHeap;
     priority_queue<int, vector<int>, greater<int>> rightMinHeap;
-    int dataCount;
 
 public:
-    /** initialize your data structure here. */
     MedianFinder() {
         dataCount = 0;
     }
     
     void addNum(int num) {
         dataCount++;
-        if (dataCount % 2 != 0)
+        if ((dataCount & 1) == 1){   // Note that bitwise and & has a lower precedence than >, <, ==, <<, etc. So parentheses are needed.
             leftMaxHeap.push(num);
-        else
-            rightMinHeap.push(num);
+            if (!rightMinHeap.empty()){
+                leftMaxHeap.push(rightMinHeap.top());
+                rightMinHeap.pop();
+
+                rightMinHeap.push(leftMaxHeap.top());
+                leftMaxHeap.pop();
+            }
+        }
+        else{
+            leftMaxHeap.push(num);
+            rightMinHeap.push(leftMaxHeap.top());
+            leftMaxHeap.pop();
+        }
     }
     
     double findMedian() {
-        if (dataCount % 2 != 0)
+        if ((dataCount & 1) == 1)
             return leftMaxHeap.top();
         else
-            return leftMaxHeap.top() + (rightMinHeap.top() - leftMaxHeap.top()) / 2.0;
+            return leftMaxHeap.top() + (rightMinHeap.top() - leftMaxHeap.top()) / 2.0;  // pay attention return double value.
     }
 };
 
