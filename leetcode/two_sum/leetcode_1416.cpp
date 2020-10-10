f72aa5712f6e59d60151857b3d5990378ccf9575
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-10-10 11:27:06 +0800

    Update leetcode_1416.cpp

--- leetcode/two_sum/leetcode_1416.cpp
+++ leetcode/two_sum/leetcode_1416.cpp
@@ -1,7 +1,10 @@
+// use dp, but pay attention to the number range in this problem.
 class Solution {
-public:
+public: 
+    long long int MOD = 1000000007;
+
     int numberOfArrays(string s, int k) {
-        vector<int> dp(s.size(), 0);
+        vector<long long int> dp(s.size(), 0);
         if (stoi(s.substr(0, 1)) >= 1 && stoi(s.substr(0, 1)) <= k)
             dp[0] = 1;
 
@@ -10,23 +13,23 @@ public:
             k1 /= 10;
             digits++;
         }
-        cout << digits << ' ';
 
-        int num;
+        long long int num;
         for (int i = 1; i < s.size(); i++){
-            for (int j = i - digits + 1; j >=1 && j <= i; j++){
+            for (int j = max(1, i - digits + 1); j <= i; j++){
                 if (s[j] == '0')
                     continue;
-                num = stoi(s.substr(j, i - j + 1));
+                num = stoll(s.substr(j, i - j + 1));
                 if (num >= 1 && num <= k)
                     dp[i] += dp[j-1];
             }
 
             if (i < digits && s[0] != '0'){
-                num = stoi(s.substr(0, i + 1));
+                num = stoll(s.substr(0, i + 1));
                 if (num >= 1 && num <= k)
                     dp[i]++;
             }
+            dp[i] %= MOD;
         }
 
         return dp.back();
