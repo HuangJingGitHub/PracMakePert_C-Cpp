5675112a5a57ecbaac86578ecf3dba6be573754e
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-10-29 21:42:01 +0800

    Update leetcode_1192.cpp

--- leetcode/two_sum/leetcode_1192.cpp
+++ leetcode/two_sum/leetcode_1192.cpp
@@ -1,50 +1,43 @@
+// Tarjar algorithm
 class Solution {
 public:
+    vector<vector<int>> res;
+    vector<vector<int>> adjacency;
+    vector<int> idx;
+    vector<int> depth; // Nodes in a circle are at the same depth.
+    vector<bool> visited;
+    int time = 1;
+
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<vector<int>> res;
-        set<int> criticalNode;
-        unordered_map<int, set<int>> adjacency;
+        adjacency = vector<vector<int>>(n);
+        idx = vector<int>(n, 0);
+        depth = vector<int>(n, 0);
+
         for (int i = 0; i < connections.size(); i++){
-            adjacency[connections[i][0]].insert(connections[i][1]);
-            adjacency[connections[i][1]].insert(connections[i][0]);
+            adjacency[connections[i][0]].push_back(connections[i][1]);
+            adjacency[connections[i][1]].push_back(connections[i][0]);
         }
 
-        int time = 1;
-        stack<int> nodeStack;
-        vector<int> idx(n, 0), depth(n, 0); // Nodes in a circle are at the same depth.
         for (int i = 0; i < n; i++){
             if (idx[i] == 0)
-                DFS(adjacency, nodeStack, criticalNode, idx, depth, time, i);
+                DFS(-1, i);
         }
 
-        for (vector<int> x : connections){
-            if (criticalNode.find(x[0]) != criticalNode.end() || criticalNode.find(x[1]) != criticalNode.end())
-                res.push_back(x);
-        }
         return res;
     }
 
-    void DFS(unordered_map<int, set<int>>& adjacency, stack<int>& nodeStack, set<int>& criticalNode, vector<int>& idx, vector<int>& depth, int& time, int i){
-        nodeStack.push(i);
+    void DFS(int parent, int i){
         idx[i] = time;
         depth[i] = time++;
-        for (int id = 0; id < idx.size(); id++)
-            cout << "(" << idx[id] << ", " << depth[id] << ") ";
-        cout << "\n";
 
-        for (set<int>::iterator it = adjacency[i].begin(); it != adjacency[i].end(); it++){
-            if (idx[*it] == 0)
-                DFS(adjacency, nodeStack, criticalNode, idx, depth, time, *it);
-            depth[i] = min(depth[i], depth[*it]);
+        for (int x : adjacency[i]){
+            if (idx[x] == 0)
+                DFS(i, x);
+            if (x != parent)
+                depth[i] = min(depth[i], depth[x]);
+            if (depth[x] > idx[i])
+                res.push_back({i, x});
         }
-        if (idx[i] == depth[i]){
-            cout << "i, stack.top() = " << i << ' ' << nodeStack.top() << endl;
-            nodeStack.pop();
-            if (nodeStack.empty() || depth[nodeStack.top()] != depth[i])
-                criticalNode.insert(i);
-            else
-                while (!nodeStack.empty() && depth[nodeStack.top()] == depth[i])
-                    nodeStack.pop();
-        }
+
     }
 };
