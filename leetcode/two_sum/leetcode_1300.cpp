08a91b43209b52f0ead9eae20a3fe2df6bdbce42
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-11-06 22:39:07 +0800

    Update leetcode_1300.cpp

--- leetcode/two_sum/leetcode_1300.cpp
+++ leetcode/two_sum/leetcode_1300.cpp
@@ -1,38 +1,25 @@
+  
+// sorting can help much.
 class Solution {
 public:
     int findBestValue(vector<int>& arr, int target) {
-        if (arr.empty())
-            return 0;
-        int res, avgTarget = target / arr.size();
-        if ((avgTarget + 1) * arr.size() - target < target - avgTarget * arr.size())  // best average can be avgTarget or avgTarget + 1. Or use round()
-            avgTarget++;
-
-        sort(arr.begin(), arr.end());
-        if (arr.front() >= avgTarget)
-            return avgTarget;
-        else if (arr.back() <= avgTarget)
-            return arr.back();
+        int res = 0;
         
-        int diff = 0, segPos = 0;
+        sort(arr.begin(), arr.end());
         for (int i = 0; i < arr.size(); i++){
-            if (arr[i] <= avgTarget)
-                diff += (arr[i] - avgTarget);
-            else{
-                segPos = i;
-                break;
-            }
-        }
-
-        cout << avgTarget << " -\n";
-        res = avgTarget;
-        for (int i = segPos; i < arr.size(); i++){
-            int addNum = (arr[i] - avgTarget) * (arr.size() - i);
-            if (abs(diff + addNum) <= abs(diff)){
-                diff += addNum;
+            int maxSum = arr[i] * (arr.size() - i);
+            if (maxSum < target){
+                target -= arr[i];
                 res = arr[i];
             }
-            else
-                break;
+            else if (maxSum == target)
+                return arr[i];
+            else{
+                int targetAvg = target / (arr.size() - i);
+                if (target - targetAvg * (arr.size() - i) > (targetAvg + 1) * (arr.size() - i) - target)
+                    targetAvg++;
+                return targetAvg;
+            }
         }
         return res;
     }
