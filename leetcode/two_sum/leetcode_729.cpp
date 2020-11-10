20f3e4a37be3d9f7bddcd13084350ef7a9db699a
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-11-10 17:53:16 +0800

    Update leetcode_729.cpp

--- leetcode/two_sum/leetcode_729.cpp
+++ leetcode/two_sum/leetcode_729.cpp
@@ -1,74 +1,24 @@
+// set keeps key order
+// use of lower_bound() vs manual binary search
 class MyCalendar {
 public:
-    set<int> bookStart;
-    set<int> bookEnd;
-    MyCalendar() { }
+    set<pair<int, int>> bookedLog;
+
+    // By inserting boundarying pairs, newly inserted {start, end}
+    // does not need to be dealt with specially if it appears beyond
+    // booked pair boundaries.
+    MyCalendar() {
+        bookedLog.insert({INT_MIN, INT_MIN});
+        bookedLog.insert({INT_MAX, INT_MAX});
+    }
     
     bool book(int start, int end) {
-        if (bookStart.empty()){
-            bookStart.insert(start);
-            bookEnd.insert(end);
-            return true;
-        }
-
-        int preStart = binarySearch(start);
-        if (preStart == -2)
+        set<pair<int, int>>::iterator nextPos = bookedLog.lower_bound({start, end});
+        if (nextPos->first < end)
             return false;
-        else if (preStart == -1){
-            if (end <= *(bookStart.begin())){
-                bookStart.insert(start);
-                bookEnd.insert(end);
-                return true;
-            }
-            else
-                return false;
-        }
-        else if (preStart == bookStart.size() - 1){
-            if (start >= *bookEnd.rbegin()){
-                bookStart.insert(start);
-                bookEnd.insert(end);
-                return true;
-            }
-            else    
-                return false;
-        }
-        else{
-            auto it1 = bookStart.begin(), it2 = bookEnd.begin();
-            for (int i = 0; i < preStart; i++){
-                it1++;
-                it2++;
-            }         
-            it1++;   
-            if (start >= *it2 && end <= *it1){
-                bookStart.insert(start);
-                bookEnd.insert(end);
-                return true;
-            }
-            else
-                return false;
-        }
-    }
-
-    int binarySearch(int start){
-        set<int>::iterator left = bookStart.begin(), right = bookStart.begin();
-        for (int i = 0; i < bookStart.size() -1; i++)
-            right++;
-        for ( ;right - left >= 0;){
-            set<int>::iterator mid = left + (right - left) / 2;
-            if (*mid == start)
-                return -2;
-            
-            if (*mid < start)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-        return right - bookStart.begin();
+        if ((--nextPos)->second > start)
+            return false;
+        bookedLog.insert({start, end});
+        return true;
     }
 };
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(start,end);
- */
