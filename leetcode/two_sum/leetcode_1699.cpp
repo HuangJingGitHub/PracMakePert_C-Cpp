8112173f6d3a3e4f2141c928bcb3a94c81b52f95
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-11-13 12:50:08 +0800

    Create leetcode_1699.cpp

---
+++ leetcode/two_sum/leetcode_1699.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *res = new ListNode(0), *head = res;
+
+        while (l1 != NULL && l2 != NULL){
+            if (l1->val < l2->val){
+                res->next = l1;
+                res = res->next;
+                l1 = l1->next;
+            }
+            else{
+                res->next = l2;
+                res = res->next;
+                l2 = l2->next;
+            }
+        }
+
+        if (l1 == NULL)
+            while (l2 != NULL){
+                res->next = l2;
+                res = res->next;
+                l2 = l2->next;
+            }
+        else if (l2 == NULL)
+            while (l1 != NULL){
+                res->next = l1;
+                res = res->next;
+                l1 = l1->next;
+            }
+        
+        return head->next;
+    }
+};
