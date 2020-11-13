ff94d028944af868a8bb3a0119294989b3228d5a
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-11-13 12:52:43 +0800

    Update leetcode_21.cpp

--- leetcode/two_sum/leetcode_21.cpp
+++ leetcode/two_sum/leetcode_21.cpp
@@ -13,16 +13,13 @@ public:
             return l2;
         else if (l2 == NULL)
             return l1;
-        else if ((*l1).val < (*l2).val)
-        {
+        else if ((*l1).val < (*l2).val){
             (*l1).next = this->mergeTwoLists((*l1).next, l2);
             return l1;
         }
-        else
-        {
+        else{
             (*l2).next = this->mergeTwoLists(l1, (*l2).next);
             return l2;
         }
     }
 };
-
