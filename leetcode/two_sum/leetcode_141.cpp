5ef6b59f515334d5e3b246ff2dd6950260fa8ce5
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-12-10 21:45:03 +0800

    Create leetcode_141.cpp

---
+++ leetcode/two_sum/leetcode_141.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        /* Sol 1: set
+        unordered_set<ListNode*> visited;
+        while (head) {
+            if (visited.find(head) != visited.end())
+                return true;
+            else
+                visited.insert(head);
+            head = head->next;
+        }
+        return false;
+        */
+        /* Sol 2: fast-slow pointer
+        */
+        if (!head || !head->next)
+            return false;
+        ListNode *slowPt = head, *fastPt = head->next;
+        while (slowPt != fastPt) {
+            if (!fastPt || !fastPt->next)
+                return false;
+            slowPt = slowPt->next;
+            fastPt = fastPt->next->next;
+        } 
+        return true;
+    }
+};
