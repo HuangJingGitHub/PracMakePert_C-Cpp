fb804308c86c0609dc94326f186f1bd8382ff5f5
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-12-23 20:55:22 +0800

    Create leetcode_147.cpp

---
+++ leetcode/two_sum/leetcode_147.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if (!head)
+            return NULL;
+
+        ListNode* res = new ListNode(0);
+        res->next = head;
+        head = head->next;
+        res->next->next = NULL;
+
+        while (head) {
+            ListNode *sortedPt = res->next, *sortedPtPre = res, *temp = head->next;
+            while (sortedPt && head->val > sortedPt->val) {
+                sortedPt = sortedPt->next;
+                sortedPtPre = sortedPtPre->next;
+            }
+            sortedPtPre->next = head;
+            head->next = sortedPt;
+            head = temp;
+        }
+        return res->next;
+    }
+};
