c7e5e33a11819198f567ea1c81b91cddb1d8d13a
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-12-27 23:10:54 +0800

    Create leetcode_150.cpp

---
+++ leetcode/two_sum/leetcode_150.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> numStack;
+
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/" )
+                calculation(numStack, tokens[i]);
+            else 
+                numStack.push(stoi(tokens[i]));
+        }
+        return numStack.top();
+    }
+    
+    void calculation(stack<int>& numStack, string& operation) {
+        int operand1, operand2;
+        operand2 = numStack.top();
+        numStack.pop();
+        operand1 = numStack.top();
+        numStack.pop();
+
+        if (operation == "+")
+            numStack.push(operand1 + operand2);
+        else if (operation == "-")
+            numStack.push(operand1 - operand2);
+        else if (operation == "*")
+            numStack.push(operand1 * operand2);
+        else   
+            numStack.push(operand1 / operand2);
+    }
+};
