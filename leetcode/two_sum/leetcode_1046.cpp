21e396101e892fcccb9c8ef127d2ba6f053007c3
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2020-12-30 18:58:03 +0800

    Create leetcode_1046.cpp

---
+++ leetcode/two_sum/leetcode_1046.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        for (int x : stones)
+            maxHeap.push(x);
+        int maxWeight = 0;
+        while (!maxHeap.empty()) {
+            maxWeight = maxHeap.top();
+            maxHeap.pop();
+            if (maxHeap.empty())
+                return maxWeight;
+            else if (maxWeight == maxHeap.top())
+                maxHeap.pop();
+            else {
+                int temp = maxWeight - maxHeap.top();
+                maxHeap.pop();     // Ensure pop before push, otherwise what is poped is the updated heap top.
+                maxHeap.push(temp);
+            }
+        }
+        return 0;
+    }
+};
