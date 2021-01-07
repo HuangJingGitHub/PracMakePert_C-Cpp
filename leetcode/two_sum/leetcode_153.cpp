90c63a49c6542f202f684acbcf7fd7701ef7d139
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-01-07 17:00:16 +0800

    Update leetcode_153.cpp

--- leetcode/two_sum/leetcode_153.cpp
+++ leetcode/two_sum/leetcode_153.cpp
@@ -15,3 +15,20 @@ public:
         return nums[left];
     }
 };
+
+
+// More compact version, see how the range change to guarantee the result always lies in range [left, right]
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1, mid = 0;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return nums[left];
+    }
+};
