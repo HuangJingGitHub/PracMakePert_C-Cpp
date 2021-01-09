e14882656c15bf49737987629d2a18b9dfe1c99d
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-01-09 23:38:15 +0800

    Update leetcode_162.cpp

--- leetcode/two_sum/leetcode_162.cpp
+++ leetcode/two_sum/leetcode_162.cpp
@@ -28,3 +28,26 @@ public:
         }
     }
 };
+
+
+// Much more compact version
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+
+        int left = 0, right = nums.size() - 1, mid;
+        // use left < right - 1 instead left < right to avoid discussion cases where left, mid overlap
+        while (left < right - 1) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                 return mid;
+            if (nums[mid - 1] >= nums[mid + 1])
+                right = mid - 1;
+            else
+                left = mid + 1; 
+        }
+        return (nums[left] > nums[right]) ? left : right;
+    }
+};
