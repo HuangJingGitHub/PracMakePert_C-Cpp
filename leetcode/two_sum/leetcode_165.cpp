82e97abf699e224b39c0182b146c9686c922c3bc
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-01-11 22:08:59 +0800

    Update leetcode_165.cpp

--- leetcode/two_sum/leetcode_165.cpp
+++ leetcode/two_sum/leetcode_165.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        vector<string> strVec1, strVec2;
-        for (int i = 0, j = 0; j < version1.size(); j++) {
-            if (version1[j] == '.') {
-                strVec1.push_back(version1.substr(i, j - i));
-                i = j + 1;
-            }
-            if (j == version1.size() - 1)
-                strVec1.push_back(version1.substr(i));
+        vector<int> idxVec1{-1}, idxVec2{-1};
+        for (int i = 0; i < version1.size(); i++) {
+            if (version1[i] == '.') 
+                idxVec1.push_back(i);
         }
-        for (int i = 0, j = 0; j < version2.size(); j++) {
-            if (version2[j] == '.') {
-                strVec2.push_back(version2.substr(i, j - i));
-                i = j + 1;
-            }
-            if (j == version2.size() - 1)
-                strVec2.push_back(version2.substr(i));
+        idxVec1.push_back(version1.size());
+
+        for (int i = 0; i < version2.size(); i++) {
+            if (version2[i] == '.')
+                idxVec2.push_back(i);
         }
+        idxVec2.push_back(version2.size());
 
-
-        int lenV1 = strVec1.size(), lenV2 = strVec2.size(), minLen = min(lenV1, lenV2), maxLen = max(lenV1, lenV2);
+        int lenV1 = idxVec1.size() - 1, lenV2 = idxVec2.size() - 1, minLen = min(lenV1, lenV2), maxLen = max(lenV1, lenV2);
         for (int i = 0; i < minLen; i++) {
-            int v1 = stoi(strVec1[i]), v2 = stoi(strVec2[i]);
+            int v1 = stoi(version1.substr(idxVec1[i] + 1, idxVec1[i + 1] - idxVec1[i] - 1)), 
+                v2 = stoi(version2.substr(idxVec2[i] + 1, idxVec2[i + 1] - idxVec2[i] - 1));
             if (v1 < v2)
                 return -1;
             else if (v1 > v2)
                 return 1;
         }
+        
         for (int i = minLen; i < maxLen; i++) {
-            int v1 = (lenV1 < maxLen) ? 0 : stoi(strVec1[i]),
-                v2 = (lenV2 < maxLen) ? 0 : stoi(strVec2[i]);
+            int v1 = (lenV1 < maxLen) ? 0 : stoi(version1.substr(idxVec1[i] + 1, idxVec1[i + 1] - idxVec1[i] - 1)),
+                v2 = (lenV2 < maxLen) ? 0 : stoi(version2.substr(idxVec2[i] + 1, idxVec2[i + 1] - idxVec2[i] - 1));
             if (v1 < v2)
                 return -1;
             else if (v1 > v2)
