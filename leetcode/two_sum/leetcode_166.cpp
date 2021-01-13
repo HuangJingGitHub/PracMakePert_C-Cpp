5661253f0acd05be9c848274e4eefb6e8ef67407
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-01-13 21:28:19 +0800

    Update leetcode_166.cpp

--- leetcode/two_sum/leetcode_166.cpp
+++ leetcode/two_sum/leetcode_166.cpp
@@ -2,25 +2,24 @@
 class Solution {
 public:
     string fractionToDecimal(int numerator, int denominator) {
-        long long int num = numerator, dem = denominator;   
+        long long int num = numerator, dem = denominator;
         vector<long long int> numeratorVec, resNum;
         string res;
-        bool recurring = false, negative = false;
+        bool recurring = false;
         int recurStart = 0;
 
         if (num % dem == 0)
             return to_string(num / dem);
         
-        if ((num > 0 && dem < 0) || (num < 0 && dem > 0)) {
-            negative = true;
-        }
+        if ((num > 0 && dem < 0) || (num < 0 && dem > 0))
+            res = "-";
         num = abs(num);
         dem = abs(dem);
         
         numeratorVec.push_back(num);
         while (num % dem != 0) {
             resNum.push_back(num / dem);
-            long long int newNumerator = 10 * (num % dem);   // Avoid exceeding int range, e.g. 10 * (1 % 2147483648) > INT_MAX, use long long int
+            long long int newNumerator = 10 * (num % dem);  // Avoid exceeding int range, e.g. 10 * (1 % 2147483648) > INT_MAX, use long long int
             
             for (int i = 0; i < numeratorVec.size(); i++) {
                 if (numeratorVec[i] == newNumerator) {
@@ -38,19 +37,13 @@ public:
 
         if (!recurring) {
             resNum.push_back(num / dem);
-            if (negative)
-                res = "-" + to_string(resNum[0]) + ".";
-            else
-                res = to_string(resNum[0]) + ".";
+            res += to_string(resNum[0]) + ".";
             for (int i = 1; i < resNum.size(); i++)
                 res += to_string(resNum[i]);
             return res;
         }
         else {
-            if (negative)
-                res = "-" + to_string(resNum[0]) + ".";
-            else
-                res = to_string(resNum[0]) + ".";
+            res += to_string(resNum[0]) + ".";
             int i = 1;
             while (i < recurStart) {
                 res += to_string(resNum[i]);
