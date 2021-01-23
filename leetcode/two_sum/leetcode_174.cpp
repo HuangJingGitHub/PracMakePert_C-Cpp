1cf0478ad441b0cdbbdebc4bbb467010d71b26b5
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-01-23 13:27:00 +0800

    Create leetcode_174.cpp

---
+++ leetcode/two_sum/leetcode_174.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        if (dungeon.empty() || dungeon[0].empty())
+            return 0;
+        int rowNum = dungeon.size(), colNum = dungeon[0].size(), healthRequirement = 0;
+        dungeon.back().back() = max(1, 1 - dungeon.back().back());  // Pay attention if the last grid is positive like 100, the health needs to be 1, not 1 - 100
+        for (int i = colNum - 2; i >= 0; i--) {
+            if (dungeon.back()[i] >= dungeon.back()[i + 1])
+                dungeon.back()[i] = 1;
+            else
+                dungeon.back()[i] = dungeon.back()[i + 1] - dungeon.back()[i];
+        }
+        for (int i  = rowNum - 2; i >= 0; i--) {
+            if (dungeon[i].back() >= dungeon[i + 1].back())
+                dungeon[i].back() = 1;
+            else
+                dungeon[i].back() = dungeon[i + 1].back() - dungeon[i].back();
+        }
+        for (int row = rowNum - 2; row >= 0; row--)
+            for (int col = colNum - 2; col >= 0; col--) {
+                if (dungeon[row][col] >= min(dungeon[row][col + 1], dungeon[row + 1][col]))
+                    dungeon[row][col] = 1;
+                else
+                    dungeon[row][col] = min(dungeon[row][col + 1], dungeon[row + 1][col]) - dungeon[row][col];
+            }
+        return dungeon[0][0]; 
+    }
+};
