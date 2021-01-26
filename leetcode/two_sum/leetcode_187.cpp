f901a65e16a68b9046baeba1f22ed1df7064822a
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-01-26 20:47:40 +0800

    Update leetcode_187.cpp

--- leetcode/two_sum/leetcode_187.cpp
+++ leetcode/two_sum/leetcode_187.cpp
@@ -37,10 +37,8 @@ public:
     vector<string> findRepeatedDnaSequences(string s) {
         vector<string> res;
         unordered_map<string, int> visitedStr;
-        if (s.size() <= 10)
-            return res;
 
-        for (int i = 0; i <= s.size() - 10; i++) {
+        for (int i = 0; i <= (int)(s.size() - 10); i++) {
             string curStr = s.substr(i, 10);
             visitedStr[curStr]++;
             if (visitedStr[curStr] == 2) 
