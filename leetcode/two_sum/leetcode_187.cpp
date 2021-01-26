ecc2ef67fbb5785c26f9c67b79560f5b23e069bd
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-01-26 20:41:56 +0800

    Create leetcode_187.cpp

---
+++ leetcode/two_sum/leetcode_187.cpp
@@ -0,0 +1,51 @@
+// brute force, two-layer loop, will exceed time limit
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        vector<string> res;
+        unordered_set<string> visitedStr;
+        if (s.size() <= 10)
+            return res;
+
+        for (int i = 0; i <= s.size() - 10; i++) {
+            string curStr = s.substr(i, 10);
+            if (visitedStr.find(curStr) != visitedStr.end()) 
+                continue;
+            
+            visitedStr.insert(curStr);
+            for (int j = i + 1; j <= s.size() - 10; j++) {
+                int cnt = 0;
+                while (cnt < 10) {
+                    if (curStr[cnt] == s[j + cnt])
+                        cnt++;
+                    else
+                        break;
+                }
+                if (cnt == 10) {
+                    res.push_back(curStr);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+// hash map, O(n) time and space complexity
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        vector<string> res;
+        unordered_map<string, int> visitedStr;
+        if (s.size() <= 10)
+            return res;
+
+        for (int i = 0; i <= s.size() - 10; i++) {
+            string curStr = s.substr(i, 10);
+            visitedStr[curStr]++;
+            if (visitedStr[curStr] == 2) 
+                res.push_back(curStr);
+        }
+        return res;
+    }
+};
