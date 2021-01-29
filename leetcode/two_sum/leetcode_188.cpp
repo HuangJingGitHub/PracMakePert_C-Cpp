738f3c9ebb78aed703d934d726a2c195705b81a5
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-01-29 11:37:25 +0800

    Update leetcode_188.cpp

--- leetcode/two_sum/leetcode_188.cpp
+++ leetcode/two_sum/leetcode_188.cpp
@@ -1,3 +1,5 @@
+// Good idea to use sorting algorithm, but not working as the fundamental thought is not complete to implement the time order.
+// Though this is equal to finding k pairs with max difference sum, it has transaction order limit.
 class Solution {
 public:
     static bool cmp(const pair<int, int>& a, const pair<int, int>& b) {
@@ -30,5 +32,35 @@ public:
                 break;
             }
         return res;
+    }
+};
+
+// 3D dp
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int res = 0;
+        if (k >= prices.size() / 2) {
+            for (int i = 1; i < prices.size(); i++)
+                if (prices[i] > prices[i - 1])
+                    res += (prices[i] - prices[i - 1]);
+            return res;
+        }
+
+        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(k+1, vector<int>(2)));    // profit of dp[i][j][0] i-th day with j times transaction / buy, no stock
+        for (int i = 0; i < prices.size(); i++) {                                                   // profit of dp[i][j][0] i-th day with j times transaction / buy, with stock
+            dp[i][0][0] = 0;
+            dp[i][0][1] = -INT_MAX;
+        }
+        for (int j = 1; j <= k; j++) {
+            dp[0][j][0]  = 0;
+            dp[0][j][1] = -prices[0];
+        }
+        for (int i = 1; i < prices.size(); i++)
+            for (int j = 1; j <= k; j++) {
+                dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1] + prices[i]);
+                dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i]);
+            }
+        return dp.back().back()[0];
     }
 };
