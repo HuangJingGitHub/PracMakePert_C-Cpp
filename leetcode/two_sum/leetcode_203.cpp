d7af6621ae19a85d27589f7f2757e367a2d4f003
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-02-04 23:18:36 +0800

    Update leetcode_203.cpp

--- leetcode/two_sum/leetcode_203.cpp
+++ leetcode/two_sum/leetcode_203.cpp
@@ -30,3 +30,21 @@ public:
         return head;
     }
 };
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *prev = dummy;
+        while (prev->next) {
+            if (prev->next->val == val)
+                prev->next = prev->next->next;
+            else 
+                prev = prev->next;
+        }
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
