989f6e767cce267a412b881d46826c7d8e73a5a6
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-03-10 21:21:25 +0800

    Update leetcode_214.cpp

--- leetcode/two_sum/leetcode_214.cpp
+++ leetcode/two_sum/leetcode_214.cpp
@@ -35,7 +35,7 @@ public:
 };
 
 
-// KMP algorithm
+// KMP algorithm 1
 class Solution {
 public:
     int computeNext(string pattern) {
@@ -54,6 +54,39 @@ public:
             else 
                 k = next[k];
         }
+        return next.back();
+    }
+
+    string shortestPalindrome(string s) {
+        if (s.size() <= 1)
+            return s;
+        string sRev(s.rbegin(), s.rend()), pattern = s + "#" + sRev;
+        int maxLen = computeNext(pattern);
+        return sRev.substr(0, sRev.size() - maxLen) + s;
+    }
+};
+
+
+// KMP algorithm 2
+class Solution {
+public:
+    int computeNext(string pattern) {
+        vector<int> next(pattern.size() + 1, 0);
+        next[0] = -1;
+        int pos = 1, cnt = 0;
+        while (pos < pattern.size()) {
+            if (pattern[pos] == pattern[cnt]) {
+                next[pos] = next[cnt];
+            }
+            else {
+                next[pos] = cnt;
+                while (cnt >= 0 && pattern[pos] != pattern[cnt])
+                    cnt = next[cnt];
+            }
+            pos++;
+            cnt++;
+        }
+        next[pos] = cnt;
         return next.back();
     }
 
