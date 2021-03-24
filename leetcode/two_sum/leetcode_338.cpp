3875c06d1531e3087df5d56d492e1229c7f16a9e
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-03-24 20:17:10 +0800

    Create leetcode_338.cpp

---
+++ leetcode/two_sum/leetcode_338.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> res(num + 1, 0);
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 == 0)
+                res[i] = res[i / 2];
+            else
+                res[i] = res[i - 1] + 1;
+        }
+        return res;
+    }
+};
