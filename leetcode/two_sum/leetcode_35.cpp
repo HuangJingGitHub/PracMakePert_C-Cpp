96e6cd43ffd2b423ad136c7b3990d2fc85fded50
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-03-24 17:29:47 +0800

    Update leetcode_35.cpp

--- leetcode/two_sum/leetcode_35.cpp
+++ leetcode/two_sum/leetcode_35.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        if (nums.size() == 0 || nums[0] >= target)
-            return 0;
-        if (nums[nums.size()-1] == target)      // Just pay attention to the details: == and <, different indices should be given.
-            return nums.size() - 1;            
-        if (nums[nums.size()-1] < target)
-            return nums.size();
-
         int left = 0, right = nums.size() - 1, mid;
-        while(left <= right)
-        {
+        while(left <= right) {
             mid = left + (right - left) / 2;
             if (nums[mid] == target)
                 return mid;
@@ -19,6 +11,6 @@ public:
             else 
                 right = mid - 1;
         }
-        return left;
-    }
+        return left;  // Note left is always the right idx target should be inserted no matter what case the binary search ends with.
+    }                 // If it connot find the target, then binary search will always end in a subrange of length 1 or 2!!!
 };
