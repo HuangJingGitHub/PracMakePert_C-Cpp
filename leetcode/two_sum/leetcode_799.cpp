ce652c1aa957c1d5abd5543136c1e8e5d0d346c1
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-04-07 19:23:23 +0800

    Create leetcode_799.cpp

---
+++ leetcode/two_sum/leetcode_799.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<double> rowState(1, poured);
+
+        for (int row = 0; row < query_row; row++) {
+            vector<double> nextRowState(row + 2, 0);
+            for (int i = 0; i < rowState.size(); i++) {
+                if (rowState[i] > 1) {
+                    nextRowState[i] += (rowState[i] - 1) / 2;
+                    nextRowState[i + 1] += (rowState[i] - 1) / 2;
+                }
+            }
+            rowState = nextRowState;
+        }
+
+        if (rowState[query_glass] > 1)
+            return 1;
+        else
+            return rowState[query_glass];
+    }
+};
