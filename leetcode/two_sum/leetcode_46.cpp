b0c625d63229417e31909705ec26e766b0da5e59
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-04-19 21:29:42 +0800

    Update leetcode_46.cpp

--- leetcode/two_sum/leetcode_46.cpp
+++ leetcode/two_sum/leetcode_46.cpp
@@ -2,9 +2,8 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-
         vector<vector<int>> res;
-        if (nums.size() == 1 || nums.size() == 0){
+        if (nums.size() == 1 || nums.size() == 0) {
             res.push_back(nums);
             return res;
         }
@@ -12,13 +11,13 @@ public:
         vector<int> initVec{nums[0]};
         res.push_back(initVec);
         int currentSize;
-        for(int i = 1; i < nums.size(); i++){
+        for(int i = 1; i < nums.size(); i++) {
             currentSize = res.size();
-            for (int j = 0; j < i; j++){
-                for (int k = 0; k < currentSize; k++){
+            for (int j = 0; j < i; j++) {
+                for (int k = 0; k < currentSize; k++) {
                     res.push_back(res[k]);
                 }
-                for(int m = 0; m < currentSize; m++){
+                for(int m = 0; m < currentSize; m++) {
                     res[res.size() - m - 1].insert(res[res.size() - m - 1].begin() + j + 1, nums[i]);
                 }
             }
@@ -30,21 +29,19 @@ public:
 };
 
 
-// This is the backtracking algorithm. See the discussion for details.
+// This is the backtracking algorithm + dfs. See the discussion for details.
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         int len = nums.size();
         vector<vector<int>> res;
-
-        if (len == 0 || len == 1)
-        {
+        vector<bool> used(len, false);
+        vector<int> path;
+        
+        if (len == 0 || len == 1) {
             res.push_back(nums);
             return res;
         }
-
-        vector<bool> used(len, false);
-        vector<int> path;
 
         dfs(nums, len, 0, path, used, res);
         return res;
@@ -52,17 +49,16 @@ public:
 
 private:
     void dfs(vector<int> nums, int len, int depth, vector<int> path, vector<bool> used, 
-            vector<vector<int>>& res){
-            if (depth == len){
+            vector<vector<int>>& res) {
+            if (depth == len) {
                 res.push_back(path);
                 return;
             }
 
-            for (int i = 0; i < len; i++){
-                if (!used[i]){
+            for (int i = 0; i < len; i++) {
+                if (!used[i]) {
                     path.push_back(nums[i]);
                     used[i] = true;
-
                     dfs(nums, len, depth + 1, path, used, res);
                     used[i] = false;
                     path.pop_back();
