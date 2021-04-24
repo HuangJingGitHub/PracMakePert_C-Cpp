7a5726ba9fbbb332125a2f74de1ccda0a0b32b6b
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-04-24 23:42:58 +0800

    Create leetcode_222.cpp

---
+++ leetcode/two_sum/leetcode_222.cpp
@@ -0,0 +1,9 @@
+// compact recursion
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+};
