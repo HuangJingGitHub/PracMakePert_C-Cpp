1e7fd7cbcf92f752273780ee54a42632f491c7cf
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-05-13 20:43:31 +0800

    Update leetcode_53.cpp

--- leetcode/two_sum/leetcode_53.cpp
+++ leetcode/two_sum/leetcode_53.cpp
@@ -31,3 +31,18 @@ public:
         return maxSum;
     }
 };
+
+// or
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = nums[0], curSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            curSum  += nums[i];
+            res = max(res, curSum);
+            if (curSum < 0)
+                curSum = 0;
+        }
+        return res;
+    }
+};
