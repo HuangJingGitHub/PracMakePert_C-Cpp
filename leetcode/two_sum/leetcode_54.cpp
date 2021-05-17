daef6d6e36a5dad90084ab40e568a59225691cb1
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-05-17 23:35:20 +0800

    Update leetcode_54.cpp

--- leetcode/two_sum/leetcode_54.cpp
+++ leetcode/two_sum/leetcode_54.cpp
@@ -38,3 +38,39 @@ public:
         return res;
     }
 };
+
+
+// more compact and clear
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        int rows = matrix.size(), cols = matrix[0].size(), 
+            startRow = 0, startCol = 0, 
+            endRow = rows - 1, endCol = cols - 1;
+        while (startRow <= endRow && startCol <= endCol) {
+            spiralLevelTraversal(startRow, startCol, endRow, endCol, matrix, res);
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+        }
+        return res;
+    }
+
+    void spiralLevelTraversal(int startRow, int startCol, int endRow, int endCol, vector<vector<int>>& matrix, vector<int>& res) {
+        for (int i = startCol; i <= endCol; i++)
+            res.push_back(matrix[startRow][i]);
+        if (startRow == endRow)
+            return;
+        for (int i = startRow + 1; i <= endRow; i++)
+            res.push_back(matrix[i][endCol]);
+        if (startCol == endCol)
+            return;
+            
+        for (int i = endCol - 1; i >= startCol; i--)
+            res.push_back(matrix[endRow][i]);
+        for (int i = endRow - 1; i > startRow; i--)
+            res.push_back(matrix[i][startCol]);
+    }
+};
