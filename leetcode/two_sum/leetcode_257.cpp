6f5c8118b323d1ea6ae6bc836021b35c99f4b5d9
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-05-27 10:25:13 +0800

    Update leetcode_257.cpp

--- leetcode/two_sum/leetcode_257.cpp
+++ leetcode/two_sum/leetcode_257.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> res;
+        string valStr = to_string(root->val),
+               prefix = valStr + "->";
+
         if (!root->left && !root->right)
+            return vector<string>(1, valStr);
+        if (root->left) {
+            res = binaryTreePaths(root->left);
+            for (string& str : res) 
+                str = prefix + str;
+        }
+        if (root->right) {
+            vector<string> rightRes = binaryTreePaths(root->right);
+            for (string str : rightRes)
+                res.push_back(prefix + str);
+        }
+
+        return res;
     }
 };
