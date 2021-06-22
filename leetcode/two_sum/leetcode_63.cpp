762da87aabdb358ab629e3ffae2d39563b83d23c
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-06-22 15:26:51 +0800

    Update leetcode_63.cpp

--- leetcode/two_sum/leetcode_63.cpp
+++ leetcode/two_sum/leetcode_63.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if (obstacleGrid.size() == 0 || obstacleGrid[0].size() == 0)
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector(n, 0));
+
+        if (obstacleGrid[0][0] == 1)
             return 0;
-
-        int m = obstacleGrid[0].size(), n = obstacleGrid.size();
-        vector<vector<int>> dp(n, vector(m, 0));
-        vector<vector<bool>> isObstacle(n, vector(m, false));
-
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                if (obstacleGrid[i][j] == 1){
-                    isObstacle[i][j] = true;
-                }
-
-        for (int i = 0; i < m; i++){
-            if (!isObstacle[0][i])
-                dp[0][i] = 1;
+        dp[0][0] = 1;
+        for (int col = 1; col < n; col++)
+            if (obstacleGrid[0][col] == 1)
+                dp[0][col] = 0;
             else
-                break;
-        } 
-        
-        for (int i = 0; i < n; i++){
-            if (!isObstacle[i][0])
-                dp[i][0] = 1;
+                dp[0][col] = dp[0][col-1];
+        for (int row = 1; row < m; row++)
+            if (obstacleGrid[row][0] == 1)
+                dp[row][0] = 0;
             else
-                break;
-        }
+                dp[row][0] = dp[row-1][0];
 
-        for (int i = 1; i < n; i++)
-            for (int j = 1; j < m; j++){
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
-                if (isObstacle[i][j])
-                    dp[i][j] = 0;
-            }
-        
-        return dp[n-1][m-1];
+        for (int row = 1; row < m; row++)
+            for (int col = 1; col < n; col++)
+                if (obstacleGrid[row][col] == 1)
+                    dp[row][col] = 0;
+                else
+                    dp[row][col] = dp[row-1][col] + dp[row][col-1]; 
+
+        return dp[m-1][n-1];
     }
 };
