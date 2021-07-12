fabcc18e7f7e5967aab2135ee9da3cd3dbde659e
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-07-12 22:13:09 +0800

    Update leetcode_287.cpp

--- leetcode/two_sum/leetcode_287.cpp
+++ leetcode/two_sum/leetcode_287.cpp
@@ -13,3 +13,21 @@ public:
         return 1;
     }
 };
+
+
+// Treat the array as a linked list and use fast-slow pointer.
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int res = 0;;
+        for (int fast = 0; res != fast || fast == 0; ) {
+            res = nums[res];
+            fast = nums[nums[fast]];
+        }
+
+        for (int i = 0; res != i; i = nums[i]) {
+            res = nums[res];
+        }
+        return res;
+    }
+};
