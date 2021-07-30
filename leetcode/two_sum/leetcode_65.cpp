e51cf4621d3fca2796e4fe117777c406423569ff
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-07-30 23:50:10 +0800

    Update leetcode_65.cpp

--- leetcode/two_sum/leetcode_65.cpp
+++ leetcode/two_sum/leetcode_65.cpp
@@ -3,16 +3,16 @@ public:
     bool isNumber(string s) {
         int i = 0;
 
-        // skip the whilespaces
+        // skip the whitespaces
         for(; s[i] == ' '; i++) {}
 
         // check the significand
         if(s[i] == '+' || s[i] == '-') i++; // skip the sign if exist
 
         int n_nm, n_pt;
-        for(n_nm=0, n_pt=0; (s[i]<='9' && s[i]>='0') || s[i]=='.'; i++)
-            s[i] == '.' ? n_pt++:n_nm++;       
-        if(n_pt>1 || n_nm<1) // no more than one point, at least one digit
+        for(n_nm = 0, n_pt = 0; (s[i] <= '9' && s[i] >= '0') || s[i] == '.'; i++)
+            s[i] == '.' ? n_pt++ : n_nm++;       
+        if(n_pt > 1 || n_nm < 1) // no more than one point, at least one digit
             return false;
 
         // check the exponent if exist
@@ -21,14 +21,14 @@ public:
             if(s[i] == '+' || s[i] == '-') i++; // skip the sign
 
             int n_nm = 0;
-            for(; s[i]>='0' && s[i]<='9'; i++, n_nm++) {}
-            if(n_nm<1)
+            for( ; s[i] >= '0' && s[i] <= '9'; i++, n_nm++) {}
+            if(n_nm < 1)
                 return false;
         }
 
         // skip the trailing whitespaces
         for(; s[i] == ' '; i++) {}
 
-        return s[i]==0;  // must reach the ending 0 of the string
+        return s[i] == 0;  // must reach the ending 0 of the string
     }
 };
