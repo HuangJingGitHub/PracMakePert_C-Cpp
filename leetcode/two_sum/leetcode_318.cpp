78eb39ee20a0dba89752ed868bb4c1bd686a55b9
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-08-17 11:11:24 +0800

    Update leetcode_318.cpp

--- leetcode/two_sum/leetcode_318.cpp
+++ leetcode/two_sum/leetcode_318.cpp
@@ -1,20 +1,35 @@
+// will exceed the time limit
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
         int res = 0;
-        vector<vector<pair<int, int>>> letterLog(26);
+        vector<set<int>> letterWordLog(26);
+        vector<set<int>> commonLetterSet(words.size());
+        vector<int> noCommonLetterMaxLen(words.size());
+        
         for (int i = 0; i < words.size(); i++)
             for (char ch : words[i])
-                letterLog[ch - 'a'].push_back(pair<int, int>(words[i].size(), i));
-        
-        for (auto& log : letterLog)
-            sort(log.begin(), log.end());
-        
+                letterWordLog[ch - 'a'].insert(i);
+
         for (int i = 0; i < words.size(); i++) {
-            unordered_set<int> curLog;
-            for (char ch : words[i])
-                curLog.push(ch - 'a');
-            for(int )
+            for (char ch : words[i]) {
+                int letterIdx = ch - 'a';
+                commonLetterSet[i].insert(letterWordLog[letterIdx].begin(), letterWordLog[letterIdx].end());
+            }
         }
+
+        for (int i = 0; i < words.size(); i++) {
+            int maxLen = 0;
+            for (int j = 0; j < words.size(); j++)
+                if (commonLetterSet[i].find(j) == commonLetterSet[i].end() && words[j].size() > maxLen) {
+                    noCommonLetterMaxLen[i] = words[j].size();
+                    maxLen = words[j].size();
+                }
+        }
+
+        for (int i = 0; i < words.size(); i++) 
+            res = max(res, (int)words[i].size() * noCommonLetterMaxLen[i]);
+        
+        return res;
     }
 };
