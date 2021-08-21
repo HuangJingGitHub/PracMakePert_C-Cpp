592162dd9aa67cd7a43e106cca538b9fad583b44
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-08-21 20:43:36 +0800

    Update leetcode_322.cpp

--- leetcode/two_sum/leetcode_322.cpp
+++ leetcode/two_sum/leetcode_322.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int res = 0, curAmount = 0;
+        if (amount == 0)
+            return 0;
         
-        sort(coins.begin(), coins.end());
-        reverse(coins.begin(), coins.end());
-        vector<int> maxCoinNum(coins.size()), curCoinNum(coins.size());
-        
-        for (int i = 0; i < maxCoinNum.size(); i++)
-            maxCoinNum[i] = amount / coins[i];
-        
-        while (curAmount != amount) {
-            
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int& coin : coins)
+                if (i - coin >= 0 && dp[i - coin] != INT_MAX) 
+                    dp[i] = min(dp[i], dp[i - coin] + 1);        
         }
-        
+        return (dp[amount] == INT_MAX) ? -1 : dp[amount];
     }
 };
