34b7519e7b59d1d3c999ec75fabe297abcb31406
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-09-07 16:10:27 +0800

    Update leetcode_328.cpp

--- leetcode/two_sum/leetcode_328.cpp
+++ leetcode/two_sum/leetcode_328.cpp
@@ -22,8 +22,6 @@ public:
             evenHead->next = oddHead->next;
             evenHead = evenHead->next;
         }
-        if (oddHead)
-            oddHead->next = nullptr;
         oddHead->next = evenHeadLog;
         return head;
     }
