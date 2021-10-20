6ed39e757ad8ba7bdd75fffdaf28d17a294a11e5
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-10-20 23:40:08 +0800

    Update leetcode_152.cpp

--- leetcode/two_sum/leetcode_152.cpp
+++ leetcode/two_sum/leetcode_152.cpp
@@ -12,3 +12,29 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int res = nums[0];
+        vector<vector<int>> dp(nums.size(), vector<int>(2));
+        dp[0][0] = nums[0];
+        dp[0][1] = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] >= 0) {
+                dp[i][0] = min(nums[i], nums[i] * dp[i - 1][0]);
+                dp[i][1] = max(nums[i], nums[i] * dp[i - 1][1]);
+            }
+            else {
+                dp[i][0] = min(nums[i], nums[i] * dp[i - 1][1]);
+                dp[i][1] = max(nums[i], nums[i] * dp[i - 1][0]); 
+            }
+        }
+
+        for (int i = 1; i < nums.size(); i++)
+            res = max(res, dp[i][1]);
+        return res;
+    }
+};
