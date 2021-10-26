003946563df1075600c38fa76c45c41d9133172b
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-10-26 23:37:02 +0800

    Update leetcode_221.cpp

--- leetcode/two_sum/leetcode_221.cpp
+++ leetcode/two_sum/leetcode_221.cpp
@@ -38,3 +38,31 @@ public:
         return resLen * resLen;
     }
 };
+
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int rowNum = matrix.size(), colNum = matrix[0].size(), resLen = 0;
+        vector<vector<int>> dp(rowNum, vector<int>(colNum, 0));
+
+        for (int i = 0; i < colNum; i++)
+            if (matrix[0][i] == '1') {
+                dp[0][i] = 1;
+                resLen = 1;
+            }
+        for (int i = 1; i < rowNum; i++)
+            if (matrix[i][0] == '1') {
+                dp[i][0] = 1;
+                resLen = 1;
+            }
+        
+        for (int row = 1; row < rowNum; row++)
+            for (int col = 1; col < colNum; col++)
+                if (matrix[row][col] == '1') {
+                    dp[row][col] = min({dp[row - 1][col - 1], dp[row][col - 1], dp[row - 1][col]}) + 1;
+                    resLen = max(resLen, dp[row][col]);
+                }
+        return resLen * resLen;
+    }
+};
