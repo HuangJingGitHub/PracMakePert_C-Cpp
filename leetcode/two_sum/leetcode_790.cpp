48ffe64c2e12da5a3bbd2a0c9ada5c1e8bfd2509
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-11-08 21:42:18 +0800

    Update leetcode_790.cpp

--- leetcode/two_sum/leetcode_790.cpp
+++ leetcode/two_sum/leetcode_790.cpp
@@ -15,3 +15,28 @@ public:
         return dp.back() % modNum;
     }
 };
+
+
+class Solution {
+public:
+    int numTilings(int N) {
+        if (N == 1)
+            return 1;
+        int modNum = 1000000007;
+        vector<long long int> dp(N + 1), cornerDp(N + 1);
+        dp[1] = 1;
+        dp[2] = 2;
+        cornerDp[1] = 0;
+        cornerDp[2] = 2;  
+
+        // Consider how the last tile is put. Corner represents styles with one row filled, one row not.
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + cornerDp[i - 1];
+            cornerDp[i] = 2 * dp[i - 2] + cornerDp[i - 1];
+            dp[i] %= modNum;
+            cornerDp[i] %= modNum;
+        }
+
+        return dp.back() % modNum;
+    }
+};
