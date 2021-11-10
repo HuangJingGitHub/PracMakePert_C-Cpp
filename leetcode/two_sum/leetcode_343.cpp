2dac89d10339762a27b0d606a2cad1952c6f8e74
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-11-10 18:27:56 +0800

    Update leetcode_343.cpp

--- leetcode/two_sum/leetcode_343.cpp
+++ leetcode/two_sum/leetcode_343.cpp
@@ -13,3 +13,18 @@ public:
 
     }
 };
+
+
+// dp solution
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n + 1, 1);
+
+        // pay attention j * (i - j) is a new possible break pair with i - j complete, rather than broken
+        for (int i = 2; i <= n; i++)
+            for (int j = 1; j <= i - 1; j++)
+                dp[i] = max({dp[i], j * dp[i - j], j * (i - j)});  
+        return dp.back();
+    }
+};
