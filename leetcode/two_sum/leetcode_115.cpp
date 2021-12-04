8c94da1b38ce7e7a41343b724d61d781cccd4167
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-12-04 20:00:24 +0800

    Update leetcode_115.cpp

--- leetcode/two_sum/leetcode_115.cpp
+++ leetcode/two_sum/leetcode_115.cpp
@@ -65,3 +65,32 @@ public:
         return matched.back();
     }
 };
+
+
+// 2D classical dp
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int sLen = s.size(), tLen = t.size();
+
+        vector<vector<long long int>> dp(sLen, vector<long long int>(tLen, 0));
+
+        if (s[0] == t[0])
+            dp[0][0] = 1;
+        for (int row = 1; row < sLen; row++)
+            if (s[row] == t[0])
+                dp[row][0] = dp[row - 1][0] + 1;
+            else
+                dp[row][0] = dp[row - 1][0];
+        
+        // dp[i][j] s[0:j] to t[0:j]
+        for (int row = 1; row < sLen; row++)
+            for (int col = 1; col < tLen; col++) {
+                if (s[row] == t[col])
+                    dp[row][col] = dp[row - 1][col - 1] + dp[row - 1][col];
+                else 
+                    dp[row][col] = dp[row - 1][col];
+            }
+        return dp.back().back();
+    }
+};
