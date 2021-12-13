90aa54cbaba3a1df6711574431f25f781c81ee56
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-12-13 12:19:52 +0800

    Create leetcode_2378.cpp

---
+++ leetcode/two_sum/leetcode_2378.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // find which node is the first one to find p or q
+        if (root == nullptr || root == p || root == q)
+            return root;
+        
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        // find p, q in left and right tree
+        if (left != nullptr && right != nullptr)
+            return root;
+        return (left != nullptr) ? left : right;
+    }
+};
