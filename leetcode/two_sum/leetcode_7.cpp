e7c6a069f2583fb6c603c4d7fd802857ab124d0f
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-12-27 23:53:41 +0800

    Update leetcode_7.cpp

--- leetcode/two_sum/leetcode_7.cpp
+++ leetcode/two_sum/leetcode_7.cpp
@@ -2,14 +2,30 @@ class Solution {
 public:
     int reverse(int x) {
         long num=0;
-		while(x) {
-			num = num * 10 + x % 10;
-			x /= 10;
-		}
-		if(x < 0)
-			num *= -1;
-		if(num < -2147483648 || num > 2147483647)	
-			num = 0;
-		return num;
+	while(x) {
+		num = num * 10 + x % 10;
+		x /= 10;
+	}
+	if(x < 0)
+		num *= -1;
+	if(num < -2147483648 || num > 2147483647)	
+		num = 0;
+	return num;
+    }
+};
+
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+	while (x != 0) {
+		int digit = x % 10;
+		if (res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > 7)
+		    return 0;
+		if (res < INT_MIN / 10 || (res == INT_MIN / 10 && digit < -8)
+		    return 0;
+		res = res * 10 + digit;
+	}
+	return res;
     }
 };
