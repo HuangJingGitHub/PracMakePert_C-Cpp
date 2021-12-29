9e18d0e0fae43cac8ebf6d7e97b8707dc0d3aa8b
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-12-29 18:07:32 +0800

    Update leetcode_137.cpp

--- leetcode/two_sum/leetcode_137.cpp
+++ leetcode/two_sum/leetcode_137.cpp
@@ -13,7 +13,7 @@ public:
         
         for (int i = 0; i < 32; i++) {
             res <<= 1;
-            res |= resBin[i] % 3;  // The mod result will only be 0 or 1 according to the problem settring. x |= 0 --> last bit intact, x |= 1 --> last bit 1
+            res |= resBin[i] % 3;  // The mod result 0 or 1 is equal to the bit balue of the unique number in array. x |= 0 --> last bit intact, x |= 1 --> last bit 1
         }
         return res;
     }
