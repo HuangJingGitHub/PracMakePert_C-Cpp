7caae72ae8f7f0e7aa359df203f09b7362ad9576
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2021-12-30 19:54:08 +0800

    Update leetcode_131.cpp

--- leetcode/two_sum/leetcode_131.cpp
+++ leetcode/two_sum/leetcode_131.cpp
@@ -9,11 +9,11 @@ public:
             return res;
         
         vector<string> path;
-        backtracking(s, 0, len, path, res);
+        backtrace(s, 0, len, path, res);
         return res;
     }
     
-    void backtracking(string& s, int start, int len, vector<string>& path, vector<vector<string>>& res){
+    void backtrace(string& s, int start, int len, vector<string>& path, vector<vector<string>>& res){
         if (start == len){
             res.push_back(path);
             return;
@@ -24,7 +24,7 @@ public:
                 continue;
             
             path.push_back(s.substr(start, i - start + 1));
-            backtracking(s, i + 1, len, path, res);
+            backtrace(s, i + 1, len, path, res);
             path.pop_back();
         }
     }
