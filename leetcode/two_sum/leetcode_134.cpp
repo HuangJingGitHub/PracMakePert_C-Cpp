4e42ac1eb4c36b8fef1edf0e1bbd295c2a398eca
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-01-05 19:44:01 +0800

    Update leetcode_134.cpp

--- leetcode/two_sum/leetcode_134.cpp
+++ leetcode/two_sum/leetcode_134.cpp
@@ -44,3 +44,21 @@ public:
         return -1;
     }
 };
+
+
+Class Solution {
+public: 
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int tankGas = 0, totalGas = 0, start = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            tankGas += gas[i] - cost[i];
+            totalGas += gas[i] - cost[i];
+            if (tankGas < 0) {
+                start = i + 1;
+                tankGas = 0;
+            }
+        }
+        
+        return totalGas < 0 ? -1 : start;
+    }
+}
