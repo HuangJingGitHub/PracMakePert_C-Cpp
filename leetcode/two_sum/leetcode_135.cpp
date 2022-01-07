39f37758fc5946ac4808f50cbc39aad8a9ed4138
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-01-07 20:04:44 +0800

    Update leetcode_135.cpp

--- leetcode/two_sum/leetcode_135.cpp
+++ leetcode/two_sum/leetcode_135.cpp
@@ -24,3 +24,45 @@ public:
         return res;
     }
 };
+
+
+// Algorithm is correct, but O(n^2) complexity
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        if (ratings.empty() == true)
+            return 0;
+
+        int res = 1, preCandy = 1;
+        vector<int> candyVec(ratings.size(), 1);
+        for (int i = 1; i < ratings.size(); i++) {
+            if (ratings[i] == ratings[i - 1]) {
+                res += 1;
+                preCandy = 1;
+            }
+            else if (ratings[i] < ratings[i - 1]) {
+                if (preCandy == 1) {
+                    res += 1;
+                    candyVec[i] = 1;
+                    for (int j = i - 1; j >= 0 && ratings[j] > ratings[j + 1] 
+                        && candyVec[j] <= candyVec[j + 1]; j--) {
+                        res += 1;
+                        candyVec[j]++;
+                    }
+                    preCandy = 1;
+                }
+                else {
+                    res += 1;
+                    preCandy = 1;
+                }
+            }
+            else {
+                res += preCandy + 1;
+                preCandy++;
+                candyVec[i] = preCandy;
+            }
+        }
+
+        return res;
+    }
+};
