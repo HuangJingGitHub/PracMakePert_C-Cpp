90522f97991b0c3079599095bfb188d2d92fa468
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-01-17 23:15:49 +0800

    Update leetcode_150.cpp

--- leetcode/two_sum/leetcode_150.cpp
+++ leetcode/two_sum/leetcode_150.cpp
@@ -4,28 +4,25 @@ public:
         stack<int> numStack;
 
         for (int i = 0; i < tokens.size(); i++) {
-            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/" )
-                calculation(numStack, tokens[i]);
+            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/" ) {
+                int operand1, operand2;
+                operand2 = numStack.top();
+                numStack.pop();
+                operand1 = numStack.top();
+                numStack.pop();
+
+                if (operation == "+")
+                    numStack.push(operand1 + operand2);
+                else if (operation == "-")
+                    numStack.push(operand1 - operand2);
+                else if (operation == "*")
+                    numStack.push(operand1 * operand2);
+                else   
+                    numStack.push(operand1 / operand2);                                
+            }
             else 
                 numStack.push(stoi(tokens[i]));
         }
         return numStack.top();
-    }
-    
-    void calculation(stack<int>& numStack, string& operation) {
-        int operand1, operand2;
-        operand2 = numStack.top();
-        numStack.pop();
-        operand1 = numStack.top();
-        numStack.pop();
-
-        if (operation == "+")
-            numStack.push(operand1 + operand2);
-        else if (operation == "-")
-            numStack.push(operand1 - operand2);
-        else if (operation == "*")
-            numStack.push(operand1 * operand2);
-        else   
-            numStack.push(operand1 / operand2);
     }
 };
