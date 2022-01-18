3e8ad32fe924d54496649f7802ec714d6d4f18cc
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-01-18 22:09:25 +0800

    Update leetcode_151.cpp

--- leetcode/two_sum/leetcode_151.cpp
+++ leetcode/two_sum/leetcode_151.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     string reverseWords(string s) {
         string res;
+        
         for (int i = s.size() - 1; i >= 0; i--) {
             if (s[i] == ' ')
                 continue;
+            
             int j = i - 1;
             while (j >= 0 && s[j] != ' ') 
                 j--;
@@ -12,7 +14,34 @@ public:
             res += " ";
             i = j;
         }
+        
         res.pop_back();
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res;
+        int idx2 = s.size() - 1, idx1 = 0;
+
+        while (idx1 >= 0) {
+            while (idx2 >= 0 && s[idx2] == ' ')
+                idx2--;
+            if (idx2 < 0)
+                break;
+
+            idx1 = idx2 - 1;
+            while (idx1 >= 0 && s[idx1] != ' ')
+                idx1--;
+            
+            res += (" " + s.substr(idx1 + 1, idx2 - idx1));
+            idx2 = idx1;
+        }
+
+        res = res.substr(1);
         return res;
     }
 };
