d427c4e95a339389c90ba84779dd264e656c41e6
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-01-24 22:13:09 +0800

    Update leetcode_165.cpp

--- leetcode/two_sum/leetcode_165.cpp
+++ leetcode/two_sum/leetcode_165.cpp
@@ -35,3 +35,45 @@ public:
         return 0;
     }
 };
+
+
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        vector<int> dotIdx1, dotIdx2;
+        
+        for (int i = 0; i < version1.size(); i++)
+            if (version1[i] == '.')
+                dotIdx1.push_back(i);
+        dotIdx1.push_back(version1.size());
+        for (int i = 0; i < version2.size(); i++)
+            if (version2[i] == '.')
+                dotIdx2.push_back(i);
+        dotIdx2.push_back(version2.size());
+        
+        vector<int> versionVec1, versionVec2;
+        versionVec1.push_back(stoi(version1.substr(0, dotIdx1[0])));
+        for (int i = 0; i < dotIdx1.size() - 1; i++)
+            versionVec1.push_back(stoi(version1.substr(dotIdx1[i] + 1, dotIdx1[i + 1] - dotIdx1[i])));
+
+        versionVec2.push_back(stoi(version2.substr(0, dotIdx2[0])));
+        for (int i = 0; i < dotIdx2.size() - 1; i++)
+            versionVec2.push_back(stoi(version2.substr(dotIdx2[i] + 1, dotIdx2[i + 1] - dotIdx2[i])));
+            
+        if (versionVec1.size() > versionVec2.size()) {
+            for (int i = versionVec2.size(); i < versionVec1.size(); i++)
+                versionVec2.push_back(0);
+        }
+        else {
+            for (int i = versionVec1.size(); i < versionVec2.size(); i++)
+                versionVec1.push_back(0);
+        }
+
+        for (int i = 0; i < versionVec1.size(); i++)
+            if (versionVec1[i] > versionVec2[i])
+                return 1;
+            else if (versionVec1[i] < versionVec2[i])
+                return -1;
+        return 0;
+    }
+};
