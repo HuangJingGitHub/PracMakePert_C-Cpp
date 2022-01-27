dbe75cebfbce66c93725dc692245626200c73b19
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-01-27 21:29:23 +0800

    Update leetcode_168.cpp

--- leetcode/two_sum/leetcode_168.cpp
+++ leetcode/two_sum/leetcode_168.cpp
@@ -50,3 +50,18 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    string convertToTitle(int n) {
+       string res;
+
+       while (n != 0) {
+           n--;
+           res.insert(0, 1, 'A' + n % 26);
+           n /= 26;
+       }
+       return res;
+    }
+};
