56baee56b867fab9c2d6cd6eb4127ada7afb66d7
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-01-30 13:02:36 +0800

    Update leetcode_174.cpp

--- leetcode/two_sum/leetcode_174.cpp
+++ leetcode/two_sum/leetcode_174.cpp
@@ -27,3 +27,24 @@ public:
         return dungeon[0][0]; 
     }
 };
+
+
+// More concise
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int rowNum = dungeon.size(), colNum = dungeon[0].size();
+
+        dungeon.back().back() = max(1 - dungeon.back().back(), 1);
+        for (int row = rowNum - 2; row >= 0; row--)
+            dungeon[row].back() =  max(dungeon[row + 1].back() - dungeon[row].back(), 1);
+        for (int col = colNum - 2; col >= 0; col--)
+            dungeon.back()[col] = max(dungeon.back()[col + 1] - dungeon.back()[col], 1);
+        
+        for (int row = rowNum - 2; row >= 0; row--)
+            for (int col = colNum - 2; col >= 0; col--)
+                dungeon[row][col] = max(min(dungeon[row][col + 1], dungeon[row + 1][col]) - dungeon[row][col], 1);
+
+        return dungeon[0][0];
+    }
+};
