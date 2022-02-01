9c991631704a8f568950d60d404524bf47e87655
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-02-01 21:24:00 +0800

    Update leetcode_190.cpp

--- leetcode/two_sum/leetcode_190.cpp
+++ leetcode/two_sum/leetcode_190.cpp
@@ -11,3 +11,18 @@ public:
        return res;
     }
 };
+
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int res = 0;
+        for (int i = 1; i < 32; i++) {
+            res += (n & 1);
+            res <<= 1;
+            n >>= 1;
+        }
+        res += (n & 1);
+        return res;
+    }
+};
