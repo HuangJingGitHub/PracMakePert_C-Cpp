837ae08b74a5f92fecb8342fb528771774ff6491
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-02-09 14:04:09 +0800

    Update leetcode_200.cpp

--- leetcode/two_sum/leetcode_200.cpp
+++ leetcode/two_sum/leetcode_200.cpp
@@ -7,10 +7,9 @@ public:
             return res;
 
         for (int i = 0; i < grid.size(); i++)
-            for (int j = 0; j < grid[0].size(); j++){
-                if (grid[i][j] == '1')
-                    if (DFS_ReacheWater(grid, i, j))
-                        res++;
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1' && DFS_ReacheWater(grid, i, j))
+                    res++;
             }
         return res;
     }
@@ -22,10 +21,10 @@ public:
             return true;
         grid[row][col] = '2';  // mark visited land
 
-        bool left = DFS_ReacheWater(grid, row, col - 1);
-        bool right = DFS_ReacheWater(grid, row, col + 1);
-        bool up = DFS_ReacheWater(grid, row - 1, col);
-        bool down = DFS_ReacheWater(grid, row + 1, col);
+        bool left = DFS_ReacheWater(grid, row, col - 1),
+             right = DFS_ReacheWater(grid, row, col + 1),
+             up = DFS_ReacheWater(grid, row - 1, col),
+             down = DFS_ReacheWater(grid, row + 1, col);
 
         return left && right && up && down;
     }
