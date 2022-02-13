a2ffe5140f639bdbf221e897a0367c8029c36585
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-02-13 22:14:36 +0800

    Update leetcode_28.cpp

--- leetcode/two_sum/leetcode_28.cpp
+++ leetcode/two_sum/leetcode_28.cpp
@@ -16,3 +16,34 @@ public:
         return -1;
     }
 };
+
+
+// KMP
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.size() == 0)
+            return 0;
+        
+        vector<int> next(needle.size(), 0);
+
+        for (int left = 0, right = 1; right < needle.size(); right++) {
+            while (left > 0 && needle[left] != needle[right])
+                left = next[left - 1];
+            if (needle[left] == needle[right])
+                left++;
+            next[right] = left;
+        }
+        for (int& num : next)
+            cout << num << ", ";
+        for (int i = 0, j = 0; i < haystack.size(); i++) {
+            while (j > 0 && haystack[i] != needle[j])
+                j = next[j - 1];
+            if (haystack[i] == needle[j])
+                j++;
+            if (j == needle.size())
+                return i - j + 1;
+        }
+        return -1;
+    }
+};
