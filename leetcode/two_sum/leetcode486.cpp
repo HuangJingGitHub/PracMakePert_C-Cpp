e903aa6ab70535131ff2fbc7b79d4aad584b8eb2
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-02-16 22:57:45 +0800

    Update leetcode486.cpp

--- leetcode/two_sum/leetcode486.cpp
+++ leetcode/two_sum/leetcode486.cpp
@@ -6,9 +6,9 @@ public:
         // dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
         for (int i = 0; i < nums.size(); i++)
             dp[i][i] = nums[i];
-        for (int row = nums.size() - 2; row >= 0; row--) 
-            for (int col = row + 1; col < nums.size(); col++)
-                dp[row][col] = max(nums[row] - dp[row + 1][col], nums[col] - dp[row][col - 1]);
+        for (int i = nums.size() - 2; i >= 0; i--) 
+            for (int j = i + 1; j < nums.size(); j++)
+                dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
 
         return dp[0][nums.size() - 1] >= 0;
     }
