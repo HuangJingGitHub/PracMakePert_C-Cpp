ad72ec903b8835f19ea7b9b83e931cdfed95c2c4
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-02-25 22:48:55 +0800

    Update leetcode_22.cpp

--- leetcode/two_sum/leetcode_22.cpp
+++ leetcode/two_sum/leetcode_22.cpp
@@ -8,18 +8,40 @@ public:
         vector<vector<string>> dp(n+1);
         dp[0] = {""};
         dp[1] = {"()"};
-        for (int i = 2; i <= n; i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
                 for (string p : dp[j])
-                 for (string q : dp[i - j - 1])
-                 {
+                 for (string q : dp[i - j - 1]) {
                     string str = "(" + p + ")" + q;
                     dp[i].push_back(str);    
                  }
             }
         }
         return dp[n];
+    }
+};
+
+
+// dfs
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        dfs("", 0, 0, n, res);
+        return res;
+    }
+
+    void dfs(string curStr, int left, int right, int n, vector<string>& res) {
+        if (left == n && right == n) {
+            res.push_back(curStr);
+            return;
+        }
+
+        if (left < right)
+            return;
+        if (left < n)
+            dfs(curStr + "(", left + 1, right, n, res);
+        if (right < n)
+            dfs(curStr + ")", left, right + 1, n, res);
     }
 };
