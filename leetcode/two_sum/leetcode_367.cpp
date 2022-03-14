c4ad3ec5926d29dae94c5483340415f4d8ca8eac
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-03-14 21:22:33 +0800

    Update leetcode_367.cpp

--- leetcode/two_sum/leetcode_367.cpp
+++ leetcode/two_sum/leetcode_367.cpp
@@ -11,3 +11,17 @@ public:
         return square == num;
     }
 };
+
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int i = 1;
+        while (i * i< num) {
+            if (num - i * i < 2 * i + 1)
+                return false;
+            i++;
+        }
+        return i * i == num;
+    }
+};
