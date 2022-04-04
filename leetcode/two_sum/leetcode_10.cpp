51de3a3e1f5f55c767bb9bca171970f2791ac19b
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-04-04 23:49:21 +0800

    Update leetcode_10.cpp

--- leetcode/two_sum/leetcode_10.cpp
+++ leetcode/two_sum/leetcode_10.cpp
@@ -14,3 +14,37 @@ public:
             return first && isMatch(s.substr(1), p.substr(1));
     }
 };
+
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        auto match = [&] (int i, int j) {
+            if (i == 0)
+                return false;
+            if (p[j - 1] == '.')
+                return true;
+            else
+                return s[i - 1] == p[j - 1];
+        };
+
+        vector<vector<int>> dp(s.size() + 1, vector<int>(p.size() + 1, false));
+        dp[0][0] = true;
+
+        for (int i = 0; i <= s.size(); i++) {
+            for (int j = 1; j <= p.size(); j++) {
+                if (p[j - 1] == '*') {
+                    dp[i][j] |= dp[i][j - 2];
+                    if (match(i, j - 1))
+                        dp[i][j] |= dp[i - 1][j];
+                }
+                else {
+                    if (match(i, j))
+                        dp[i][j] |= dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp.back().back();
+    }
+};
