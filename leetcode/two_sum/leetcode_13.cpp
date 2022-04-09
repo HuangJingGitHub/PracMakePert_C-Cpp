2843e7f3b2c413b3f35e3dda1464c64baba55005
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-04-09 23:59:31 +0800

    Update leetcode_13.cpp

--- leetcode/two_sum/leetcode_13.cpp
+++ leetcode/two_sum/leetcode_13.cpp
@@ -4,20 +4,17 @@
 
 using namespace std;
 
-class Solution_self{
+class Solution_self {
     public:
-    int romanToInt(string s)
-    {
+    int romanToInt(string s) {
         vector<string> romanStr = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", 
                                     "IX", "V", "IV", "I"};
         vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
         int resultInt = 0;
-        for (int i = 0; i < romanStr.size(); i++)
-        {
+        for (int i = 0; i < romanStr.size(); i++) {
             int j = romanStr[i].size();
-            while (s.substr(0, j) == romanStr[i])
-            {
+            while (s.substr(0, j) == romanStr[i]) {
                 resultInt += values[i];
                 s = s.substr(j, s.size());
                 cout << "result = " << resultInt << " s = " << s << endl;
@@ -28,8 +25,7 @@ class Solution_self{
     }
 };
 
-int main()
-{
+int main() {
     Solution_self solRef;
     cout << solRef.romanToInt("V");
 }
@@ -40,26 +36,23 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
 
-        for(int i = 0; i < nums.size() - 2; i++)
-        {
+        for(int i = 0; i < nums.size() - 2; i++) {
             if (nums[i] > 0)
                 break;
 
             int first = nums[i];
             int leftPointer = i + 1, rightPointer = nums.size() - 1;
-            while (leftPointer < rightPointer)
-            {
+            while (leftPointer < rightPointer) {
                 cout << nums[i] << "+" << nums[leftPointer] << "+" << nums[rightPointer] << endl;
                 if (nums[leftPointer] + nums[rightPointer] + nums[first] < 0){
                     leftPointer++;
                     cout << nums[i] << "+" << nums[leftPointer] << "+" << nums[rightPointer] << endl;
                 }
-                else if (nums[leftPointer] + nums[rightPointer] + nums[first] > 0){
+                else if (nums[leftPointer] + nums[rightPointer] + nums[first] > 0) {
                     rightPointer--;
                 }
 
-                else if (nums[leftPointer] + nums[rightPointer] + nums[first] == 0)
-                {
+                else if (nums[leftPointer] + nums[rightPointer] + nums[first] == 0) {
                     vector<int> triplet{nums[first], nums[leftPointer], nums[rightPointer]};
                     result.push_back(triplet);
                     leftPointer++;
