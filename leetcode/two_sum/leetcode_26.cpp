d6a94cfd780bae4740180e5abe0f4f791cd8440c
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-04-16 23:28:40 +0800

    Update leetcode_26.cpp

--- leetcode/two_sum/leetcode_26.cpp
+++ leetcode/two_sum/leetcode_26.cpp
@@ -17,10 +17,14 @@ public:
 // Faster with fewer manipulations.
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int newLength = 0;
-        for (int i = 0; i < nums.size(); i++)
-         if (nums[i] != val) nums[newLength++] = nums[i];  
-        return newLength;
+int removeDuplicates(vector<int>& nums) {
+	if (nums.size() < 2) return nums.size();
+	int j = 0;
+	for (int i = 1; i < nums.size(); i++)
+		if (nums[j] != nums[i]) {
+            j++;
+            nums[j] = nums[i];
+        }
+	return ++j;
     }
 };
