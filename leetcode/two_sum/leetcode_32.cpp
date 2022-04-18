49fda50a1d04301b6f908a307fc4d50f06062781
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-04-18 23:56:44 +0800

    Update leetcode_32.cpp

--- leetcode/two_sum/leetcode_32.cpp
+++ leetcode/two_sum/leetcode_32.cpp
@@ -11,25 +11,21 @@ public:
         stackStr.push_back(s[0]);
         strIndex.push_back(0);
 
-        for (int i = 1; i < s.length(); i++)
-        {
-            switch(s[i])
-            {
+        for (int i = 1; i < s.length(); i++) {
+            switch(s[i]) {
                 case '(':
                     stackStr.push_back(s[i]);
                     strIndex.push_back(i);
                     break;
 
                 case ')':
-                    if (stackStr.size() > 0 && stackStr[stackStr.size() - 1] == '(')
-                    {
+                    if (stackStr.size() > 0 && stackStr[stackStr.size() - 1] == '(') {
                         validMap[strIndex[strIndex.size() - 1]] = 1;
                         validMap[i] = 1;
                         stackStr.pop_back();
                         strIndex.pop_back();
                     }
-                    else
-                    {
+                    else {
                         stackStr.push_back(s[i]);
                         strIndex.push_back(i);
                     }
@@ -37,15 +33,12 @@ public:
             }
         }
 
-        for (int i = 0; i < validMap.size(); i++)
-        {
-            if (validMap[i] == 0)
-            {
+        for (int i = 0; i < validMap.size(); i++) {
+            if (validMap[i] == 0) {
                 longestValid = longestValid > counter ? longestValid : counter;
                 counter = 0;
             }
-            else if ( i == validMap.size() - 1)
-            {
+            else if ( i == validMap.size() - 1) {
                 counter += 1;
                 longestValid = longestValid > counter ? longestValid : counter;
             }
@@ -76,5 +69,29 @@ public:
             }
         }
         return maxans;
+    }
+};
+
+
+// dp solution
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        vector<int> dp(s.size(), 0);
+        int res = 0;
+
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == ')') {
+                int testIdx = i - dp[i - 1] - 1;
+                if (testIdx >= 0 && s[testIdx] == '(') {
+                    dp[i] = dp[i - 1] + 2;
+                    if (testIdx - 1 >= 0)
+                        dp[i] += dp[testIdx - 1];
+                }
+                res = max(res, dp[i]);
+            }
+        }
+
+        return res;
     }
 };
