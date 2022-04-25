7f2f9c007034ec727dd35cb8b1c2f29ebcb8d08a
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-04-25 23:53:28 +0800

    Update leetcode_48.cpp

--- leetcode/two_sum/leetcode_48.cpp
+++ leetcode/two_sum/leetcode_48.cpp
@@ -25,7 +25,7 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         for (int i = 0; i < n; i++)
-            for (int j = i; j < n; j++)
+            for (int j = i + 1; j < n; j++)
                 swap(matrix[i][j], matrix[j][i]);
         
         for (int col = 0; col < n / 2; col++)
