40bdc56fa72432a31a752182408f380b55500a22
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-05-02 22:31:34 +0800

    Update leetcode_59.cpp

--- leetcode/two_sum/leetcode_59.cpp
+++ leetcode/two_sum/leetcode_59.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> res;
-        vector<int> onesRow(n, 1);
-        for (int i = 0; i < n; i++)
-            res.push_back(onesRow);
+        vector<vector<int>> res(n, vector<int>(n, 1));
 
         int row = 0, col = 0, num = 1;
-        for (int i = 0; i < n-1; i++){
+        for (int i = 0; i < n-1; i++) {
             for (row = i, col = i; col < n - i; col++, num++)
                 res[row][col] = num;
             for (row++, col--; row < n - i; row++, num++)
