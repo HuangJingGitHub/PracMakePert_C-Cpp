8fff33a9a665335163e40aa1107dd454832825ad
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-05-12 11:00:49 +0800

    Update leetcode_72.cpp

--- leetcode/two_sum/leetcode_72.cpp
+++ leetcode/two_sum/leetcode_72.cpp
@@ -1,26 +1,24 @@
-// DP is the right thought to solve the problem, quite stuble solution.
+// DP is the right thought to solve the problem, quite subtle solution.
 // Can refer to the discussion and solution sections on LeetCode.
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int m = word1.length(),
-            n = word2.length();
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (int col = 0; col <= n; col++)
+            dp[0][col] = col;
+        for (int row = 0; row <= m; row++)
+            dp[row][0] = row;
         
-        vector<vector<int>> cost(m+1, vector<int>(n+1));
-
-        for (int i = 0; i < m+1; i++) 
-            cost[i][0] = i;
-        for (int i = 0; i < n+1; i++)
-            cost[0][i] = i;
-
-        for (int i = 1; i < m+1; i++)
-            for (int j = 1; j < n+1; j++){
-                if (word1[i-1] == word2[j-1])
-                    cost[i][j] = cost[i-1][j-1];
+        for (int row = 1; row <= m; row++)
+            for (int col = 1; col <= n; col++) {
+                // Note row - 1, col - 1 are indices in word1 and word2, -1 is needed.
+                if (word1[row - 1] == word2[col - 1])
+                    dp[row][col] = dp[row - 1][col - 1];
                 else
-                    cost[i][j] = 1 + min(cost[i-1][j-1], min(cost[i][j-1], cost[i-1][j]));
-            } 
-        
-        return cost[m][n];
+                    dp[row][col] = min(dp[row - 1][col - 1], min(dp[row - 1][col], dp[row][col - 1])) + 1;
+            }
+        return dp.back().back();
     }
 };
