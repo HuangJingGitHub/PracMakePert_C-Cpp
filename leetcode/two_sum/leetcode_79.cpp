e7b4044fef6d830be1af98e8847cb9df636c481f
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-05-15 20:23:35 +0800

    Update leetcode_79.cpp

--- leetcode/two_sum/leetcode_79.cpp
+++ leetcode/two_sum/leetcode_79.cpp
@@ -1,47 +1,35 @@
-// A very good example of traceback. Also pay attention to the details.
-// Quite interesting is the speed between passing by value and by reference:
 // word(string) passing by value: time 344 ms, storage: 151.3 MB
 // word(string) passing by reference: time 24 ms, storage: 7.7 MB. Such a big difference!
-
 class Solution {
 public:
-    bool traceback(int startRow, int startCol, vector<vector<char>>& board, string& word, int wordIdx)
-    {
-        if (board[startRow][startCol] != word[wordIdx]){
-            return false;
-        }
-
-        if (wordIdx == word.size()-1){
-            return true; 
-        }
-
-        char currentChar = board[startRow][startCol];
-        board[startRow][startCol] = 0;
-        wordIdx++;
-        if ((startCol < board[0].size()-1 && traceback(startRow, startCol+1, board, word, wordIdx)) ||
-            (startRow > 0 && traceback(startRow-1, startCol, board, word, wordIdx)) ||
-            (startCol > 0 && traceback(startRow, startCol-1, board, word, wordIdx)) || 
-            (startRow < board.size()-1 && traceback(startRow+1, startCol, board, word, wordIdx))){
-                return true;
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size();
+        for (int row = 0; row < m; row++)
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] == word[0]) 
+                    if (dfs(0, board, word, row, col))
+                        return true;
             }
         
-        board[startRow][startCol] = currentChar;
         return false;
-
     }
 
-    bool exist(vector<vector<char>>& board, string word) {
-        if (board.empty() || board[0].empty())
+    bool dfs(int wordIdx, vector<vector<char>>& board, string& word, int row, int col) {
+        if (board[row][col] != word[wordIdx])
             return false;
+        if (wordIdx == word.size() - 1)
+            return true;
         
-        int row = board.size(), col = board[0].size();
+        char orginalCh = board[row][col];
+        board[row][col] = '*';
 
-        for (int i = 0; i < row; i++)
-            for (int j = 0; j < col; j++){
-                    if (traceback(i, j, board, word, 0))
-                        return true;
-                }
+        if ((row < board.size() - 1 && dfs(wordIdx + 1, board, word, row + 1, col))
+            || (row > 0 && dfs(wordIdx + 1, board, word, row - 1, col))
+            || (col > 0 && dfs(wordIdx + 1, board, word, row, col - 1))
+            || (col < board[0].size() - 1 && dfs(wordIdx + 1, board, word, row, col + 1)))
+            return true;
         
+        board[row][col] = orginalCh;
         return false;
     }
 };
