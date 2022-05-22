e1e304e730a874181a9a79b5f26bc7b2027ee9fe
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-05-22 23:33:11 +0800

    Update leetcode_74.cpp

--- leetcode/two_sum/leetcode_74.cpp
+++ leetcode/two_sum/leetcode_74.cpp
@@ -29,3 +29,25 @@ public:
         return false;
     }
 };
+
+
+√class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int left = 0, right = m * n - 1, mid, midRow, midCol;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            midRow = mid / n;
+            midCol = mid % n;
+            if (matrix[midRow][midCol] == target)
+                return true;
+            else if (matrix[midRow][midCol] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+};
