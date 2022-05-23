7102328a263b8dea4513091d1ac2e598b8b2bc8c
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-05-23 22:44:26 +0800

    Update leetcode_81.cpp

--- leetcode/two_sum/leetcode_81.cpp
+++ leetcode/two_sum/leetcode_81.cpp
@@ -6,22 +6,20 @@ public:
             return false;
         
         int left = 0, right = nums.size() - 1, mid;
-        while (left <= right){
+        while (left <= right) {
             mid = (left + right) / 2;
-
             if (nums[mid] == target)
                 return true;
-            
             if (nums[left] < nums[mid]){
                 if (nums[left] <= target && target < nums[mid])
                     right = mid - 1;
                 else
                     left = mid + 1;
             }
-            else if (nums[left] == nums[mid]){
+            else if (nums[left] == nums[mid]) {
                 left++;
             }
-            else{
+            else {
                 if (nums[mid] < target && target <= nums[right])
                     left = mid + 1;
                 else
