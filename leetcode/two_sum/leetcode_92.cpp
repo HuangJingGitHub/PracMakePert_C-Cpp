aadc017e8d6593130e5a02cabb2814a7ae90e98e
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-06-04 12:42:51 +0800

    Update leetcode_92.cpp

--- leetcode/two_sum/leetcode_92.cpp
+++ leetcode/two_sum/leetcode_92.cpp
@@ -13,18 +13,52 @@ public:
         p->next = head;
         head = p;
 
-        for (int i = 0; i < n; i++){
-            if (i < m){
+        for (int i = 0; i < n; i++) {
+            if (i < m) {
                 pre = p;
                 p = p->next;
             }
-            else{  // Keep inserting the nodes to the back of m-1 th node
+            else {  // Keep inserting the nodes to the back of m-1 th node
                 ListNode  *tmp = p->next->next;
                 p->next->next = pre->next;
                 pre->next = p->next;
                 p->next = tmp;
             }
         }
+        
        return head->next; 
+    }
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode* dummy = new ListNode(0, head), *pre = dummy;
+        for (int i = 1; i < left; i++)
+            pre = pre->next;
+        
+        ListNode *pt = pre->next->next, *reverseEnd = pre->next, *nextPt = nullptr;
+        for (int i = left + 1; i <= right; i++) {
+            nextPt = pt->next;
+            pt->next = pre->next;
+            pre->next = pt;
+            pt = nextPt;
+        }
+        reverseEnd->next = pt;
+
+        head = dummy->next;
+        delete dummy;
+        return head;
     }
 };
