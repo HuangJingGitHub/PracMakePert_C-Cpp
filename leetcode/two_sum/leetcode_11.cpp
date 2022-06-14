5f5b638c527381bc2fc35ebe599636a31831fa93
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-06-14 23:54:49 +0800

    Update leetcode_11.cpp

--- leetcode/two_sum/leetcode_11.cpp
+++ leetcode/two_sum/leetcode_11.cpp
@@ -28,3 +28,24 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int res = 0;
+        
+        int left = 0, right = height.size() - 1;
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                res = max(res, height[left] * (right - left));
+                left++;
+            }
+            else {
+                res = max(res, height[right] * (right - left));
+                right--;
+            }
+        }
+        return res;
+    }
+};
