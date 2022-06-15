02bfdea54534dc88d2b2936ae63e8167b0507346
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-06-15 23:50:32 +0800

    Update leetcode_387.cpp

--- leetcode/two_sum/leetcode_387.cpp
+++ leetcode/two_sum/leetcode_387.cpp
@@ -19,3 +19,18 @@ public:
         return res == INT_MAX ? -1 : res;
     }
 };
+
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<char, int> frequencyLog;
+        
+        for (char& ch : s)
+            frequencyLog[ch]++;
+        for (int i = 0; i < s.size(); i++)
+            if (frequencyLog[s[i]] == 1)
+                return i;
+        return -1;
+    }
+};
