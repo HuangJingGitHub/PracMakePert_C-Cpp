eed3dafc0949d9e83a5675b9c567d3da0e7f1a50
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-06-27 23:16:53 +0800

    Update leetcode_1.cpp

--- leetcode/two_sum/leetcode_1.cpp
+++ leetcode/two_sum/leetcode_1.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res;
-        unordered_map<int, int> hashMap;
+        unordered_map<int, int> numToIdx;
         
         for (int i = 0; i < nums.size(); i++){
-            if (hashMap.find(nums[i]) != hashMap.end()){
-                res.push_back(hashMap.find(nums[i])->second);
+            if (numToIdx.find(target - nums[i]) != numToIdx.end()){
+                res.push_back(numToIdx.find(target - nums[i])->second);
                 res.push_back(i);
                 return res;
             }
-            hashMap.insert(std::make_pair(target-nums[i], i));  // Instead store the pair (nums[i], i), store (target - nums[i], i)
+            numToIdx.insert(std::make_pair(nums[i], i));
         }
         return res;
     }
