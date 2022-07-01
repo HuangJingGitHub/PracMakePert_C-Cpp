65dc5be4849a54257152eeccb4c8b730993bdb78
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-07-01 22:44:15 +0800

    Update leetcode_154.cpp

--- leetcode/two_sum/leetcode_154.cpp
+++ leetcode/two_sum/leetcode_154.cpp
@@ -7,7 +7,7 @@ public:
             mid = left + (right - left) / 2;
             if (nums[mid] < nums[right])
                 right = mid;
-            else if (nums[mid] == nums[right])   // duplicate case e.g. [3,3,3,3,1,3,3]
+            else if (nums[mid] == nums[right])   // duplicate case, e.g. [3,3,3,3,1,3,3]
                 right--;
             else
                 left = mid + 1;
