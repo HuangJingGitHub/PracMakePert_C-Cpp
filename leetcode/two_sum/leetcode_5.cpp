8eef08e63ed2d7cc7a7b205a70afcea68daf1782
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-07-03 23:36:07 +0800

    Update leetcode_5.cpp

--- leetcode/two_sum/leetcode_5.cpp
+++ leetcode/two_sum/leetcode_5.cpp
@@ -35,3 +35,28 @@ public:
         return true;
     } 
 };
+
+
+// two dimensional dp solution
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+        int maxLen = 1, startIdx = 0;
+
+        for (int i = s.size() - 1; i >= 0; i--)
+            for (int j = i; j < s.size(); j++) {
+                if (j - i <= 2) {
+                    dp[i][j] = s[i] == s[j];
+                }
+                else {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] == true && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    startIdx = i;
+                }
+            }
+        return s.substr(startIdx, maxLen);
+    }
+};
