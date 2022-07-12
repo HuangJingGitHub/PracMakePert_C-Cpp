92a2519708a25680e338e6e324ec63ef4111cae5
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-07-12 23:27:59 +0800

    Create leetcode_1252.cpp

---
+++ leetcode/two_sum/leetcode_1252.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int oddCells(int m, int n, vector<vector<int>>& indices) {
+        int res = 0;
+        vector<int> addTimesRow(m, 0), addTimesCol(n, 0);
+
+        for (auto& indixPair : indices) {
+            addTimesRow[indixPair[0]]++;
+            addTimesCol[indixPair[1]]++;
+        }
+
+        int addTimesOddRow = 0, addTimesEvenRow = 0;
+        for (int i = 0; i < m; i++) {
+            if (addTimesRow[i] % 2 == 0)
+                addTimesEvenRow++;
+            else
+                addTimesOddRow++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (addTimesCol[i] % 2 == 0)
+                res += addTimesOddRow;
+            else
+                res += addTimesEvenRow;
+        }
+        return res;
+    }
+};
