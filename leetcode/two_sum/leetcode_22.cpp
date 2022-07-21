956c23d70b524d22bde106de06cc753aea2132e2
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-07-21 23:48:59 +0800

    Update leetcode_22.cpp

--- leetcode/two_sum/leetcode_22.cpp
+++ leetcode/two_sum/leetcode_22.cpp
@@ -5,16 +5,16 @@ public:
         if (n == 0) return {};
         if (n == 1) return {"()"};
 
-        vector<vector<string>> dp(n+1);
+        vector<vector<string>> dp(n + 1);
         dp[0] = {""};
         dp[1] = {"()"};
         for (int i = 2; i <= n; i++) {
             for (int j = 0; j < i; j++) {
                 for (string p : dp[j])
-                 for (string q : dp[i - j - 1]) {
-                    string str = "(" + p + ")" + q;
-                    dp[i].push_back(str);    
-                 }
+                    for (string q : dp[i - j - 1]) {
+                        string str = "(" + p + ")" + q;
+                        dp[i].push_back(str);    
+                    }
             }
         }
         return dp[n];
