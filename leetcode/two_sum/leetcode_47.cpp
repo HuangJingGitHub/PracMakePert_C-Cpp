997fe7b2839640368f2a04c88b5bcd48a11deaef
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-08-10 23:53:38 +0800

    Update leetcode_47.cpp

--- leetcode/two_sum/leetcode_47.cpp
+++ leetcode/two_sum/leetcode_47.cpp
@@ -4,24 +4,22 @@ public:
         int len = nums.size();
         vector<vector<int>> res;
 
-        if (len == 0 || len == 1)
-        {
+        if (len == 0 || len == 1) {
             res.push_back(nums);
             return res;
         }
 
         vector<bool> used(len, false);
         vector<int> path;
-
         sort(nums.begin(), nums.end());   // First sort.
         dfs(nums, len, 0, path, used, res);
         return res;
     }
 
 private:
-    void dfs(vector<int> nums, int len, int depth, vector<int> path, vector<bool> used, 
+    void dfs(vector<int>& nums, int len, int depth, vector<int>& path, vector<bool>& used, 
             vector<vector<int>>& res) {
-            if (depth == len){
+            if (depth == len) {
                 res.push_back(path);
                 return;
             }
@@ -30,10 +28,8 @@ private:
                 if (!used[i]){
                     if (i > 0 && nums[i-1] == nums[i] && !used[i-1]) // Add judgement.
                         continue;
-
                     path.push_back(nums[i]);
                     used[i] = true;
-
                     dfs(nums, len, depth + 1, path, used, res);
                     used[i] = false;
                     path.pop_back();
