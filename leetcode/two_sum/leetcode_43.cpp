1674e1111b0def8877b192ddba5a81d88e0ebb5d
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-08-11 22:45:10 +0800

    Update leetcode_43.cpp

--- leetcode/two_sum/leetcode_43.cpp
+++ leetcode/two_sum/leetcode_43.cpp
@@ -41,15 +41,15 @@ public:
         int n1 = num1.size(), n2 = num2.size();
         string  res(n1 + n2, '0');
 
-        for (int i = n2 - 1; i >= 0; i--){
-            for (int j = n1 - 1; j >= 0; j--){
+        for (int i = n2 - 1; i >= 0; i--) {
+            for (int j = n1 - 1; j >= 0; j--) {
                 int temp = (res[i + j + 1] - '0') + (num1[j] - '0') * (num2[i] - '0');
                 res[i + j + 1] = temp % 10 + '0';
                 res[i + j] += temp / 10;
             }
         }
         
-        for (int i = 0; i < res.size(); i++){
+        for (int i = 0; i < res.size(); i++) {
             if (res[i] != '0')
                 return res.substr(i);
         }
