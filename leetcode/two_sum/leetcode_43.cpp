aa64d02648d9ecb8ce9fba9f3b812be959dd7c75
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-08-11 22:44:46 +0800

    Update leetcode_43.cpp

--- leetcode/two_sum/leetcode_43.cpp
+++ leetcode/two_sum/leetcode_43.cpp
@@ -39,13 +39,13 @@ class Solution {
 public:
     string multiply(string num1, string num2) {
         int n1 = num1.size(), n2 = num2.size();
-        string  res(n1+n2, '0');
+        string  res(n1 + n2, '0');
 
         for (int i = n2 - 1; i >= 0; i--){
             for (int j = n1 - 1; j >= 0; j--){
-                int temp = (res[i+j+1] - '0') + (num1[j] - '0') * (num2[i] - '0');
-                res[i+j+1] = temp % 10 + '0';
-                res[i+j] += temp / 10;
+                int temp = (res[i + j + 1] - '0') + (num1[j] - '0') * (num2[i] - '0');
+                res[i + j + 1] = temp % 10 + '0';
+                res[i + j] += temp / 10;
             }
         }
         
