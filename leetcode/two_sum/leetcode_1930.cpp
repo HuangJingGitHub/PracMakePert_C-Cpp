30b63d22f6f419db0ed5615d14b9b70a7a0f2c05
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-08-17 22:48:11 +0800

    Create leetcode_1930.cpp

---
+++ leetcode/two_sum/leetcode_1930.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int res  = 0;
+        map<int, vector<int>> charToindex;
+
+        for (int i = 0; i < s.size(); i++) 
+            charToindex[s[i]].push_back[i];
+        
+        for (auto it = charToindex.begin(); it != charToindex.end(); it++) {
+            if (it->second.size() < 2)
+                continue;
+            int startIdx = it->second.front(), endIdx = it->second.back();
+            set<char> distinctChar;
+            for (int i = startIdx + 1; i < endIdx; i++)
+                distinctChar.insert(s[i]);
+            res += distinctChar.size();
+        }
+
+        return res;
+    }
+};
