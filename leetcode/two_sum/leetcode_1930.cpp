0454ffedd0198fb24f0a63291e9f4babaa38678b
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-08-18 23:59:13 +0800

    Update leetcode_1930.cpp

--- leetcode/two_sum/leetcode_1930.cpp
+++ leetcode/two_sum/leetcode_1930.cpp
@@ -10,6 +10,7 @@ public:
         for (auto it = charToindex.begin(); it != charToindex.end(); it++) {
             if (it->second.size() < 2)
                 continue;
+            
             int startIdx = it->second.front(), endIdx = it->second.back();
             set<char> distinctChar;
             for (int i = startIdx + 1; i < endIdx; i++)
