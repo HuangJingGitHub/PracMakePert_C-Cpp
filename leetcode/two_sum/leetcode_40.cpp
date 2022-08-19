00c17262b7484f14224858ba2c160913b7d1d0e2
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-08-19 22:48:17 +0800

    Update leetcode_40.cpp

--- leetcode/two_sum/leetcode_40.cpp
+++ leetcode/two_sum/leetcode_40.cpp
@@ -5,16 +5,16 @@ private:
     vector<int> path;
 
 public:
-    void backtrack(int start, int target){
-        if (target == 0){
+    void backtrack(int start, int target) {
+        if (target == 0) {
             res.push_back(path);
             return;
         }
         for (int i = start; i < candidates.size() && target - candidates[i] >= 0; i++){
-            if (i > start && candidates[i] == candidates[i-1])
+            if (i > start && candidates[i] == candidates[i - 1])
                 continue;
             path.push_back(candidates[i]);
-            backtrack(i+1, target - candidates[i]);
+            backtrack(i + 1, target - candidates[i]);
             path.pop_back();
         }
     }
