9217dee4c4c1b1819b51a6f0c8ba77ba59afb505
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-09-03 22:26:30 +0800

    Update leetcode_59.cpp

--- leetcode/two_sum/leetcode_59.cpp
+++ leetcode/two_sum/leetcode_59.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int>> res(n, vector<int>(n, 1));
 
         int row = 0, col = 0, num = 1;
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             for (row = i, col = i; col < n - i; col++, num++)
                 res[row][col] = num;
             for (row++, col--; row < n - i; row++, num++)
