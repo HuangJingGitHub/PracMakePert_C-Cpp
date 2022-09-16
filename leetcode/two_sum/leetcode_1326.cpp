270e5cbfa1b200365a98e34e918c15f4492eccf0
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-09-16 21:06:43 +0800

    Create leetcode_1326.cpp

---
+++ leetcode/two_sum/leetcode_1326.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minTaps(int n, vector<int>& ranges) {
+        int res = 0;
+        vector<pair<int, int>> intervals;
+        
+        for (int i = 0; i < ranges.size(); i++)
+            intervals.push_back(pair<int, int>(i - ranges[i], i + ranges[i]));
+        sort(intervals.begin(), intervals.end());
+
+        int max_right = 0, idx = 0;
+        while (max_right < n) {
+            int next_max_right = -1;
+            while (idx < intervals.size() && intervals[idx].first <= max_right) {
+                next_max_right = max(next_max_right, intervals[idx].second);
+                idx++;
+            }
+            if (next_max_right == -1)
+                return -1;
+            max_right = next_max_right;
+            res++;
+        }
+
+        return res;
+    }
+};
