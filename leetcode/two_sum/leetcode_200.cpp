7f25ba225f215d4940b84b054b4a276a1216ae68
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-09-22 11:02:47 +0800

    Update leetcode_200.cpp

--- leetcode/two_sum/leetcode_200.cpp
+++ leetcode/two_sum/leetcode_200.cpp
@@ -63,3 +63,35 @@ public:
         }    
     }
 };
+
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int res = 0, m = grid.size(), n = grid[0].size();
+
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (grid[row][col] == '1') {
+                    dfs(grid, row, col);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    // Use dfs to convert the current island to water
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        if (row < 0 || row == grid.size() || col < 0 || col == grid[0].size())
+            return;
+        if (grid[row][col] == '0')
+            return;
+        
+        grid[row][col] = '0';
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+    }
+};
