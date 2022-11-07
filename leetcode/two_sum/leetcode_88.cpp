554bde2612e7895c4b8d9c1e401fbda0a5a910c2
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-11-07 22:26:34 +0800

    Update leetcode_88.cpp

--- leetcode/two_sum/leetcode_88.cpp
+++ leetcode/two_sum/leetcode_88.cpp
@@ -1,10 +1,9 @@
 // The commented parts is a feasible but not simple enough solution. The uncommented part is simper and better as for speed and memory cost.
 class Solution {
 public:
-    /*void placeEntry(vector<int> &nums, int num, int &start, int end)
-    {
-        for ( ;start <= end; start++){
-            if (nums[start] >= num){
+    /*void placeEntry(vector<int> &nums, int num, int &start, int end) {
+        for ( ;start <= end; start++) {
+            if (nums[start] >= num) {
                 break;
             }
         }
@@ -15,13 +14,13 @@ public:
 
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
        /* int start = 0, end = m-1;
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             placeEntry(nums1, nums2[i], start, end);
             end++;
         }
         nums1.erase(nums1.begin()+m+n, nums1.end());*/
         int resIdx = m-- + --n;
-        while (n>=0){
+        while (n >= 0){
             nums1[resIdx--] = (m >= 0 && nums1[m] > nums2[n]) ? nums1[m--] : nums2[n--];
         }
     }
