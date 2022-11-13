48aeb1a042e1ae7759c65f34d36d0b37e1991813
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-11-13 21:23:52 +0800

    Create leetcode_1910.cpp

---
+++ leetcode/two_sum/leetcode_1910.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        vector<int> lps(part.size(), 0);
+        for (int i = 1, len = 0; i < part.size(); ) {
+            if (part[len] == part[i]) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+                if (len != 0)
+                    len = lps[len - 1];
+                else {
+                    lps[i] = 0; 
+                    i++;
+                }
+            }
+        }
+
+        int startIdx = strStr(s, part, lps);
+        while (startIdx != -1) {
+            s.erase(startIdx, part.size());
+            startIdx = strStr(s, part, lps);
+        }
+
+        return s;
+    }
+
+    int strStr(string& s, string& needle, vector<int>& lps) {
+        int i  = 0, j = 0;
+        while(s.size() - i >= needle.size() - j) {
+            if (j == needle.size())
+                return i - j;
+
+            if (s[i] == needle[j]) {
+                i++;
+                j++;
+            }
+            else {
+                if (j > 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+        return -1;
+    }    
+};
