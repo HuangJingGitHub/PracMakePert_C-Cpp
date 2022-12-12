e6219f664f41a3190c1bd397749a11a8b446132d
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-12-12 20:56:33 +0800

    Update leetcode_409.cpp

--- leetcode/two_sum/leetcode_409.cpp
+++ leetcode/two_sum/leetcode_409.cpp
@@ -7,6 +7,19 @@ public:
         for (int i = 0; i < s.size(); i++)
             charFrequency[s[i]]++;
         
+        bool oddAdded = false;
+        for (auto it = charFrequency.begin(); it != charFrequency.end(); it++) {
+            if (it->second % 2 == 0)
+                res += it->second;
+            else if (oddAdded == false) {
+                res += it->second;
+                oddAdded = true;
+            }
+            else
+                res += it->second - 1;
+
+        }
+
         return res;
     }
 };
