eeb03c7d20e7549e93239a3041050a4b0876c944
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2022-12-30 13:07:32 +0800

    Update leetcode_423.cpp

--- leetcode/two_sum/leetcode_423.cpp
+++ leetcode/two_sum/leetcode_423.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     string originalDigits(string s) {
-        vector<int> nums;
         vector<char> keyChar = {'z', 'w', 'x', 's', 'g', 't', 'v', 'f', 'o', 'i'};
         vector<string> mapStr = {"zero", "two", "six", "seven", "eight", "three", "five", "four", "one", "nine"};
         vector<int> mapNum = {0, 2, 6, 7, 8, 3, 5, 4, 1, 9};
+        unordered_map<int, int> numFrequency;
 
         unordered_map<char, int> charFrequency;
         for (int i = 0; i < s.size(); i++)
@@ -12,19 +12,17 @@ public:
 
         for (int i = 0; i < keyChar.size(); i++) {
             char curKeyChar = keyChar[i];
-
             int numTimes = charFrequency[curKeyChar];
-            for (int j = 0; j < numTimes; j++)
-                nums.push_back(mapNum[i]);
+            numFrequency[mapNum[i]] = numTimes;
             for (char c : mapStr[i])
                 charFrequency[c] -= numTimes;
         }        
-
-        sort(nums.begin(), nums.end());
         
-        string res = "";
-        for (int& num : nums)
-            res += to_string(num);
+        string res;
+        for (int i = 0; i <= 9; i++)
+            if (numFrequency.find(i) != numFrequency.end())
+                for (int j = 0; j < numFrequency[i]; j++)
+                    res += to_string(i);
         return res;
     }
 };
