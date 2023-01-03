9324d050e2a2db5a4f3ef98eb429483c6c7b3c52
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-01-03 23:57:36 +0800

    Update leetcode_424.cpp

--- leetcode/two_sum/leetcode_424.cpp
+++ leetcode/two_sum/leetcode_424.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char, vector<int>> idxLog;
-        for (int i = 0; i < s.size(); i++)
-            idxLog[s[i]].push_back(i);
-        
-        return 0;
+        unordered_map<char, vector<int>> charFrequency;
+        int left = 0, right = 0, maxLen = 0;
+
+
+        return right - left;
     }
 };
