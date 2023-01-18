c4a61abd59e9eab42805a2b2cae8f98eebd1cc9b
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-01-18 14:18:46 +0800

    Update leetcode_433.cpp

--- leetcode/two_sum/leetcode_433.cpp
+++ leetcode/two_sum/leetcode_433.cpp
@@ -1,6 +1,49 @@
 class Solution {
 public:
+    bool canMutate(string& a, string& b) {
+        int differentNum = 0;
+        for (int i = 0; i < 8; i++)
+            if (a[i] != b[i])
+                differentNum += 1;
+        
+        return differentNum == 1;
+    }
+
+
     int minMutation(string startGene, string endGene, vector<string>& bank) {
-        return 0;
+        bank.push_back(startGene);
+        int start = bank.size() - 1, goal;
+        vector<vector<int>> edge(bank.size());
+
+        for (int i = 0; i < bank.size(); i++) {
+            if (bank[i] == endGene)
+                goal = i;
+            for (int j = i + 1; j < bank.size(); j++) {
+                if (canMutate(bank[i], bank[j])) {
+                    edge[i].push_back(j);
+                    edge[j].push_back(i);
+                }
+            }
+        }
+
+        // BFS
+        queue<pair<int, int>> que;
+        vector<bool> visited(bank.size(), false);
+        que.push(make_pair(start, 0));
+        visited[start] = true;
+        while (que.empty() == false) {
+            int index = que.front().first, cost = que.front().second;
+            que.pop();
+            for (auto it = edge[index].begin(); it != edge[index].end(); it++) {
+                if (*it == goal)
+                    return cost + 1;
+                if (visited[*it] == false) {
+                    que.push(make_pair(*it, cost + 1));
+                    visited[*it] = true;
+                }
+            }
+        }
+
+        return -1;
     }
 };
