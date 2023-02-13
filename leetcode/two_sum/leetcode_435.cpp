a13c3bc2230e3292480c15f3bcc67c979dc6b02a
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-02-13 23:59:27 +0800

    Update leetcode_435.cpp

--- leetcode/two_sum/leetcode_435.cpp
+++ leetcode/two_sum/leetcode_435.cpp
@@ -5,9 +5,9 @@ public:
     }
 
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), cmp);
-        
+        sort(intervals.begin(), intervals.end(), cmp); 
         int res = 0;
+        
         int end = intervals[0][1];
         for (int i = 1; i < intervals.size(); i++) {
             if (intervals[i][0] >= end)
