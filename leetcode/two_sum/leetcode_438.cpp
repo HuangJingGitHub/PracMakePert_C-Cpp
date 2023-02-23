645b59db546908a1500fc428b003ae3e70fd178e
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-02-23 12:05:58 +0800

    Update leetcode_438.cpp

--- leetcode/two_sum/leetcode_438.cpp
+++ leetcode/two_sum/leetcode_438.cpp
@@ -1,47 +1,47 @@
 class Solution {   
 public:
-    vector<int> findAnagrams(string s, string p)
+    vector<int> findAnagrams(string s, string p) {
+        if (s.size() < p.size())
+            return {};
+            
         vector<int> res;
         unordered_map<char, int> charFrequency;
-        set<char> charSet;
         for (char ch : p) {
             charFrequency[ch]++;
-            charSet.insert(ch);
         }
 
         vector<unordered_map<char, int>> occurenceLog(s.size());
-        for (auto it = charSet.begin(); it != charSet.end(); it++)
-            occurenceLog[0][*it] = 0;
+        for (auto it = charFrequency.begin(); it != charFrequency.end(); it++)
+            occurenceLog[0][it->first] = 0;
         for (int i = 1; i < s.size(); i++)
             occurenceLog[i] = occurenceLog[i - 1];
 
-        if (charSet.find(s[0]) != charSet.end())
+        if (charFrequency.find(s[0]) != charFrequency.end())
             occurenceLog[0][s[0]]++;
-        for (i = 1; i < s.size(); i++) {
+        for (int i = 1; i < s.size(); i++) {
             occurenceLog[i] = occurenceLog[i - 1];
-            if (charSet.find(s[i]) != charSet.end())
+            if (charFrequency.find(s[i]) != charFrequency.end())
                 occurenceLog[i][s[i]]++;
         }
 
         for (int i = 0; i <= s.size() - p.size(); i++) 
-            if (charSet.find(s[i]) != charSet.end()) {
+            if (charFrequency.find(s[i]) != charFrequency.end()) {
                 int endIdx = i + p.size() - 1;
-                if (occurenceLog[endIdx][s[i]] - occurenceLog[i][s[i]]] + 1 != charFrequency[s[i]])
+                if (occurenceLog[endIdx][s[i]] - occurenceLog[i][s[i]] + 1 != charFrequency[s[i]])
                     continue;
 
-                auto it = charSet.begin();
-                for (; it != charSet.end(); it++) {
-                    char curChar = *it;
+                auto it = charFrequency.begin();
+                for (; it != charFrequency.end(); it++) {
+                    char curChar = it->first;
                     if (curChar == s[i])
                         continue;
 
                     if (occurenceLog[endIdx][curChar] - occurenceLog[i][curChar] != charFrequency[curChar])
                         break;
                 }
-                if (it == charSet.end())
+                if (it == charFrequency.end())
                     res.push_back(i);
             }
-
             return res;
     }
 };
