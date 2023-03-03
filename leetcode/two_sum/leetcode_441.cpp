e6f46958d71cd2432076932a38abd548a0b83f64
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-03-03 23:57:22 +0800

    Update leetcode_441.cpp

--- leetcode/two_sum/leetcode_441.cpp
+++ leetcode/two_sum/leetcode_441.cpp
@@ -11,3 +11,26 @@ public:
         return res - 1;
     }
 };
+
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        if (n == 1)
+            return 1;
+            
+        long sum = 1, left = 1, right = n, mid;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            sum = mid * (mid + 1) / 2;
+            if (sum < n)
+                left = mid + 1;
+            else if (sum == n)
+                return mid;
+            else 
+                right = mid;
+        }
+
+        return left - 1;
+    }
+};
