cec0ab0996ecaee909b37ba00e05c3266ad28390
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-03-07 22:39:29 +0800

    Update leetcode_440.cpp

--- leetcode/two_sum/leetcode_440.cpp
+++ leetcode/two_sum/leetcode_440.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int findKthNumber(int n, int k) {
-        return 0;
+        int cur = 1;
+        k--;
+        
+        while (k > 0) {
+            long long left = cur;
+            long long right = cur + 1;
+            int node_num = 0;
+            
+            while (left <= n) {
+                node_num += min(right, (long long)(n + 1)) - left;
+                left *= 10;
+                right *= 10;
+            }
+            
+            if (node_num <= k) {
+                k -= node_num;
+                cur++;
+            }
+            else {
+                k--;
+                cur *= 10;
+            }
+        }
+        return cur;       
     }
 };
