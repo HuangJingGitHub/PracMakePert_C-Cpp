c45c1f99f054c966d8d6de69e62a1c017ca1d1d9
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-03-13 23:11:50 +0800

    Create leetcode_443.cpp

---
+++ leetcode/two_sum/leetcode_443.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        if (chars.size() == 1)
+            return 1;
+
+        int slowPt = 0, fastPt = 0, cnt = 1;
+        while (fastPt < chars.size()) {
+            cnt = 1;
+            while (fastPt + cnt < chars.size() && chars[fastPt] == chars[fastPt + cnt])
+                cnt++;
+            chars[slowPt] = chars[fastPt];
+            slowPt++;
+            fastPt += cnt;
+            if (cnt > 1) {
+                string cntStr = to_string(cnt);
+                for (char numChar : cntStr)
+                    chars[slowPt++] = numChar;
+            }
+        }
+
+        return slowPt;
+    }
+};
