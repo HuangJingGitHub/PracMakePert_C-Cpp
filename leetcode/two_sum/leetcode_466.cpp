f918351aab58fa6db7f6e7dd2a1dc0bafa2f6b5d
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-06-21 22:03:41 +0800

    Update leetcode_466.cpp

--- leetcode/two_sum/leetcode_466.cpp
+++ leetcode/two_sum/leetcode_466.cpp
@@ -7,28 +7,29 @@ public:
 			int len2 = s2.size();
 			int index1 = 0;
 			int index2 = 0;
+
 			while (index1 / len1 < n1) {
-				if (index1%len1 == len1 - 1) {
-					if (hash.count(index2%len2)) {
-						int cycleLen = index1 - hash[index2%len2].first;
-						int cycNum2 = (index2 - hash[index2%len2].second) / len2;  //每个循环有几个s2
-						int cycNum1 = cycleLen / len1;  //每个循环有几个S1
+				if (index1 % len1 == len1 - 1) {
+					if (hash.count(index2 % len2)) {
+						int cycleLen = index1 - hash[index2 % len2].first;
+						int cycNum2 = (index2 - hash[index2 % len2].second) / len2;  //每个循环有几个s2
+						int cycNum1 = cycleLen / len1;  //每个循环有几个s1
 						int cycle = (n1 - 1 - index1 / len1) / cycNum1;
 						res += cycle * cycNum2;
 						index1 += cycle * cycleLen;
 					}
 					else {
-						hash[index2%len2].first = index1;
-						hash[index2%len2].second = index2;
+						hash[index2 % len2].first = index1;
+						hash[index2 % len2].second = index2;
 					}
-			}
+				}
 			
-			if (s1[index1%len1] == s2[index2%len2]) {
-				if (index2 % len2 == len2 - 1)
-					res++;
-				index2++;
-			}
-			index1++;
+				if (s1[index1 % len1] == s2[index2 % len2]) {
+					if (index2 % len2 == len2 - 1)
+						res++;
+					index2++;
+				}
+				index1++;
 		}
 		return res / n2;
 	}
