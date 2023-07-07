056f68c2f589fcb4bd8e15bc0d49e645a9abfbf0
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-07-07 23:56:46 +0800

    Update leetcode_497.cpp

--- leetcode/two_sum/leetcode_497.cpp
+++ leetcode/two_sum/leetcode_497.cpp
@@ -13,8 +13,8 @@ public:
         accumulated_area_[0] = (rects_[0][2] - rects_[0][0] + 1) * (rects_[0][3] - rects_[0][1] + 1);
         for (int i = 1; i < rect_num_; i++)
             accumulated_area_[i] = accumulated_area_[i - 1] + (rects_[i][2] - rects_[i][0] + 1) * (rects_[i][3] - rects_[i][1] + 1);
-            // Note that side lengths need to be added by 1 since we are samping interger points covered by the rectangle uniformly, not 
-            // all points covered by the redctangle.
+            // Note that side lengths need to be added by 1 since we are sampling integer points covered by the rectangle uniformly, not 
+            // the area covered by the rectangle.
         total_area_ = accumulated_area_.back();
     }
     
