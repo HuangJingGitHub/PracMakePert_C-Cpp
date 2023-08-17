a92c8f22b813a2ec5bc5596d2774a93ef9c5a6a5
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-08-17 11:34:50 +0800

    Update leetcode_480.cpp

--- leetcode/two_sum/leetcode_480.cpp
+++ leetcode/two_sum/leetcode_480.cpp
@@ -9,6 +9,7 @@
  * };
  */
 
+// High time complexity
 class Solution {
 public:
     ListNode* list_head;
@@ -97,5 +98,79 @@ public:
         }
 
         pre_ptr->next = new_node;
+    }
+};
+
+
+// maintain max-heap, min-heap
+class Solution {
+public:
+    priority_queue<int> small;
+    priority_queue<int, vector<int>, greater<int>> big;
+    unordered_map<int, int> mp;
+
+    double get(int& k){
+        if (k % 2 == 1) return small.top();
+        else return 
+            ((long long)small.top() + big.top()) * 0.5;
+    }
+
+    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
+        for (int i = 0; i < k; i++) {
+            small.push(nums[i]);
+        }
+        for (int i = 0; i < k / 2; i++) {
+            big.push(small.top()); 
+            small.pop();
+        }
+
+        vector<double> ans{get(k)};
+        for(int i = k; i < nums.size(); i++){
+            int balance = 0;
+            int l = nums[i - k];
+            mp[l]++;
+            if (!small.empty() && l <= small.top()) 
+                balance--;
+            else 
+                balance++;
+            if (!small.empty() && nums[i] <= small.top()) {
+                small.push(nums[i]);
+                balance++;
+            }
+            else {
+                big.push(nums[i]);
+                balance--;
+            }
+            
+            while (!small.empty() && mp[small.top()] > 0){
+                mp[small.top()]--;
+                small.pop();
+            }
+            while (!big.empty() && mp[big.top()] > 0){
+                mp[big.top()]--;
+                big.pop();
+            }
+
+            if (balance > 0){
+                big.push(small.top());
+                small.pop();
+            }
+            if (balance < 0){
+                small.push(big.top());
+                big.pop();
+            }
+            while (!big.empty() && mp[big.top()] > 0) {
+                mp[big.top()]--;
+                big.pop();
+            }
+            while (!small.empty() && mp[small.top()] > 0) {
+                mp[small.top()]--;
+                small.pop();
+            }
+            
+            ans.push_back(get(k));
+            
+        }
+        return ans;
     }
 };
