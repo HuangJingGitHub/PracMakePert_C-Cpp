5b7e0da626e3d661f62115dc5ef980bc4b815c35
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-08-24 18:20:15 +0800

    Update leetcode_475.cpp

--- leetcode/two_sum/leetcode_475.cpp
+++ leetcode/two_sum/leetcode_475.cpp
@@ -5,26 +5,16 @@ public:
         sort(heaters.begin(), heaters.end());
 
         int res = 0;
-        vector<vector<double>> border(heaters.size(), vector<double>(2, 0.0));
-
-        border[0][0] = 1;
-        border[0][1] = heaters.size() == 1 ? 1e9 : heaters[0] + ((double)heaters[1] - heaters[0]) / 2;
-
-        for (int i = 1; i < heaters.size() - 1; i++) {
-            border[i][0] = heaters[i - 1] + ((double)heaters[i] - heaters[i - 1]) / 2;
-            border[i][1] = heaters[i] + ((double)heaters[i + 1] - heaters[i]) / 2;
-        }
-
-        if (heaters.size() > 1) {
-            border.back()[0] = heaters[heaters.size() - 2] + ((double)heaters.back() - heaters[heaters.size() - 2]) / 2;
-            border.back()[1] = 1e9;
-        }
-
         int heater_idx = 0;
         for (int i = 0; i < houses.size(); i++) {
-            while (!(houses[i] >= border[heater_idx][0] && houses[i] <= border[heater_idx][1]))
+            while (heater_idx < heaters.size() && heaters[heater_idx] <= houses[i])
                 heater_idx++;
-            res = max(res, abs(houses[i] - heaters[heater_idx]));
+            if (heater_idx == 0)
+                res = max(res, heaters[0] - houses[i]);
+            else if (heater_idx == heaters.size())
+                res = max(res, houses[i] - heaters.back());
+            else
+                res = max(res, min(heaters[heater_idx] - houses[i], houses[i] - heaters[heater_idx - 1]));
         }
 
         return res;
