9709048a1938cabb2d9f5cfe6bf88c6897877fb9
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-08-29 22:53:14 +0800

    Update leetcode_593.cpp

--- leetcode/two_sum/leetcode_593.cpp
+++ leetcode/two_sum/leetcode_593.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        int sqrt_dis_1_2 = computeSqrtDistance(p1, p2),
-            sqrt_dis_1_3 = computeSqrtDistance(p1, p3);
+        int sqrt_dis_1_2 = computeSqrDistance(p1, p2),
+            sqrt_dis_1_3 = computeSqrDistance(p1, p3);
     
         // Assume 2, 3 are neighbors of 1. 1 and 4 are on the diagonal, i.e., -1-2-4-3-1-
         vector<int> side_1_2, side_1_3, side_2_4, side_3_4;
@@ -21,10 +21,10 @@ public:
         side_2_4 = {p4[0] - p2[0], p4[1] - p2[1]};
         side_3_4 = {p4[0] - p3[0], p4[1] - p3[1]}; 
 
-        int len_1_2 = computeSqrtDistance(p1, p2),
-            len_1_3 = computeSqrtDistance(p1, p3),
-            len_2_4 = computeSqrtDistance(p2, p4),
-            len_3_4 = computeSqrtDistance(p3, p4),
+        int len_1_2 = computeSqrDistance(p1, p2),
+            len_1_3 = computeSqrDistance(p1, p3),
+            len_2_4 = computeSqrDistance(p2, p4),
+            len_3_4 = computeSqrDistance(p3, p4),
             inner_product_2_1_3 = computeInnerProduct(side_1_2, side_1_3),
             inner_product_1_2_4 = computeInnerProduct(side_1_2, side_2_4),
             inner_product_1_3_4 = computeInnerProduct(side_1_3, side_3_4);
@@ -33,7 +33,7 @@ public:
                inner_product_2_1_3 == 0 && inner_product_1_2_4 == 0 && inner_product_1_3_4 == 0;
     }
 
-    int computeSqrtDistance(vector<int>& p1, vector<int>& p2) {
+    int computeSqrDistance(vector<int>& p1, vector<int>& p2) {
         if (p1.size() != 2 || p2.size() != 2)
             return 0;
         return (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]);
@@ -43,5 +43,29 @@ public:
         if (v1.size() != 2 || v2.size() != 2)
             return 0;
         return v1[0] * v2[0] + v1[1] * v2[1];
+    }
+};
+
+
+// More compact. Only two side lengths are possible for segments linking two vertices of a square.
+class Solution {
+public:
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        set<int> sqr_dist_set;
+
+        sqr_dist_set.insert(computeSqrDistance(p1, p2));
+        sqr_dist_set.insert(computeSqrDistance(p1, p3));
+        sqr_dist_set.insert(computeSqrDistance(p1, p4));
+        sqr_dist_set.insert(computeSqrDistance(p2, p3));
+        sqr_dist_set.insert(computeSqrDistance(p2, p4));
+        sqr_dist_set.insert(computeSqrDistance(p3, p4));
+
+        return sqr_dist_set.size() == 2 && sqr_dist_set.count(0) == 0;
+    }
+
+    int computeSqrDistance(vector<int>& p1, vector<int>& p2) {
+        if (p1.size() != 2 || p2.size() != 2)
+            return 0;
+        return (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]);
     }
 };
