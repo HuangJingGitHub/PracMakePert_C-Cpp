f5b7af838cc69ebfe3444b4305a2a1679d84ae23
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-08-31 20:57:32 +0800

    Create leetcode_572.cpp

---
+++ leetcode/two_sum/leetcode_572.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool res = false;
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) { 
+        inOrderCheck(root, subRoot);
+        return res;
+    }
+    
+    void inOrderCheck(TreeNode* root, TreeNode* subRoot) {
+        if (root == nullptr)
+            return;
+
+        inOrderCheck(root->left, subRoot);
+        if (root->val == subRoot->val)
+            if (isEqualTree(root, subRoot) == true)
+                res = true;
+        if (res == true)
+            return;
+        inOrderCheck(root->right, subRoot);
+    }
+
+    bool isEqualTree(TreeNode* root_1, TreeNode* root_2) {
+        if (root_1 == nullptr && root_2 == nullptr)
+            return true;
+        if (root_1 == nullptr && root_2 != nullptr)
+            return false;
+        if (root_1 != nullptr && root_2 == nullptr)
+            return false;
+        if (root_1->val != root_2->val)
+            return false;
+        return 
+            isEqualTree(root_1->left, root_2->left) && isEqualTree(root_1->right, root_2->right);
+    }
+};
+
+
+// recursion 
+class Solution {
+public:
+    bool res = false;
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) { 
+        if (root == nullptr && subRoot == nullptr)
+            return true;
+        if (root == nullptr && subRoot != nullptr)
+            return false;
+
+        return isEqualTree(root, subRoot) ||
+               isSubtree(root->left, subRoot) ||
+               isSubtree(root->right, subRoot);
+    }
+
+    bool isEqualTree(TreeNode* root_1, TreeNode* root_2) {
+        if (root_1 == nullptr && root_2 == nullptr)
+            return true;
+        return root_1 && root_2 &&
+               root_1->val == root_2->val &&
+               isEqualTree(root_1->left, root_2->left) &&
+               isEqualTree(root_1->right, root_2->right);
+    }
+};
