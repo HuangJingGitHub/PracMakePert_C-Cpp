440f65fd46a7ca3598eb60e12756b601321c1437
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-09-20 23:58:15 +0800

    Create leetcode_519.cpp

---
+++ leetcode/two_sum/leetcode_519.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+unordered_map<int, int> map_;
+int size_;
+int row_num_;
+int col_num_;
+
+    Solution(int m, int n) {
+        size_ = m * n;
+        row_num_ = m;
+        col_num_ = n;
+        srand(time(NULL));
+    }
+    
+    vector<int> flip() {
+        int random_indix = rand() % size_;
+        if (random_indix == size_ - 1) {
+            size_--;
+            return {random_indix / col_num_, random_indix % col_num_};
+        }
+
+        int temp = random_indix;
+        while (map_.count(temp) == 1) 
+            temp = map_[temp];
+        map_[random_indix] = size_ - 1;
+        size_--;
+        return {temp / col_num_, temp % col_num_};
+    }
+    
+    void reset() {
+        map_.clear();
+        size_ = row_num_ * col_num_;
+    }
+
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(m, n);
+ * vector<int> param_1 = obj->flip();
+ * obj->reset();
+ */
