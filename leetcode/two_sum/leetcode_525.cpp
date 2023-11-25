5b067e87fb385848bbabef024aaf5b2076d5a929
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2023-11-25 23:25:24 +0800

    Update leetcode_525.cpp

--- leetcode/two_sum/leetcode_525.cpp
+++ leetcode/two_sum/leetcode_525.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        return 0;
+        int res = 0;
+        unordered_map<int, int> pre_sum_idx;
+        int cnt = 0;
+        pre_sum_idx[cnt] = -1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1)
+                cnt++;
+            else
+                cnt--;
+            
+            if (pre_sum_idx.count(cnt) != 0) 
+                res = max(res, i - pre_sum_idx[cnt]);
+            else
+                pre_sum_idx[cnt] = i;
+        }
+        return res;
     }
 };
