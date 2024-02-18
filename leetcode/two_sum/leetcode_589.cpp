26ce1e5709ee5b19e3e02bdaf8c48cda01797f60
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-02-18 18:24:12 +0800

    Create leetcode_589.cpp

---
+++ leetcode/two_sum/leetcode_589.cpp
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> res_;
+    vector<int> preorder(Node* root) {
+        if (root == nullptr)
+            return {};
+        
+        res_.push_back(root->val);
+        for (auto& child : root->children)
+            preorder(child);
+        return res_;
+    }
+};
