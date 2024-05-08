d51139ecf9d144056f76e0b718b18ccbac46e145
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-05-08 12:43:24 +0800

    Update leetcode_553.cpp

--- leetcode/two_sum/leetcode_553.cpp
+++ leetcode/two_sum/leetcode_553.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     string optimalDivision(vector<int>& nums) {
-        string res;
+        string res = to_string(nums[0]);
+
+        if (nums.size() == 1)
+            return res;
+        if (nums.size() == 2) {
+            res += "/" + to_string(nums[1]);
+            return res;
+        }
+
+        res += "/(";
+        for (int i = 1; i < nums.size() - 1; i++)
+            res += to_string(nums[i]) + "/";
+        res += to_string(nums.back()) + ")";
 
         return res;
     }
