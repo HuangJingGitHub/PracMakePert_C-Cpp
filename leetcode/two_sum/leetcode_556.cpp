8f0cfc0941eba89a1f0b175eebd5844892f093bb
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-05-13 23:52:21 +0800

    Create leetcode_556.cpp

---
+++ leetcode/two_sum/leetcode_556.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        vector<int> digits;
+        int original_num = n;
+        while (original_num != 0) {
+            digits.push_back(original_num % 10);
+            original_num /= 10；
+        }
+        std::reverse(digits.begin(), digits.end());
+
+        int digit_num = digits.size();
+        vector<int> smaller_idx(digit_num, -1);
+        
+        return -1;
+    }
+};
