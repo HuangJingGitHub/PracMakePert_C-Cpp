24aa4b96ec9354c852e134e52ad7e41000cb0ac5
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-05-21 14:09:46 +0800

    Update leetcode_556.cpp

--- leetcode/two_sum/leetcode_556.cpp
+++ leetcode/two_sum/leetcode_556.cpp
@@ -48,3 +48,41 @@ public:
         return res;
     }
 };
+
+// More concise
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        if (n < 10)
+            return -1;
+        
+        vector<int> digits;
+        while (n != 0) {
+            digits.push_back(n % 10);
+            n /= 10;
+        }
+        std::reverse(digits.begin(), digits.end());
+
+        bool max_element = true;
+        for (int i = digits.size() - 2; i >= 0; i--) {
+            if (digits[i] < digits[i + 1]) {
+                std::partial_sort(digits.begin() + i + 1, digits.end(), digits.end());
+                for (int j = i + 1; j < digits.size(); j++) {
+                    if (digits[j] > digits[i]) {
+                        std::swap(digits[i], digits[j]);
+                        max_element = false;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        if (max_element == true)
+            return -1;
+
+        long long res = 0;
+        for (int& single_digit : digits)
+            res = res * 10 + single_digit;
+        return (res > INT_MAX) ? -1 : res;
+    }
+};
