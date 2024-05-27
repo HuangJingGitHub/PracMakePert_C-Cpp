2d6304931979b59b4ce90c625eb0daf5d02f729e
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-05-27 22:59:51 +0800

    Update leetcode_560.cpp

--- leetcode/two_sum/leetcode_560.cpp
+++ leetcode/two_sum/leetcode_560.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-
+        int res = 0, pre_sum = 0;
+        map<int, int> pre_sum_counter;
+        pre_sum_counter[0] = 1;
+        for (size_t i = 0; i < nums.size(); i++) {
+            pre_sum += nums[i];
+            if (pre_sum_counter.count(pre_sum - k) != 0)
+                res += pre_sum_counter[pre_sum - k];
+            pre_sum_counter[pre_sum]++;
+        }
+        return res;
     }
 };
