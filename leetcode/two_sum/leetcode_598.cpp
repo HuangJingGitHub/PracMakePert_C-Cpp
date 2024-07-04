04bf874401f6b75502bea5d1fb272d596a8ebf37
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-07-04 19:59:48 +0800

    Create leetcode_598.cpp

---
+++ leetcode/two_sum/leetcode_598.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        if (ops.size() == 0)
+            return m * n;
+
+        int row_num = 1e5, col_num = 1e5;
+        for (auto& ops_i : ops) {
+            row_num = min(row_num, ops_i[0]);
+            col_num = min(col_num, ops_i[1]);
+        }
+        
+        return row_num * col_num;
+    }
+};
