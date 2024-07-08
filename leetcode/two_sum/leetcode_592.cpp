db1b9410bf4c854c258a47cd6a9cf687f1aee9c0
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-07-08 23:45:39 +0800

    Create leetcode_592.cpp

---
+++ leetcode/two_sum/leetcode_592.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string fractionAddition(string expression) {
+        bool leading_positive = true;
+        if (expression[0] != '-')
+            leading_positive = false;
+        
+        return "";
+    }
+};
