1e2657a113ca58806743128e744a197119b7550b
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-07-11 22:43:52 +0800

    Update leetcode_592.cpp

--- leetcode/two_sum/leetcode_592.cpp
+++ leetcode/two_sum/leetcode_592.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     string fractionAddition(string expression) {
-        bool leading_positive = true;
-        if (expression[0] != '-')
-            leading_positive = false;
-        
+        int res_numerator = 0, res_denominator = 1;
+        int start_idx = 0, end_idx = 0;
+
         return "";
     }
 };
