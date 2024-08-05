d4520f3460cf7462006c55b8eb5ab64cee9bd609
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-08-05 23:21:09 +0800

    Create leetcode_581.cpp

---
+++ leetcode/two_sum/leetcode_581.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int mid_max = nums[0], mid_min = nums.back();
+        int mid_start = 0, mid_end = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < mid_max)
+                mid_end = i;
+            else
+                mid_max = nums[i];
+        }
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (nums[i] > mid_min)  
+                mid_start = i;
+            else
+                mid_min = nums[i];
+        } 
+
+        if (mid_start == 0 && mid_end == 0)
+            return 0;
+        return mid_end - mid_start + 1;
+    }
+};
