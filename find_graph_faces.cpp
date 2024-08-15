8f74ef7262bfa37d437911efebb2a00eee7c7a10
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-08-15 22:11:51 +0800

    Add files via upload

---
+++ find_graph_faces.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct Point {
+    int64_t x, y;
+
+    Point(int64_t x_, int64_t y_): x(x_), y(y_) {}
+
+    Point operator - (const Point & p) const {
+        return Point(x - p.x, y - p.y);
+    }
+
+    int64_t cross (const Point & p) const {
+        return x * p.y - y * p.x;
+    }
+
+    int64_t cross (const Point & p, const Point & q) const {
+        return (p - *this).cross(q - *this);
+    }
+
+    int half () const {
+        return int(y < 0 || (y == 0 && x < 0));
+    }
+};
+
+std::vector<std::vector<size_t>> find_faces(std::vector<Point> vertices, std::vector<std::vector<size_t>> adj) {
+    size_t n = vertices.size();
+    std::vector<std::vector<char>> used(n);
+    for (size_t i = 0; i < n; i++) {
+        used[i].resize(adj[i].size());
+        used[i].assign(adj[i].size(), 0);
+        auto compare = [&](size_t l, size_t r) {
+            Point pl = vertices[l] - vertices[i];
+            Point pr = vertices[r] - vertices[i];
+            if (pl.half() != pr.half())
+                return pl.half() < pr.half();
+            return pl.cross(pr) > 0;
+        };
+        std::sort(adj[i].begin(), adj[i].end(), compare);
+    }
+    std::vector<std::vector<size_t>> faces;
+    for (size_t i = 0; i < n; i++) {
+        for (size_t edge_id = 0; edge_id < adj[i].size(); edge_id++) {
+            if (used[i][edge_id]) {
+                continue;
+            }
+            std::vector<size_t> face;
+            size_t v = i;
+            size_t e = edge_id;
+            while (!used[v][e]) {
+                used[v][e] = true;
+                face.push_back(v);
+                size_t u = adj[v][e];
+                size_t e1 = std::lower_bound(adj[u].begin(), adj[u].end(), v, [&](size_t l, size_t r) {
+                    Point pl = vertices[l] - vertices[u];
+                    Point pr = vertices[r] - vertices[u];
+                    if (pl.half() != pr.half())
+                        return pl.half() < pr.half();
+                    return pl.cross(pr) > 0;
+                }) - adj[u].begin() + 1;
+                if (e1 == adj[u].size()) {
+                    e1 = 0;
+                }
+                v = u;
+                e = e1;
+            }
+            std::reverse(face.begin(), face.end());
+            int sign = 0;
+            for (size_t j = 0; j < face.size(); j++) {
+                size_t j1 = (j + 1) % face.size();
+                size_t j2 = (j + 2) % face.size();
+                int64_t val = vertices[face[j]].cross(vertices[face[j1]], vertices[face[j2]]);
+                if (val > 0) {
+                    sign = 1;
+                    break;
+                } else if (val < 0) {
+                    sign = -1;
+                    break;
+                }
+            }
+            if (sign <= 0) {
+                faces.insert(faces.begin(), face);
+            } else {
+                faces.emplace_back(face);
+            }
+        }
+    }
+    return faces;
+}
+
+int main(int argc, char** argv) {
+    std::vector<Point> vertex_vec{Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0), Point(0, 2)};
+    std::vector<std::vector<size_t>> adj{{1, 2, 3}, {0, 2, 4}, {0, 1, 3, 4}, {0, 2}, {1, 2}};
+    std::vector<std::vector<size_t>> faces = find_faces(vertex_vec, adj);
+    for (std::vector<size_t>& cur_face : faces) {
+        for (size_t& cur_node : cur_face)
+            std::cout << cur_node << "--";
+        std::cout << "\n";
+    }
+    return 0;
+}
