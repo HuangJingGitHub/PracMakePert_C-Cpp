9a9b26a8279bc85dedc571f931a6c73b9cbb9580
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-09-25 19:13:25 -0700

    Create leetcode_575.cpp

---
+++ leetcode/two_sum/leetcode_575.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        set<int> totalType;
+        for (int& type : candyType) {
+            totalType.insert(type);
+        }
+
+        return min(totalType.size(), candyType.size() / 2);
+    }
+};
