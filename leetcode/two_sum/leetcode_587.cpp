4ebd67d9b86a4ee50bb0926850f49759e0ca2c9f
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-10-09 00:39:20 -0700

    Update leetcode_587.cpp

--- leetcode/two_sum/leetcode_587.cpp
+++ leetcode/two_sum/leetcode_587.cpp
@@ -8,7 +8,7 @@ public:
         vector<int> v_12{p_2[0] - p_1[0], p_2[1] - p_1[1]},
                     v_23{p_3[0] - p_2[0], p_3[1] - p_2[1]};
         int cross_product = v_12[0] * v_23[1] - v_23[0] * v_12[1];
-        return cross_product < 0;
+        return cross_product <= 0;
     }
 
     vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
@@ -21,10 +21,8 @@ public:
         upper.push(trees[0]);
         upper.push(trees[1]);
 
-        std::cout << "OK1\n"; 
         vector<int> p_right, p_mid, p_left;
         for (int i = 2; i < tree_num; i++) {
-            cout << i << "\n";
             upper.push(trees[i]);
 
             p_right = upper.top();
@@ -35,26 +33,28 @@ public:
             upper.push(p_mid);
             upper.push(p_right);
 
-            while (upper.size() > 2 &&
-                   TurnRight(p_left, p_mid, p_right) == false) {
+            while (TurnRight(p_left, p_mid, p_right) == false) {
                 upper.pop();
                 upper.pop();
                 upper.push(p_right);
 
-                p_right = upper.top();
-                upper.pop();
-                p_mid = upper.top();
-                upper.pop();
-                p_left = upper.top();
-                upper.push(p_mid);
-                upper.push(p_right);
+                if (upper.size() > 2) {
+                    p_right = upper.top();
+                    upper.pop();
+                    p_mid = upper.top();
+                    upper.pop();
+                    p_left = upper.top();
+                    upper.push(p_mid);
+                    upper.push(p_right);
+                }
+                else
+                    break;
             }
         }
-        std::cout << "OK\n"; 
 
         stack<vector<int>> lower;
-        upper.push(trees.back());
-        upper.push(trees[tree_num - 2]);
+        lower.push(trees.back());
+        lower.push(trees[tree_num - 2]);
 
         for (int i = tree_num - 3; i >= 0; i--) {
             lower.push(trees[i]);
@@ -67,27 +67,30 @@ public:
             lower.push(p_mid);
             lower.push(p_left);
 
-            while (upper.size() > 2 &&
-                   TurnRight(p_right, p_mid, p_left) == false) {
+            while (TurnRight(p_right, p_mid, p_left) == false) {
                 lower.pop();
                 lower.pop();
                 lower.push(p_left);
 
-                p_left = lower.top();
-                lower.pop();
-                p_mid = lower.top();
-                lower.pop();
-                p_right = lower.top();
-                lower.push(p_mid);
-                lower.push(p_left);
+                if (lower.size() > 2) {
+                    p_left = lower.top();
+                    lower.pop();
+                    p_mid = lower.top();
+                    lower.pop();
+                    p_right = lower.top();
+                    lower.push(p_mid);
+                    lower.push(p_left);
+                }
+                else
+                    break;
             }
         }
 
-        std::cout << "OK\n";
-
         vector<vector<int>> upper_vec(upper.size());
+        set<vector<int>> vertex_set;
         for (int i = 0; i < upper_vec.size(); i++) {
             upper_vec[i] = upper.top();
+            vertex_set.insert(upper.top());
             upper.pop();
         }
         std::reverse(upper_vec.begin(), upper_vec.end());
@@ -99,8 +102,10 @@ public:
             lower.pop();
         }
         std::reverse(lower_vec.begin(), lower_vec.end());
-        for (vector<int>& p : lower_vec)
-            upper_vec.push_back(p);
+        for (vector<int>& p : lower_vec) {
+            if (vertex_set.count(p) == 0)   // Handle cases where all vertices are on a line. upper and lower
+                upper_vec.push_back(p);     // have identical vertices.
+        }
         return upper_vec;
     }
 };
