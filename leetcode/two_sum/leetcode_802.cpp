9e0aea15396826329500209f6973467ed55e8011
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-10-21 00:35:53 -0700

    Update leetcode_802.cpp

--- leetcode/two_sum/leetcode_802.cpp
+++ leetcode/two_sum/leetcode_802.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         vector<int> res;
-        vector<bool> visited(graph.size(), false);
-        for (int i = 0; i < graph.size(); i++) {
-            if (dfs(graph, visited, i, res) == true)
+        vector<bool> visited(graph.size(), false), is_eventual_safe(graph.size(), false);
+
+        for (int i = 0; i < graph.size(); i++) 
+            if (dfs(graph, visited, is_eventual_safe, i) == true)
                 res.push_back(i);
-        }
+        
         return res;
     }
 
-    bool dfs(vector<vector<int>>& graph, vector<bool>& visited, int i, vector<int>& res) {
-        if (visited[i] == true)
-            return false;
+    bool dfs(vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& is_eventual_safe, int i) {
+        if (visited[i] == true) 
+            return is_eventual_safe[i];
 
         visited[i] = true;
-        for (int j : graph[i]) {
-            if (dfs(graph, visited, j, res) == false)
+        for (int j : graph[i]) 
+            if (dfs(graph, visited, is_eventual_safe, j) == false) 
                 return false;
-        }
+        
+        is_eventual_safe[i] = true;
         return true;
     }
 };
