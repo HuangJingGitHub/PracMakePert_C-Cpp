2d49ffffc23a22830c57567e89ca460afcfc51f1
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-10-24 13:18:50 -0700

    Update leetcode_841.cpp

--- leetcode/two_sum/leetcode_841.cpp
+++ leetcode/two_sum/leetcode_841.cpp
@@ -19,3 +19,27 @@ public:
             dfs(rooms, visited, key);
     }
 };
+
+// bfs
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<bool> visited(rooms.size(), false);
+
+        queue<int> room_keys;
+        room_keys.push(0);
+        while (room_keys.empty() == false) {
+            int cur_room = room_keys.front();
+            room_keys.pop();
+
+            visited[cur_room] = true;
+            for (int key : rooms[cur_room])
+                if (visited[key] == false)
+                    room_keys.push(key);
+        }
+        for (bool is_visited : visited)
+            if (is_visited == false)
+                return false;
+        return true;
+    }
+};
