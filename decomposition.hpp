1744b6dd5776ffa4e1dabf1eb8bdedbc4f2fa842
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2024-10-25 18:40:43 -0700

    Add files via upload

--- decomposition.hpp
+++ decomposition.hpp
@@ -48,7 +48,7 @@ vector<vector<int>> FindPlannarFaces(const vector<PolygonObstacle>& obstacles, c
     vector<vector<int>> faces;
     for (int i = 0; i < obs_num; i++) {
         for (int edge_id = 0; edge_id < adjacency_list[i].size(); edge_id++) {
-            if (used[i][edge_id]) {
+            if (used[i][edge_id] == true) {
                 continue;
             }
             vector<int> face;
@@ -58,26 +58,26 @@ vector<vector<int>> FindPlannarFaces(const vector<PolygonObstacle>& obstacles, c
                 used[v][e] = true;
                 face.push_back(v);
                 int u = adjacency_list[v][e];
-                int e1 = std::lower_bound(adjacency_list[u].begin(), adjacency_list[u].end(), v, [&](int l, int r) {
+                int e_1 = std::lower_bound(adjacency_list[u].begin(), adjacency_list[u].end(), v, [&](int l, int r) {
                     Point2f pl = obs_centroids[l] - obs_centroids[u];
                     Point2f pr = obs_centroids[r] - obs_centroids[u];
                     if (InNegativeHalfPlane(pl) != InNegativeHalfPlane(pr))
                         return InNegativeHalfPlane(pl) < InNegativeHalfPlane(pr);
                     return pl.cross(pr) > 0;
                 }) - adjacency_list[u].begin() + 1;
-                if (e1 == adjacency_list[u].size()) {
-                    e1 = 0;
+                if (e_1 == adjacency_list[u].size()) {
+                    e_1 = 0;
                 }
                 v = u;
-                e = e1;
+                e = e_1;
             }
 
             std::reverse(face.begin(), face.end());
             int sign = 0;
             for (int j = 0; j < face.size(); j++) {
-                int j1 = (j + 1) % face.size();
-                int j2 = (j + 2) % face.size();
-                float val = TriplePtCross(obs_centroids[face[j]], obs_centroids[face[j1]], obs_centroids[face[j2]]);
+                int j_1 = (j + 1) % face.size();
+                int j_2 = (j + 2) % face.size();
+                float val = TriplePtCross(obs_centroids[face[j]], obs_centroids[face[j_1]], obs_centroids[face[j_2]]);
                 if (val > 0) {
                     sign = 1;
                     break;
