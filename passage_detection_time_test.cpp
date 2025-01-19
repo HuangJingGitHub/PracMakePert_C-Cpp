5dd86f79116d7f277c6693d74c7be83b75240b62
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2025-01-18 18:45:21 -0800

    Add files via upload

---
+++ passage_detection_time_test.cpp
@@ -0,0 +1,109 @@
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include "../decomposition.hpp"
+
+using namespace std::chrono;
+
+void PassageDetectionTest(const int test_num = 100, 
+                        const int obs_num = 50, 
+                        const int side_len = 30, 
+                        Mat back_img = Mat(Size(1000, 600), CV_64FC3, Scalar(255, 255, 255))) {
+    vector<float> EV_check_time(test_num, 0), DG_check_time(test_num, 0),
+                  EV_cell_check_time(test_num, 0);
+    vector<int> EV_psg_num(test_num, 0), DG_psg_num(test_num, 0), 
+                EV_cell_num(test_num, 0),
+                EV_psg_segment_num(test_num, 0), 
+                EV_cell_via_segment_num(test_num, 0);
+    bool varying_side_len = false;
+    for (int test_idx = 0; test_idx < test_num; test_idx++) {
+        vector<PolygonObstacle> obs_vec = GenerateRandomObstacles(obs_num, back_img.size(), side_len, varying_side_len);
+
+        auto start_time = high_resolution_clock::now();
+        // EV: extended visibility
+        auto EV_check_res = ExtendedVisibilityPassageCheck(obs_vec, false, false);
+        auto end_time = high_resolution_clock::now();
+        auto duration_time = duration_cast<milliseconds>(end_time - start_time);
+        EV_check_time[test_idx] = (float) duration_time.count();
+        EV_psg_num[test_idx] = EV_check_res.first.size();
+
+        start_time = high_resolution_clock::now();
+        vector<vector<int>> cells = ReportGabrielCells(obs_vec, EV_check_res.first);
+        end_time = high_resolution_clock::now();
+        duration_time = duration_cast<milliseconds>(end_time - start_time);
+        EV_cell_check_time[test_idx] = (float) duration_time.count();
+        EV_cell_num[test_idx] = cells.size();
+
+        auto EV_check_seg_res = ExtendedVisibilityPassageCheck(obs_vec, false, true);
+        EV_psg_segment_num[test_idx] = EV_check_seg_res.first.size();
+
+        cells = ReportGabrielCells(obs_vec, EV_check_seg_res.first);
+        EV_cell_via_segment_num[test_idx] = cells.size();
+
+        // Using Delaunary graph
+        start_time = high_resolution_clock::now();
+        auto DG_check_res = PassageCheckInDelaunayGraph(obs_vec, false);        
+        end_time = high_resolution_clock::now();
+        duration_time = duration_cast<milliseconds>(end_time - start_time);
+        DG_check_time[test_idx] = (float) duration_time.count();
+        DG_psg_num[test_idx] = DG_check_res.first.size();
+    }
+
+    string save_directory = "./src/ptopp/src/data/";
+    string file_name_postfix, file_name, test_info;
+    file_name = "detection_data_obs_" + to_string(obs_num) + "_side_" + to_string(side_len) + "_";
+    std::time_t cur_time = std::time(0);
+    std::tm* cur_tm = std::localtime(&cur_time);
+    file_name_postfix = to_string(cur_tm->tm_year + 1900) + "-"
+                        + to_string(cur_tm->tm_mon + 1) + "-"
+                        + to_string(cur_tm->tm_mday) + "_"
+                        + to_string(cur_tm->tm_hour) + "-"
+                        + to_string(cur_tm->tm_min) + ".tex";
+    file_name = file_name + file_name_postfix;
+    test_info = "Environment dimension: " + to_string(back_img.size().width) + " x " + to_string(back_img.size().height) + "\n"  
+                + "Obstacle num: " + to_string(obs_num) + "\n"
+                + "Obstacle main/max dimension: " + to_string(side_len) + "\n"
+                + "Total test number: " + to_string(test_num) + "\n" 
+                + "Varying side length: " + (varying_side_len ? "true" : "false") + "\n"
+                + "No environment boundaries are considered\n";    
+
+    ofstream  data_save_os;
+    data_save_os.open(save_directory + file_name, std::ios::trunc);
+    if (data_save_os.is_open() == false) {
+        std::cerr << "Fail to open the data storage file!\n";
+    }
+    else {
+        data_save_os << test_info;
+        data_save_os << "Valid passage number using Gabriel condition: \n";
+        for (int num : EV_psg_num)
+            data_save_os << num << "\n";
+        data_save_os << "Corresponding detection time (ms) via brute-force traversal: \n";        
+        for (float time : EV_check_time)
+            data_save_os << time << "\n";
+        data_save_os << "Valid passage segment number using extended visibility condition: \n";        
+        for (int num : EV_psg_segment_num)
+            data_save_os << num << "\n";
+        data_save_os << "Gabriel cell number from Gabriel condition: \n";        
+        for (int num : EV_cell_num)
+            data_save_os << num << "\n";            
+        data_save_os << "Corresponding cell detection time (ms): \n";        
+        for (float time : EV_cell_check_time)
+            data_save_os << time << "\n";     
+        data_save_os << "Cell number from passage segments using extended visibility condition: \n";        
+        for (int num : EV_cell_via_segment_num)
+            data_save_os << num << "\n";          
+        data_save_os << "Valid assage number using Gabriel condition, detected in Delaunay garph: \n";
+        for (int num : DG_psg_num)
+            data_save_os << num << "\n";
+        data_save_os << "Corresponding detection time in Delaunay graph (ms): \n";        
+        for (float time : DG_check_time)
+            data_save_os << time << "\n";                                 
+    }           
+}
+
+
+int main(int argc, char** argv) {
+    Mat back_img(Size(1000, 600), CV_64FC3, Scalar(255, 255, 255));
+    PassageDetectionTest(50, 100, 30, back_img);
+    return 0;
+}
