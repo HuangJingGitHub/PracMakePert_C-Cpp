90b4415c275ccb4f608a03a8be297bef675a99ee
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2025-03-07 20:36:06 +0800

    Create leetcode_990.cpp

---
+++ leetcode/two_sum/leetcode_990.cpp
@@ -0,0 +1,42 @@
+class UnionFind {
+public:
+    vector<int> parent;
+
+    UnionFind(int n) {
+        parent.resize(n);
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    void unionSet(int idx_1, int idx_2) {
+        parent[find(idx_2)] = find(idx_1);
+    }
+
+    int find(int idx) {
+        if (parent[idx] != idx)
+            parent[idx] = find(parent[idx]);
+        return parent[idx];
+    }
+};
+
+class Solution {
+public:
+    bool equationsPossible(vector<string>& equations) {
+        UnionFind groups(26);
+        for (auto& eqn : equations) {
+            if (eqn[1] == '=') {
+                int x = eqn[0] - 'a', y = eqn[3] - 'a',
+                    min_v = min(x, y), max_v = max(x, y);
+                groups.unionSet(min_v, max_v);
+            }
+        }
+        for (auto& eqn: equations) {
+            if (eqn[1] == '!') {
+                int x = eqn[0] - 'a', y = eqn[3] - 'a';
+                if (groups.find(x) == groups.find(y))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
