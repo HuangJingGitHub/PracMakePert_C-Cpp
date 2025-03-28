bcff46abb50dc4d18f91dc09a7775abf34fb4dbf
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2025-03-28 23:35:15 +0800

    Add files via upload

---
+++ passage_detection_time_test_3d.cpp
@@ -0,0 +1,50 @@
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include "../3d/RRTStar_3d.hpp"
+#include "../decomposition.hpp"
+
+using namespace std::chrono;
+
+void PassageDetectionTest3d(const int test_num = 50, 
+                          const int obs_num = 50, 
+                          const int side_len = 30,
+                          Mat back_img = Mat(Size(1000, 600), CV_64FC3, Scalar(255, 255, 255))) {
+    
+          
+}
+
+int main(int argc, char** argv) {
+    Mat back_img(Size(1000, 600), CV_64FC3, Scalar(255, 255, 255));
+    int obs_num = 200;
+    float config_height = 400, side_len = 60;
+    bool varying_side_len = true;
+
+    vector<PolygonObstacle3d> obs_vec = GenerateRandomObstacles3d(obs_num, back_img.size(), config_height, side_len, varying_side_len);
+    vector<PolygonObstacle> obs_vec_2d = ConvertTo2dObstacles(obs_vec);
+
+    Passages3d passages = PassageCheckInDelaunayGraph3d(obs_vec);
+    Passages passages_2d = PassageCheckInDelaunayGraph(obs_vec_2d, false);
+    int cnt = 0;
+    for (int i = 0; i < passages.pairs.size(); i++) 
+        if (passages.heights[i][0] < 1e-2) 
+            cnt++;
+        
+    // cout << "2d results: \n";
+    // for (int i = 0; i < passages_2d.pairs.size(); i++)
+    //     cout << passages_2d.pairs[i][0] << "-" << passages_2d.pairs[i][1] << "\n";
+    cout << cnt << " vs " << passages_2d.pairs.size() << "\n";
+    
+    for (int i = 4; i < obs_num + 4; i++) {
+        Point2f cur_centroid = GetObstaclesCentroids({obs_vec_2d[i]}).front();
+        vector<vector<Point>> input_array_cv(1, vector<Point>(obs_vec_2d[i].vertices.begin(), obs_vec_2d[i].vertices.end()));
+        fillPoly(back_img, input_array_cv, Scalar(0.827, 0.827, 0.827));
+        int obs_vertex_num = obs_vec_2d[i].vertices.size();
+        for (int j = 0; j < obs_vertex_num; j++)
+            line(back_img, obs_vec_2d[i].vertices[j], obs_vec_2d[i].vertices[(j + 1) % obs_vertex_num], Scalar(0, 0, 0), 2);   
+        putText(back_img, std::to_string(i), cur_centroid - Point2f(10, -5), cv::FONT_HERSHEY_SIMPLEX, 0.6, Scalar(255, 0, 0), 2);         
+    }
+    imshow("Obstacles on Base Ground", back_img);
+    waitKey(0);
+    return 0;    
+}
