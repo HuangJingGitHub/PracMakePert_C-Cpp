d40f559ac7ceb286366eb3dc3af0868a700d18d3
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2025-06-24 23:47:56 +0800

    Create leetcode_605.cpp

---
+++ leetcode/two_sum/leetcode_605.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int cnt = 0;
+        for (int i = 0; i < flowerbed.size(); i++) {
+            if (flowerbed[i] == 1)
+                continue;
+            
+            
+        }
+    }
+};
