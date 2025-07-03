3a10c7d9b0f76a57e6db7081e008f602d514a59d
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2025-07-03 12:53:25 +0800

    Create leetcode_611.cpp

---
+++ leetcode/two_sum/leetcode_611.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int res = 0, n = nums.size();
+        if (n < 3)
+            return 0;
+
+        sort(nums.begin(), nums.end());
+        for (int idx_0 = 0; idx_0 < n - 2; idx_0++) {
+            for (int idx_1 = idx_0 + 1; idx_1 < n - 1; idx_1++) {
+                for (int idx_2 = idx_1 + 1; idx_2 < n; idx_2++) {
+                    if (nums[idx_0] + nums[idx_1] > nums[idx_2]) 
+                        res++;
+                    else 
+                        break;
+                }
+            }
+        }
+        return res;
+    }
+};
