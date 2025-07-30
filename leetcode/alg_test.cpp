36bc15dd64aade445c4732f4c30910179d9ef5d1
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2025-07-30 11:58:10 +0800

    Update alg_test.cpp

--- leetcode/alg_test.cpp
+++ leetcode/alg_test.cpp
@@ -41,3 +41,72 @@ int main() {
     solution(input);
     return 0;
 }
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct State; // x y theta
+bool IsSafe(const State& state); // provided
+
+/*
+0				    																					  n-1
+   a1 b1            a2 b2
+   --------------------
+   | | i i+1        | |
+----- - - -------------------------------------------------
+   |-|--------------|-| 
+   a3 b3            a4 b4
+                            mid
+------|-------|------------|---------------|---------------
+
+*/
+
+/*
+0																									  n-1
+-------|------|------------|-------------|--------------
+
+                          | mid
+                          
+              |
+                                         |
+                                         
+       |
+                     |       
+                                  |
+                                                |
+                                                
+*/
+
+
+
+
+//---|---|-------|-------------|--------------------------
+
+bool IsPathSafe(const std::vector<State> &path) {
+  // for (auto state : path)
+  //   if (!IsSafe(state))
+  //     return false;
+  // return true;
+
+  if (path.size() == 1)
+    return IsSafe(path.front());
+  
+  int mid_idx = path.size() / 2, len = path.size();
+  queue<vector<int>> check_pair;
+  check_pair.push({mid_idx, len});
+  while (!check_pair.empty()) {
+    vector<int> cur_pair = check_pair.front();
+    mid_idx = cur_pair[0];
+    len = cur_pair[1];
+
+    State mid_state = path[mid_idx];
+    if (!IsSafe(mid_state))
+      return false;
+    check_pair.push{mid_idx - len / 4， len / 2};
+    check_pair.push{mid_idx + len / 4, len / 2};
+  }
+  
+  return true;
+}
