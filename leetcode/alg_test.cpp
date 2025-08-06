55a55a12c6f096ed9160c42dc06137e26b3d2a70
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2025-08-06 17:48:23 +0800

    Update alg_test.cpp

--- leetcode/alg_test.cpp
+++ leetcode/alg_test.cpp
@@ -110,3 +110,59 @@ bool IsPathSafe(const std::vector<State> &path) {
   
   return true;
 }
+/* 
+  你必须定义一个 `main()` 函数入口。
+  you must define a `main()` function entry.
+*/
+// given bicycle model,
+// v_rear, angular velocity w, wheelbase L
+// v_front = sqrt((v_rear^2 + L^2 * w^2)
+// v_front = w x (r_rear + L) = v_rear + w x L
+//
+// m x n 棋盘格, (p,q)位置是obs.
+// 左上到右下有多少种走法. （analytical expression）f(m, n) = f(m, n - 1) + f(m - 1, n)
+//                                                      = C(m + n - 2, m - 1) 
+// ---------
+// Given F(m, n)
+// G(m, n, (p, q)) = F(m, n) - F(p, q) * F(m - p + 1, n - q + 1)
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int m, int n) {
+  if (m <= 0 || n <= 0)
+    return -1;
+  
+  vector<vector<int>> dp(m, vector<int>(n, 0));
+  dp[0] = vector<int>(n, 1);
+  for (int row = 1; row < m; row++) {
+    dp[row][0] = 1;
+  }
+  /*if (p == 0) {
+    for (int col = q; col < n; col++)
+      dp[0][col] = 0;
+  }
+  if (q == 0) {
+    for (int row = p; row < m; row++)
+      dp[row][0] = 0;
+  }*/
+
+  // dp[p][q] = 0;
+  for (int row = 1; row < m; row++) {
+    for (int col = 1; col < n; col++) {
+      // if (row == p && col == q)
+      //    continue;
+      
+      dp[row][col] = dp[row][col - 1] + dp[row - 1][col];
+    }
+  }
+  return dp.back().back();
+}
+
+int main() {
+  int m = 3, n = 3;
+  cout << solution(m, n) << endl;
+  return 0;
+}
