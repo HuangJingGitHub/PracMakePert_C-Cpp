4bc71374449833326861fc538c7a7f20f454d687
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2025-08-14 12:51:47 +0800

    Update leetcode_1091.cpp

--- leetcode/two_sum/leetcode_1091.cpp
+++ leetcode/two_sum/leetcode_1091.cpp
@@ -5,12 +5,47 @@ public:
         if (grid[0][0] == 1)
             return -1;
         
-        int n = grid.size(), res = 0;
+        int n = grid.size(), res = 1;
         if (n == 1)
             return 1;
         
         vector<vector<bool>> visited(n, vector<bool>(n, false));
         queue<vector<int>> pos;
         pos.push({0, 0});
+        visited[0][0] = true;
+
+        while (!pos.empty()) {
+            int level_size = pos.size();
+            while (level_size > 0) {
+                vector<int> cur_pos = pos.front();
+                pos.pop();
+
+                int x = cur_pos[0], y = cur_pos[1];
+                if (x == n - 1 && y == n - 1) {
+                    return res;
+                }
+                
+
+                for (int x_dif : direction_x) {
+                    for (int y_dif : direction_y) {
+                        if (x_dif == 0 && y_dif == 0) {
+                            continue;
+                        }
+
+                        int new_x = x + x_dif, new_y = y + y_dif;
+                        if (new_x >= 0 && new_x < n
+                            && new_y >= 0 && new_y < n
+                            && grid[new_x][new_y] == 0
+                            && !visited[new_x][new_y]) {
+                            pos.push({new_x, new_y});
+                            visited[new_x][new_y] = true;
+                        }
+                    }
+                }
+                level_size--;
+            }
+            res += 1;
+        }
+        return -1;
     }
 };
