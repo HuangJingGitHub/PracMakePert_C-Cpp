c47423aaee434c8317ad7eb4c23032888546319a
Jing Huang <35371389+HuangJingGitHub@users.noreply.github.com>
2026-02-23 17:46:24 +0800

    Update leetcode_688.cpp

--- leetcode/two_sum/leetcode_688.cpp
+++ leetcode/two_sum/leetcode_688.cpp
@@ -1,25 +1,25 @@
 class Solution {
     static constexpr int DIRS[8][2] = {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+
 public:
     double knightProbability(int n, int k, int row, int column) {
         vector memo(k + 1, vector(n, vector<double>(n)));
-        auto dfs = [&](this auto& dfs, int k, int i, int j) -> double {
-            if (i < 0 || i >= n || j < 0 || j >= n) {
-                return 0;
-            }
-            if (k == 0) {
-                return 1;
-            }
-            double& res = memo[k][i][j]; 
-            if (res) { 
-                return res;
-            }
-            for (auto& [dx, dy] : DIRS) {
-                res += dfs(k - 1, i + dx, j + dy);
-            }
-            res /= 8;
+        return dfs(k, row, column, n, memo);
+    }
+
+    double dfs(int k, int row, int col, int n, vector<vector<vector<double>>>& memo) {
+        if (row < 0 || row >= n || col < 0 || col >= n)
+            return 0;
+        if (k == 0)
+            return 1;
+        
+        double& res = memo[k][row][col];
+        if (res)
             return res;
-        };
-        return dfs(k, row, column);
+        
+        for (auto& [dx, dy] : DIRS)
+            res += dfs(k - 1, row + dx, col + dy, n, memo);
+        res /= 8;
+        return res;
     }
 };
